21383201b1d50fbfa11b5e913b49303c2cbc8e62
Kimberly-Fasbender <kimberly.fasbender@gmail.com>
2019-03-11 15:01:51 -0700

    Added testing for ReservationManager#initialize - generates an array of rooms

--- spec/reservation_manager_spec.rb
+++ spec/reservation_manager_spec.rb
@@ -2,8 +2,17 @@ require_relative "spec_helper"
 
 describe "ReservationManager class" do
   describe "Initialize" do
+    let (:manager) do
+      manager = ReservationManager.new
+    end
+
     it "Must be an instance of ReservationManager" do
-      expect(ReservationManager.new).must_be_kind_of ReservationManager
+      expect(manager).must_be_kind_of ReservationManager
+    end
+
+    it "Must " do
+      expect(manager.rooms).must_be_kind_of Array
+      expect(manager.rooms[0]).must_be_kind_of Room
     end
   end
 end
