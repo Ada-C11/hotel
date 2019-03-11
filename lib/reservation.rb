f6ff8ae6ec8c9e065004887e0d062dfb1b66d5f1
Kelly Downes <kedownes@gmail.com>
2019-03-11 12:11:47 -0700

    Setup Guard and SimpleCov

---
+++ lib/reservation.rb
@@ -0,0 +1,8 @@
+module Hotel
+  class Reservation
+    def initialize(checkin, checkout)
+      @checkin = checkin
+      @checkout = checkout
+    end
+  end
+end
---
+++ spec/reservation_spec.rb
@@ -0,0 +1,10 @@
+require_relative 'spec_helper'
+
+describe "Reservation" do
+  describe "#initialize" do
+    it "Creates an instance of Reservation" do
+      reservation = Hotel::Reservation.new("April 1, 2019", "April 5, 2019")
+      reservation.must_be_kind_of Hotel::Reservation
+    end
+  end
+end
