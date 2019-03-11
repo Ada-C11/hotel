9a01da5f9ef81d54acc10812a65dcc941bd85f76
Kimberly-Fasbender <kimberly.fasbender@gmail.com>
2019-03-11 14:33:25 -0700

    added testing to Reservation#Initialize - raise an ArgumentError for invalid check-out times

--- spec/reservation_spec.rb
+++ spec/reservation_spec.rb
@@ -2,20 +2,32 @@ require_relative "spec_helper"
 
 describe "Reservation Class" do
   describe "Initialize" do
-    before do
-      @reservation = Reservation.new(
-        check_in_date: "09-03-2018",
-        check_out_date: "10-03-2018",
+    let (:reservation) {
+      Reservation.new(
+        check_in_date: "2018-03-09",
+        check_out_date: "2018-03-10",
         room_number: 2,
       )
-    end
+    }
 
     it "Is an instance of Reservation" do
-      expect(@reservation).must_be_kind_of Reservation
+      expect(reservation).must_be_kind_of Reservation
     end
 
     it "Stores a room number" do
-      expect(@reservation.room_number).must_equal 2
+      expect(reservation.room_number).must_equal 2
+    end
+
+    it "Raises an ArgumentError for invalid check-out date" do
+      reservation = {
+        check_in_date: "2018-03-09",
+        check_out_date: "2018-03-08",
+        room_number: 2,
+      }
+
+      expect {
+        Reservation.new(reservation)
+      }.must_raise ArgumentError
     end
   end
 end
