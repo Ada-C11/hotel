a6ce373a742cdcc796164150bcae7818bd3554af
Alexandria McCarthy <alexandria.mccarthy77@gmail.com>
2019-03-11 14:30:01 -0700

    added duration_of_stay method for reservation class

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -1,9 +1,22 @@
 class Reservation
-  attr_accessor :reservation_id, :check_in_time, :check_out_time
+  attr_reader :reservation_id, :check_in_time, :check_out_time, :duration_of_stay
 
   def initialize(reservation_id: 0, check_in_time: nil, check_out_time: nil)
     @reservation_id = reservation_id
     @check_in_time = check_in_time
     @check_out_time = check_out_time
+  end
+
+  def duration_of_stay
+    #check out time - check in time = some time in days
+    unless @check_in_time == nil && @check_out_time == nil
+      @check_in_time = Date.parse(check_in_time)
+      @check_out_time = Date.parse(check_out_time)
+    end
+
+    return (check_out_time - check_in_time).to_i
+  end
+
+  def total_cost
   end
 end
--- spec/reservation_spec.rb
+++ spec/reservation_spec.rb
@@ -1,10 +1,18 @@
 require_relative "spec_helper"
 
-describe "Reservation class" do
+describe "Reservation initialization" do
   it "initializes a reservation when no keyword arguments are given" do
     my_rez = Reservation.new
     expect(my_rez.reservation_id).must_equal 0
     expect(my_rez.check_in_time).must_equal nil
     expect(my_rez.check_out_time).must_equal nil
+  end
+end
+
+describe "duration_of_time" do
+  it "must return an integer for duration of time" do
+    my_rez = Reservation.new(reservation_id: 3, check_in_time: "3rd April 2019", check_out_time: "10th April 2019")
+
+    expect(my_rez.duration_of_stay).must_be_instance_of Integer
   end
 end
