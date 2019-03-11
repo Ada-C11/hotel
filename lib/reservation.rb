6f250ef40abb43201c6b17816ced5cca3dcddc26
Angela <angela.ohh@gmail.com>
2019-03-11 13:47:36 -0700

    added testing for creating instance of Reservation

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -1,4 +1,15 @@
 class Reservation
-  def initialize(id, rooms, start_day, end_day)
+  attr_reader :reservation_id, :check_in, :check_out
+
+  def initialize(reservation_id, check_in: nil, check_out: nil)
+    @reservation_id = reservation_id
+    @check_in = check_in
+    @check_out = check_out
+  end
+
+  def cost
+  end
+
+  def duration
   end
 end
--- spec/reservation__spec.rb
+++ spec/reservation__spec.rb
@@ -1,8 +1,22 @@
 require_relative "spec_helper"
 
-describe "test" do
+describe "Reservation class initialization" do
   it "will return an instance of reservation" do
-    reservation = Reservation.new(1, {}, "time", "time")
+    reservation = Reservation.new(1)
     expect(reservation).must_be_kind_of Reservation
   end
+
+  it "check in date is nil unless values are given" do
+    test_reserve = Reservation.new(1)
+    expect(test_reserve.check_in).must_equal nil
+  end
+
+  it "check out date is nil unless values are given" do
+    test_reserve = Reservation.new(1)
+    expect(test_reserve.check_out).must_equal nil
+  end
 end
+
+# describe "calculate total cost of reservation" do
+#     it
+# end
