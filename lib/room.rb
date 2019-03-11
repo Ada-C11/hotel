f4ad29a977123ece679c1562a4a59b0316abeb06
Karla Guadron <ksguadron@gmail.com>
2019-03-11 12:15:54 -0700

    Made lib and spec files for the Reservation and Hotel class. Updated spec_helper. Created initialize method for Room class

--- lib/room.rb
+++ lib/room.rb
@@ -1,2 +1,17 @@
-class Room
+module HotelSystem
+  class Room
+    attr_reader :id, :price, 
+
+    def initalize(id:)
+      @id = id
+      @price = 200.00
+      @reservations = []
+    end
+
+    def add_reservation
+    end
+
+    def update_status
+    end
+  end
 end
