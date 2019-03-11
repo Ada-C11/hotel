47b37abab28c19e3865e835504cc24be20aeb719
Kimberly-Fasbender <kimberly.fasbender@gmail.com>
2019-03-11 14:34:14 -0700

    added raising an ArgumentError for invalid check-out dates functionality

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -1,9 +1,13 @@
+require "Date"
+
 class Reservation
   attr_reader :check_in_date, :check_out_date, :room_number
 
   def initialize(check_in_date:, check_out_date:, room_number:)
-    @check_in_date = check_in_date
-    @check_out_date = check_out_date
+    @check_in_date = Date.strptime(check_in_date)
+    @check_out_date = Date.strptime(check_out_date)
     @room_number = room_number
+
+    raise ArgumentError, "Check-out date cannot be before check-in date" if check_out_date < check_in_date
   end
 end
