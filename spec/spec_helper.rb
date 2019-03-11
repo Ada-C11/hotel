6047634f17a05a1da42b3175f6bf48fb91a80a30
Jessica <jessica.homet@gmail.com>
2019-03-11 13:13:59 -0700

    Modify spec helper to include front desk, reservation, and room

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,4 +6,6 @@ require "minitest/reporters"
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 # Require_relative your lib files here!
-require_relative "../lib/sample"
+require_relative "../lib/front_desk"
+require_relative "../lib/room"
+require_relative "../lib/reservation"
