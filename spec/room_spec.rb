169cd9b81ccb754deff8d7490840bf1bcb419d0c
Kimberly-Fasbender <kimberly.fasbender@gmail.com>
2019-03-11 13:23:52 -0700

    added testing for Room#initialize

--- spec/room_spec.rb
+++ spec/room_spec.rb
@@ -3,7 +3,7 @@ require_relative "spec_helper"
 describe "Room Class" do
   describe "Initialize" do
     it "Creates an instance of a Room" do
-      expect(Room.new).must_be_kind_of Room
+      expect(Room.new(number: 3)).must_be_kind_of Room
     end
   end
 end
