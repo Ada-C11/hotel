d4c6617029dd2d93de5f26468bc7fceade610970
Margaret Finnan <msfinnan@gmail.com>
2019-03-11 15:03:26 -0700

    Added duration method and tests

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -1,4 +1,5 @@
 # require_relative "spec_helper"
+require "date"
 
 class Reservation
   attr_reader :reservation_id, :start_date, :end_date
@@ -8,4 +9,18 @@ class Reservation
     @start_date = start_date
     @end_date = end_date
   end
+
+  def duration
+    duration = (Date.parse(@end_date) - Date.parse(@start_date))
+    if duration < 1
+      raise ArgumentError, "The duration must be at least one day"
+    else
+      return duration
+    end
+  end
+
+  #   def total_cost
+  #     cost = duration * 200
+  #     return cost
+  #   end
 end
--- spec/reservation_spec.rb
+++ spec/reservation_spec.rb
@@ -1,19 +1,36 @@
 require_relative "spec_helper"
+
 describe "Reservation class" do
-  it "Creates an instance of reservation" do
-    reservation = Reservation.new
-    expect(reservation).must_be_instance_of Reservation
+  describe "Initialize" do
+    it "Creates an instance of reservation" do
+      reservation = Reservation.new
+      expect(reservation).must_be_instance_of Reservation
+    end
+    it "Reservation class initialized with nil start  date if no date is given" do
+      reservation = Reservation.new
+      expect(reservation.start_date).must_equal nil
+    end
+    it "Reservation class initialized with nil  end date if no date is given" do
+      reservation = Reservation.new
+      expect(reservation.end_date).must_equal nil
+    end
+    it "Reservation id initialized as 0 if no reservation id is entered" do
+      reservation = Reservation.new
+      expect(reservation.reservation_id).must_equal 0
+    end
   end
-  it "Reservation class initialized with nil start  date if no date is given" do
-    reservation = Reservation.new
-    expect(reservation.start_date).must_equal nil
-  end
-  it "Reservation class initialized with nil  end date if no date is given" do
-    reservation = Reservation.new
-    expect(reservation.end_date).must_equal nil
-  end
-  it "Reservation id initialized as 0 if no reservation id is entered" do
-    reservation = Reservation.new
-    expect(reservation.reservation_id).must_equal 0
+  describe "Duration method" do
+    it "Duration returns the length of time between 2 dates" do
+      reservation = Reservation.new(start_date: "1st Mar 2019", end_date: "8th Mar 2019")
+      expect(reservation.duration).must_equal 7
+    end
+    it "Raises an ArgumentError if the duration is less than 1 day" do
+      reservation = Reservation.new(start_date: "7th Mar 2019", end_date: "6th Mar 2019")
+      expect { reservation.duration }.must_raise ArgumentError
+    end
+    it "Can calulate duration between 2 months" do
+      reservation = Reservation.new(start_date: "28th Feb 2019", end_date: "7th Mar 2019")
+      expect(reservation.duration).must_equal 7
+    end
   end
 end
