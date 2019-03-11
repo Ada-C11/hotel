83a09a24b7aac8621ff207fef1d45145c4aa7045
Alexandria McCarthy <alexandria.mccarthy77@gmail.com>
2019-03-11 13:38:59 -0700

    created initialize method for Reservation class

---
+++ lib/reservation.rb
@@ -0,0 +1,7 @@
+class Reservation
+    def initialize(reservation_id, check_in_time, check_out_time)
+        @reservation_id = reservation_id
+        @check_in_time = check_in_time
+        @check_out_time = check_out_time
+    end 
+end
