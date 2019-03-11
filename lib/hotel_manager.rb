7542f19a69ee953dc53bba2a60deac8cacc7cbe0
Erica Norris <norrise120@gmail.com>
2019-03-11 16:17:07 -0700

    Wrote tests for accessing reservations by date and wrote code to pass by creating make_reservation and list_reservations_by_date methods

--- lib/hotel_manager.rb
+++ lib/hotel_manager.rb
@@ -1,16 +1,34 @@
 require_relative "room.rb"
+require_relative "reservation.rb"
 
 module Hotel
   class Hotel_manager
     ROOM_NUMBERS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 
-    attr_reader :rooms
+    attr_reader :rooms, :reservations
 
     def initialize
       @rooms = ROOM_NUMBERS.map do |number|
         Hotel::Room.new(room: number)
       end
       @reservations = []
+    end
+
+    def make_reservation(room, start_date, end_date)
+      new_reservation = Hotel::Reservation.new(room: room, start_date: start_date, end_date: end_date)
+      @reservations.push(new_reservation)
+      return new_reservation
+    end
+
+    def list_reservations_by_date(date)
+      date = Date.parse(date)
+      found_reservations = []
+      @reservations.each do |reservation|
+        if date >= reservation.start_date && date < reservation.end_date
+          found_reservations.push(reservation)
+        end
+      end
+      return found_reservations
     end
   end
 end
--- spec/hotel_manager_spec.rb
+++ spec/hotel_manager_spec.rb
@@ -3,6 +3,8 @@ require_relative "spec_helper"
 describe "Hotel_manager class" do
   before do
     @hotel_manager = Hotel::Hotel_manager.new
+    @hotel_manager.make_reservation(1, "2019-3-15", "2019-3-20")
+    @hotel_manager.make_reservation(2, "2019-3-15", "2019-3-18")
   end
 
   describe "Hotel_manager instantiation" do
@@ -12,6 +14,27 @@ describe "Hotel_manager class" do
 
     it "has an array of 20 rooms" do
       expect(@hotel_manager.rooms.length).must_equal 20
+    end
+
+    describe "access reservations by date" do
+      it "returns an array with the correct number of reservations" do
+        reservations = @hotel_manager.list_reservations_by_date("2019-3-15")
+
+        expect(reservations).must_be_kind_of Array
+        expect(reservations.length).must_equal 2
+      end
+
+      it "doesn't include reservations where the end date is the date" do
+        reservations = @hotel_manager.list_reservations_by_date("2019-3-20")
+
+        expect(reservations.length).must_equal 0
+      end
+
+      it "includes reservations where the date is between the start and end date" do
+        reservations = @hotel_manager.list_reservations_by_date("2019-3-19")
+
+        expect(reservations.length).must_equal 1
+      end
     end
   end
 end
