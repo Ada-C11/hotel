79213a46712aa2e12712b7c4403b508e23925563
Jansen <v.jansen.martin@gmail.com>
2019-03-11 14:33:25 -0700

    Clean up tests with a before block

--- spec/manager_spec.rb
+++ spec/manager_spec.rb
@@ -5,19 +5,19 @@ require_relative "spec_helper"
 require_relative "../lib/manager.rb"
 
 describe "Manager class" do
+  before do
+    @manager = Hotel::Manager.new
+  end
   it "creates an instance of Manager" do
-    expect(Hotel::Manager.new).must_be_kind_of Hotel::Manager
+    expect(@manager).must_be_kind_of Hotel::Manager
   end
 
   it "includes an array of rooms" do
-    manager = Hotel::Manager.new
-
-    expect(manager.rooms).must_be_kind_of Array
+    expect(@manager.rooms).must_be_kind_of Array
   end
 
   it "contains Room objects inside the rooms array" do
-    manager = Hotel::Manager.new
-    room = manager.rooms[0]
+    room = @manager.rooms[0]
 
     expect(room).must_be_kind_of Hotel::Room
   end
