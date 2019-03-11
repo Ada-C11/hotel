fc148d5864a5af5def2c633ceac87ebfa4e888a5
Angela <angela.ohh@gmail.com>
2019-03-11 15:13:25 -0700

    added duration method in Reservation class and created passing corresponding tests

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -1,5 +1,9 @@
+require "date"
+require "pry"
+
 class Reservation
-  attr_reader :reservation_id, :check_in, :check_out
+  attr_reader :reservation_id
+  attr_accessor :check_in, :check_out
 
   def initialize(reservation_id, check_in: nil, check_out: nil)
     @reservation_id = reservation_id
@@ -7,9 +11,23 @@ class Reservation
     @check_out = check_out
   end
 
-  def cost
+  def duration
+    if self.check_in != nil && self.check_out != nil
+      duration = (Date.parse(check_out) - Date.parse(check_in)).to_i
+      if duration <= 0
+        raise ArgumentError, "Check out time is not after check in time. Duration is currently #{duration} days."
+      end
+      #   binding.pry
+    else
+      return nil
+    end
+    return duration
+    #check validity
+    # if duration <= 0
+    #   raise ArguementError, "Check out time is not after check in time. Duration is currently #{duration} days."
+    # end
   end
 
-  def duration
+  def cost
   end
 end
--- spec/reservation__spec.rb
+++ spec/reservation__spec.rb
@@ -8,15 +8,34 @@ describe "Reservation class initialization" do
 
   it "check in date is nil unless values are given" do
     test_reserve = Reservation.new(1)
-    expect(test_reserve.check_in).must_equal nil
+    expect(test_reserve.check_in).must_be_kind_of NilClass
   end
 
   it "check out date is nil unless values are given" do
     test_reserve = Reservation.new(1)
-    expect(test_reserve.check_out).must_equal nil
+    expect(test_reserve.check_out).must_be_kind_of NilClass
+  end
+end
+
+describe "calculate duration of total days spent in reservation" do
+  it "returns accurate duration of stay given check in and check out" do
+    test_reserve = Reservation.new(1, check_in: "2019-3-15", check_out: "2019-3-20")
+
+    expect(test_reserve.duration).must_equal 5
+  end
+
+  it "throws an Argument Error if check out time is equal to or before check in time" do
+    test_reserve = Reservation.new(1, check_in: "2019-3-20", check_out: "2019-3-15")
+    other_test_reserve = Reservation.new(1, check_in: "2019-3-20", check_out: "2019-3-20")
+
+    expect { test_reserve.duration }.must_raise ArgumentError
   end
 end
 
 # describe "calculate total cost of reservation" do
-#     it
+#   it "will return accurate total cost of reservation given number of days" do
+#     check_in =
+#     check_out =
+#     test_reserve = Reservation.new()
+#   end
 # end
