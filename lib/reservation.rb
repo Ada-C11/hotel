d471c3db1324cf420c23b8fe570ee176909ba2ad
Margaret Finnan <msfinnan@gmail.com>
2019-03-11 13:49:52 -0700

    added tests for Reservation Class initialization

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -1,6 +1,8 @@
 # require_relative "spec_helper"
 
 class Reservation
+  attr_reader :reservation_id, :start_date, :end_date
+
   def initialize(reservation_id: 0, start_date: nil, end_date: nil)
     @reservation_id = reservation_id
     @start_date = start_date
--- spec/reservation_spec.rb
+++ spec/reservation_spec.rb
@@ -4,4 +4,16 @@ describe "Reservation class" do
     reservation = Reservation.new
     expect(reservation).must_be_instance_of Reservation
   end
+  it "Reservation class initialized with nil start  date if no date is given" do
+    reservation = Reservation.new
+    expect(reservation.start_date).must_equal nil
+  end
+  it "Reservation class initialized with nil  end date if no date is given" do
+    reservation = Reservation.new
+    expect(reservation.end_date).must_equal nil
+  end
+  it "Reservation id initialized as 0 if no reservation id is entered" do
+    reservation = Reservation.new
+    expect(reservation.reservation_id).must_equal 0
+  end
 end
