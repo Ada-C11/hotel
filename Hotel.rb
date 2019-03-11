6b9df7e8fd31632c82464f98f97d0fd43f42ee1b
Tatiana Quintana <tatsqui@gmail.com>
2019-03-11 10:46:32 -0700

    created Hotel module file and added relative spec_helper

---
+++ Hotel.rb
@@ -0,0 +1,4 @@
+module Hotel; end
+
+# require relative files here for: 
+# require_relative 'lib/Room'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,9 @@ end
 require 'minitest'
 require 'minitest/autorun'
 require 'minitest/reporters'
+require 'minitest/skip_dsl'
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 # Require_relative your lib files here!
+require_relative 'Hotel'
