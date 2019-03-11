55759ac6ed45fd6f392f41fa35b958e45a97e8a5
Tatiana Quintana <tatsqui@gmail.com>
2019-03-11 11:35:07 -0700

    creating spec files for booker, room, reservation

--- Hotel.rb
+++ Hotel.rb
@@ -1,4 +1,6 @@
 module Hotel; end
 
 # require relative files here for: 
-# require_relative 'lib/Room'
+require_relative 'lib/Room'
+require_relative 'lib/Reservation'
+require_relative 'lib/Booker'
