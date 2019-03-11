83f5371e58082e5471486284586e970271390a21
Evelynn Kaplan <evelynnkaplan@gmail.com>
2019-03-11 14:29:48 -0700

    made valid_room_number a class method instead of instance method

--- lib/room.rb
+++ lib/room.rb
@@ -5,11 +5,12 @@ module HotelSystem
         attr_accessor :cost_per_night
 
         def initialize(number)
+            
             @room_number = valid_room_number(number)
             @cost_per_night = 200
         end
 
-        def valid_room_number(number)
+        def self.valid_room_number(number)
             if number.class != Integer || number <= 0
                 raise ArgumentError, "Please enter a whole number greater than 0."
             else
