9a9abee28096799faf9d8b742caab03193dd644a
Jansen <v.jansen.martin@gmail.com>
2019-03-11 14:31:51 -0700

    Clean up tests with a before block

--- spec/room_spec.rb
+++ spec/room_spec.rb
@@ -2,16 +2,16 @@ require "simplecov"
 SimpleCov.start
 
 require_relative "spec_helper"
-#require_relative "../lib/room.rb"
 
 describe "Room class" do
+  before do
+    @room = Hotel::Room.new(room_number: 1)
+  end
   it "creates an instance of Room" do
-    expect(Hotel::Room.new(room_number: 1)).must_be_kind_of Hotel::Room
+    expect(@room).must_be_kind_of Hotel::Room
   end
 
   it "includes an array when initialized" do
-    room = Hotel::Room.new(room_number: 1)
-
-    expect(room.reservations).must_be_kind_of Array
+    expect(@room.reservations).must_be_kind_of Array
   end
 end
