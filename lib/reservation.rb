2c3367e130581c5a30e27987721cf04dc2b5dc7a
Evelynn Kaplan <evelynnkaplan@gmail.com>
2019-03-11 14:31:17 -0700

    add initialization method, valid_room method and valid_room_number methods

---
+++ lib/reservation.rb
@@ -0,0 +1,30 @@
+require_relative "room"
+
+module HotelSystem
+  class Reservation
+    attr_reader :room, :room_number
+    
+    def initialize(room: nil, room_number: nil)
+      if room
+        @room = valid_room(room)
+        @room_number = room.room_number
+      elsif room_number
+        @room_number = valid_room_number(room_number)
+      else
+        raise ArgumentError, "Please enter either a Room or a room number."
+      end
+    end
+
+    def valid_room(room)
+      if room.class != room
+        raise ArgumentError, "Please enter a Room."
+      else
+        return room
+      end
+    end
+
+    def valid_room_number(number)
+      HotelSystem::Room.valid_room_number(number)
+    end
+  end
+end
