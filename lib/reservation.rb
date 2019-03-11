8bd5217346b0a760899c2bcd2cfe9c8950a7d5a0
Erica Norris <norrise120@gmail.com>
2019-03-11 14:08:38 -0700

    Created test for Reservation instantiation and calculate_cost method, wrote code to pass

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -1,1 +1,14 @@
+module Hotel
+  class Reservation
+    def initialize(room, start_date, end_date)
+      @room = room
+      @start_date = Date.parse(start_date)
+      @end_date = Date.parse(end_date)
+    end
 
+    def calculate_cost
+      total_cost = ((@end_date - @start_date) - 1) * @room.cost_per_night
+      return total_cost
+    end
+  end
+end
--- lib/room.rb
+++ lib/room.rb
@@ -1,6 +1,8 @@
 module Hotel
   class Room
-    def initialize(room_number, cost_per_night: $200)
+    attr_reader :cost_per_night
+
+    def initialize(room_number, cost_per_night: 200)
       @room_number = room_number
       @cost_per_night = cost_per_night
     end
