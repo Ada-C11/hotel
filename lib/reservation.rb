63055c39cc09db6b41760e68527e922a2ff103b4
Margaret Finnan <msfinnan@gmail.com>
2019-03-11 13:42:31 -0700

    added initialize to Reservation Class

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -1,4 +1,9 @@
 # require_relative "spec_helper"
 
 class Reservation
+  def initialize(reservation_id: 0, start_date: nil, end_date: nil)
+    @reservation_id = reservation_id
+    @start_date = start_date
+    @end_date = end_date
+  end
 end
