b790c5a5b3f69ed30c8c7818ff5d9a248335d22a
Heather Izumi <hkizumi@gmail.com>
2019-03-11 15:08:50 -0700

    Added SimpleCov and lib files

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,8 @@
 # Add simplecov
+require "simplecov"
+SimpleCov.start do
+  add_filter %r{^/specs/}
+end
 require "minitest"
 require "minitest/autorun"
 require "minitest/reporters"
@@ -7,3 +11,5 @@ Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 # Require_relative your lib files here!
 require_relative "../lib/reservation_manager.rb"
+require_relative "../lib/reservation.rb"
+require_relative "../lib/room.rb"
