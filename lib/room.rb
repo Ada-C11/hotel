a323cce705d7db4ee5d93e6f9724df137054a3e9
Jansen <v.jansen.martin@gmail.com>
2019-03-11 14:01:32 -0700

    Add 'room_number' and 'reservations' attributes. Verify 'reservations' is an array.

--- lib/room.rb
+++ lib/room.rb
@@ -4,9 +4,11 @@
 
 module Hotel
   class Room
-    attr_reader
+    attr_reader :room_number, :reservations
 
-    def initialize
+    def initialize(room_number:, reservations: nil)
+      @room_number = room_number
+      @reservations = reservations || []
     end
   end
 end
--- spec/room_spec.rb
+++ spec/room_spec.rb
@@ -6,6 +6,12 @@ require_relative "../lib/room.rb"
 
 describe "Room class" do
   it "creates an instance of Room" do
-    expect(Hotel::Room.new).must_be_kind_of Hotel::Room
+    expect(Hotel::Room.new(room_number: 1)).must_be_kind_of Hotel::Room
+  end
+
+  it "includes an array when initialized" do
+    room = Hotel::Room.new(room_number: 1)
+
+    expect(room.reservations).must_be_kind_of Array
   end
 end
