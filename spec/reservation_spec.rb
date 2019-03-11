d96daa47ff7562f22d6b5e2e944c6d1880473a5b
qqdipps <sdipp007@gmail.com>
2019-03-11 15:03:12 -0700

    added test for checking if dates are valid

--- spec/reservation_spec.rb
+++ spec/reservation_spec.rb
@@ -1,10 +1,10 @@
 require_relative "spec_helper"
 
 describe "Reservation class" do
+  let(:valid_reservation) {
+    Hotel::Reservation.new(start_date: "March 20, 2020", end_date: "March 27, 2020")
+  }
   describe "Reservation#initialize" do
-    let(:valid_reservation) {
-      Hotel::Reservation.new(start_date: "March 20, 2020", end_date: "March 27, 2020")
-    }
     it "is an instance of Reservation" do
       expect(valid_reservation).must_be_instance_of Hotel::Reservation
     end
@@ -19,6 +19,18 @@ describe "Reservation class" do
       expect(valid_reservation).must_respond_to :end_date
       expect(valid_reservation.end_date).must_be_instance_of Date
       expect(valid_reservation.end_date).must_equal Date.new(2020, 03, 27)
+    end
+  end
+
+  let(:valid_start_date) {
+    valid_reservation.start_date
+  }
+  let(:valid_end_date) {
+    valid_reservation.end_date
+  }
+  describe "dates are valid" do
+    it "start_date is before end_date" do
+      expect(valid_date_range?(valid_start_date, valid_end_date)).must_equal true
     end
   end
 end
