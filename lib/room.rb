ecf47a28ad3f784c85ea7401446bfb660f14875a
Evelynn Kaplan <evelynnkaplan@gmail.com>
2019-03-11 14:54:54 -0700

    Fixed initialize to consider valid_room_number as a class method instead of instance method

--- lib/room.rb
+++ lib/room.rb
@@ -1,22 +1,19 @@
 module HotelSystem
-    
-    class Room
-        attr_reader :room_number
-        attr_accessor :cost_per_night
+  class Room
+    attr_reader :room_number
+    attr_accessor :cost_per_night
 
-        def initialize(number)
-            
-            @room_number = valid_room_number(number)
-            @cost_per_night = 200
-        end
-
-        def self.valid_room_number(number)
-            if number.class != Integer || number <= 0
-                raise ArgumentError, "Please enter a whole number greater than 0."
-            else
-                return number
-            end
-        end
-    
+    def initialize(number)
+      @room_number = self.class.valid_room_number(number)
+      @cost_per_night = 200
     end
+
+    def self.valid_room_number(number)
+      if number.class != Integer || number <= 0
+        raise ArgumentError, "Please enter a whole number greater than 0."
+      else
+        return number
+      end
+    end
+  end
 end
