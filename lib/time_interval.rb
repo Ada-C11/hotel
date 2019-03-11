d0fc1b4d96fdfc32a716792b72475ba85535ce19
Elise Pham <elise.pham88@gmail.com>
2019-03-11 16:28:10 -0700

    clean up time_interval class and tests

--- lib/time_interval.rb
+++ lib/time_interval.rb
@@ -2,20 +2,28 @@ require 'time'
 
 class Time_Interval
   attr_reader :check_in, :check_out
-  DATE_FORMAT = "%Y-%m-%d"
 
+  # Everytime we initalize an object interval, we need to use begin/rescue block
   def initialize(check_in, check_out)
     if check_in !~ /\d{4}-\d{2}-\d{2}/ || check_out !~ /\d{4}-\d{2}-\d{2}/
       raise ArgumentError.new("Invalid date format")
     end
 
+    if check_in > check_out
+      raise ArgumentError.new("Check out time cannot be greater than check in time")
+    end
+
+    check_in = process_date(check_in)
+    check_out = process_date(check_out)
+
     @check_in = check_in
     @check_out = check_out
   end
 
-  def process_date(date, format)
+  private
+  def process_date(date, format="%Y-%m-%d")
     begin
-      return date = Date.strptime(date, DATE_FORMAT)
+      return Date.strptime(date)
     rescue ArgumentError => e
       puts e
     end
--- spec/time_interval_spec.rb
+++ spec/time_interval_spec.rb
@@ -1,17 +1,27 @@
 require 'spec_helper.rb'
 
-describe "initialize" do
-  it "returns an instance of time_interval object" do
-    check_in = "2019-03-24"
-    check_out = "2019-03-26"
-    expect(Time_Interval.new(check_in, check_out)).must_be_instance_of Time_Interval
-  end
+describe "time_interval class" do
+  describe "initialize" do
+    it "returns an instance of time_interval object" do
+      check_in = "2019-03-24"
+      check_out = "2019-03-26"
+      expect(Time_Interval.new(check_in, check_out)).must_be_instance_of Time_Interval
+    end
 
-  it "raises an argument error for wrong date format" do
-    check_in = "2019/03/24"
-    check_out = "2019/03/26"
-    expect {
-      Time_Interval.new(check_in, check_out)
-    }.must_raise ArgumentError
+    it "raises an argument error for wrong date format" do
+      check_in = "2019/03/24"
+      check_out = "2019/03/26"
+      expect {
+        Time_Interval.new(check_in, check_out)
+      }.must_raise ArgumentError
+    end
+
+    it "raises an argument error if check in time is after check out time" do
+      check_in = "2019/03/26"
+      check_out = "2019/03/24"
+      expect {
+        Time_Interval.new(check_in, check_out)
+      }.must_raise ArgumentError
+    end
   end
 end
