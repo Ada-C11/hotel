23c0c6497ca87b8f1a0c6c96481df2b6176ad5f0
sjscotton <sj.scotton@gmail.com>
2019-03-11 14:41:10 -0700

    Added Hotel#list_rooms and tests for it

--- lib/hotel.rb
+++ lib/hotel.rb
@@ -1,12 +1,19 @@
 module HotelSystem
   class Hotel
-    attr_reader :rooms, :reservations, :blocks
+    attr_reader :reservations, :blocks
+    attr_accessor :rooms
 
     def initialize(id:, rooms: [], reservations: [], blocks: [])
       @id = id
       @rooms = rooms
       @reservations = reservations
       @blocks = blocks
+    end
+
+    def list_rooms
+      room_list = @rooms.map { |room| room.id }
+      return nil if rooms.length == 0
+      return room_list
     end
   end
 end
--- spec/hotel_spec.rb
+++ spec/hotel_spec.rb
@@ -1,18 +1,41 @@
 require_relative "spec_helper"
 
 describe "Hotel" do
+  before do
+    @hotel = HotelSystem::Hotel.new(id: 1)
+  end
   describe "initialize" do
-    before do
-      @hotel = HotelSystem::Hotel.new(id: 1)
-    end
     it "Creates an instance of a Hotel" do
       expect(@hotel).must_be_kind_of HotelSystem::Hotel
     end
 
     it "Creates a hotel with default parameters" do
-      expect(@hotel.reservations).must_equal []
-      expect(@hotel.rooms).must_equal []
-      expect(@hotel.blocks).must_equal []
+      expect(@hotel.reservations).must_be_kind_of Array
+      expect(@hotel.rooms).must_be_kind_of Array
+      expect(@hotel.blocks).must_be_kind_of Array
+    end
+  end
+
+  describe "#list_rooms" do
+    before do
+      @new_room = HotelSystem::Room.new(id: 1)
+    end
+
+    it "returns nil if there are no rooms in rooms array" do
+      expect(@hotel.list_rooms).must_equal nil
+    end
+
+    it "Can access information about the rooms in the array of rooms" do
+      @hotel.rooms << @new_room
+      expect(@hotel.rooms[-1].id).must_equal @new_room.id
+      expect(@hotel.rooms[-1]).must_be_kind_of HotelSystem::Room
+    end
+
+    it "returns an array listing the number of each room in the hotel" do
+      @hotel.rooms << @new_room
+      room_list = @hotel.list_rooms
+      expect(room_list).must_be_kind_of Array
+      expect(room_list[0]).must_equal 1
     end
   end
 end
