08286225fdbcd3c4ebe1fd39c83a01fd9bfe4b37
Erica Norris <norrise120@gmail.com>
2019-03-11 14:26:20 -0700

    Created tests to expect error if end date is same or before start date, wrote code to pass, updated Reservation constructor

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -1,7 +1,8 @@
 module Hotel
   class Reservation
-    def initialize(room, start_date, end_date)
+    def initialize(room:, start_date:, end_date:)
       @room = room
+      self.class.validate_dates(start_date, end_date)
       @start_date = Date.parse(start_date)
       @end_date = Date.parse(end_date)
     end
@@ -9,6 +10,12 @@ module Hotel
     def calculate_cost
       total_cost = ((@end_date - @start_date) - 1) * @room.cost_per_night
       return total_cost
+    end
+
+    def self.validate_dates(start_date, end_date)
+      unless end_date > start_date
+        raise ArgumentError, "End date must be after start date"
+      end
     end
   end
 end
