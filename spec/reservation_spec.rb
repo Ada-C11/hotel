f53bbcb7b4e25e0a39bcaf6ec6a9f3efaa846804
Jillianne Ramirez <jillianne.ramirez@gmail.com>
2019-03-11 13:13:23 -0700

    establishing file setup

---
+++ spec/reservation_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+describe "Reservation" do
+    it "works" do
+        expect(true).must_equal true
+
+    end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,5 @@
 # Add simplecov
+require 'date'
 require 'minitest'
 require 'minitest/autorun'
 require 'minitest/reporters'
@@ -6,3 +7,6 @@ require 'minitest/reporters'
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 # Require_relative your lib files here!
+require_relative '../lib/hotel'
+require_relative '../lib/room'
+require_relative '../lib/reservation'
