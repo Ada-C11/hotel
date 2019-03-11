29af9d749802551e3d1b09463dd423fd0ace0cfc
Kate Sandler <kate@katekatekate.net>
2019-03-11 15:34:59 -0700

    datespans initialize

--- lib/datespans.rb
+++ lib/datespans.rb
@@ -1,15 +1,23 @@
 require 'date'
 module Hotel
-class DateSpan
-  module Error
-    class DatePast < StandardError; end
-    class RangeInvalid < Standard; end
-  end
-  def initialize_clone(check_in, check_out)
+
+  class DateSpan
+
+    module Error
+      class DatePast < StandardError; end
+      class RangeInvalid < Standard; end
+    end
+
+  attr_reader :check_in, :check_out
+
+  def initialize(check_in, check_out)
 
     @check_in = Date.parse(check_in)
     @check_out = Date.parse(check_out)
 
+    raise Error::DatePast unless @check_in > Date.today
+    raise Error::RangeInvalid if @check_in > @check_out
+    end
   end
 end
 end
