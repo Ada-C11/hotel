7b7ffd4ae98e8c4836d971ca5d1adf4f1e278e96
Erica Norris <norrise120@gmail.com>
2019-03-11 12:27:52 -0700

    Updated spec_helper.rb to include room.rb and reservation.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,10 @@
 # Add simplecov
-require 'minitest'
-require 'minitest/autorun'
-require 'minitest/reporters'
+require "minitest"
+require "minitest/autorun"
+require "minitest/reporters"
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 # Require_relative your lib files here!
+require_relative "../lib/room.rb"
+require_relative "../lib/reservation.rb"
