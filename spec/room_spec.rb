f05a638030b30c53c27ec38f26c0fa91463df99a
Kimberly-Fasbender <kimberly.fasbender@gmail.com>
2019-03-11 15:03:36 -0700

    refactored testing for Room class

--- spec/room_spec.rb
+++ spec/room_spec.rb
@@ -2,8 +2,15 @@ require_relative "spec_helper"
 
 describe "Room Class" do
   describe "Initialize" do
-    it "Creates an instance of a Room" do
-      expect(Room.new(number: 3)).must_be_kind_of Room
+    before do
+      @room = Room.new(number: 3)
+    end
+    it "Is an instance of Room" do
+      expect(@room).must_be_kind_of Room
+    end
+
+    it "Stores a room number" do
+      expect(@room.number).must_equal 3
     end
   end
 end
