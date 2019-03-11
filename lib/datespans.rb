b677bfc1775c32e87991c28a7209995ceded24cf
Kate Sandler <kate@katekatekate.net>
2019-03-11 15:22:58 -0700

    Exceptions for datespans

--- lib/datespans.rb
+++ lib/datespans.rb
@@ -1,9 +1,14 @@
 require 'date'
-
 module Hotel
 class DateSpan
+  module Error
+    class DatePast < StandardError; end
+    class RangeInvalid < Standard; end
+  end
+  def initialize_clone(check_in, check_out)
 
-  def available?
+    @check_in = Date.parse(check_in)
+    @check_out = Date.parse(check_out)
 
   end
 end
