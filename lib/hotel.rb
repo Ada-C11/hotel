e9bac71f27077620cf51ea5fc1a78a0d8f1c2c8a
Cyndi Lopez <cyndilopez6@gmail.com>
2019-03-11 15:44:25 -0700

    added to hotel class: reserve room method, find room object associated with room id, and update list of reservations; and tests associated with these three methods

--- lib/hotel.rb
+++ lib/hotel.rb
@@ -7,5 +7,19 @@ module Hotel
       @rooms = rooms
       @reservations ||= reservations
     end
+
+    def reserve_room(id, start_date, end_date, room_id)
+      # find room object with room ID
+      room = find_room(room_id)
+      return Reservation.new(id: id, start_date: start_date, end_date: end_date, room: room)
+    end
+
+    def find_room(room_ID)
+      @rooms.detect { |room| room.id == room_ID }
+    end
+
+    def add_reservation(reservation)
+      reservations << reservation
+    end
   end
 end
--- spec/hotel_spec.rb
+++ spec/hotel_spec.rb
@@ -1,19 +1,18 @@
 require_relative "spec_helper"
-
+require "Date"
 describe "Hotel class" do
-  describe "Hotel instantiation" do
-    before do
-      @rooms = []
-      20.times do |k|
-        @rooms << Hotel::Room.new(id: k)
-      end
-      @hotel = Hotel::Hotel.new(
-        id: 1,
-        rooms: @rooms,
-        reservations: [],
-      )
+  before do
+    @rooms = []
+    20.times do |k|
+      @rooms << Hotel::Room.new(id: k)
     end
-
+    @hotel = Hotel::Hotel.new(
+      id: 1,
+      rooms: @rooms,
+      reservations: [],
+    )
+  end
+  describe "Hotel instantiation" do
     it "is an instance of Hotel" do
       expect(@hotel).must_be_kind_of Hotel::Hotel
     end
@@ -28,5 +27,29 @@ describe "Hotel class" do
   end
 
   describe "Reserve room method" do
+    it "can create a reservation" do
+      #year, month, day
+      reservation_id = 333
+      new_reservation = @hotel.reserve_room(reservation_id, Date.new(2001, 3, 5), Date.new(2001, 3, 7), 0)
+      expect(new_reservation).must_be_kind_of Hotel::Reservation
+    end
+
+    it "adds the reservation to the Hotel's list of reservations" do
+      #year, month, day
+      reservation_id = 333
+      new_reservation = @hotel.reserve_room(reservation_id, Date.new(2001, 3, 5), Date.new(2001, 3, 7), 0)
+      @hotel.add_reservation(new_reservation)
+      expect(@hotel.reservations.length).must_equal 1
+    end
+  end
+
+  describe "Find room object that has certain room id" do
+    it "returns object associated with room id" do
+      expect(@hotel.find_room(0)).must_be_kind_of Hotel::Room
+    end
+
+    it "returns the correct room id" do
+      expect(@hotel.find_room(0).id).must_equal 0
+    end
   end
 end
