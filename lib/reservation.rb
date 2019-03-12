87e3004a33bc7986163115c335d3e4b750514f34
Heather Izumi <hkizumi@gmail.com>
2019-03-11 22:24:12 -0700

    Methods for total reservation cost & list all rooms. With tests.

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -1,12 +1,25 @@
 require "date"
 
 class Reservation
-  def initialize(check_in, check_out)
+  attr_reader :name, :room_number, :check_in, :check_out
+  ROOM_RATE = 200
+
+  def initialize(name, room_number, check_in, check_out)
+    @name = name
+    @room_number = (1..20).to_a
     @check_in = Date.parse(check_in)
     @check_out = Date.parse(check_out)
 
     if @check_out < @check_in
       raise ArgumentError, "Invalid dates, checkout date must be after checkin date."
     end
+  end
+
+  def reservation_cost
+    (check_out - check_in) * ROOM_RATE
+  end
+
+  def all_rooms
+    room_number
   end
 end
--- spec/reservation_spec.rb
+++ spec/reservation_spec.rb
@@ -2,12 +2,25 @@ require_relative "spec_helper"
 
 describe "Reservation instantiation" do
   it "is an instance of Reservation " do
-    trip = Reservation.new("dec 15, 2019", "dec 20, 2019")
+    trip = Reservation.new("Kathy", 5, "dec 15, 2019", "dec 20, 2019")
     expect(trip).must_be_kind_of Reservation
   end
 
   it "raises an argument error if the checkout date is before the check in date" do
-    trip = Reservation.new("dec 20, 2019", "dec 15, 2019")
-    expect(trip).must_raise ArgumentError
+    expect { Reservation.new("Quinn", 5, "dec 20, 2019", "dec 15, 2019") }.must_raise ArgumentError
+  end
+end
+
+describe "total_cost method" do
+  it "Calculates the total cost of the reservation" do
+    trip = Reservation.new("Merrick", 5, "dec 15, 2019", "dec 20, 2019")
+    expect(trip.reservation_cost).must_equal 1000
+  end
+
+  describe "all_rooms method" do
+    it "Lists all rooms in the hotel" do
+      trip = Reservation.new("Chris", 5, "dec 15, 2019", "dec 20, 2019")
+      expect(trip.all_rooms.length).must_equal 20
+    end
   end
 end
