fd052db8a9e4bac3378105bd846ea615b519f162
ranuseh <rosalyn.anuseh@gmail.com>
2019-03-11 17:31:17 -0700

    hotel specs for adding rooms, storing rooms, finding and listing rooms completed

--- lib/hotel.rb
+++ lib/hotel.rb
@@ -4,31 +4,35 @@ class Hotel
 
   attr_reader :hotel_name, :rooms
 
+  # Is initialized with the name of the hotel
   def initialize(hotel_name)
     @hotel_name = hotel_name
     @rooms = []
   end
 
-  def add_room (room)
+  # Can add an instance of room to the array of rooms
+  def add_room(room)
     @rooms.push(room)
   end
 
 end
 
-#   def list_rooms
-#     counter = 1
-#     list_rooms = []
-#     list_rooms.push("hotel_name")
-#     @rooms.each do |room|
-#       list_rooms.push("#{counter}: #{room.name}")
-#       counter += 1
-#     end
-#     return list_rooms.join("\n")
-#   end
+  # Can list all the rooms
+  def list_rooms
+    counter = 1
+    list_rooms = []
+    list_rooms.push("hotel_name")
+    @rooms.each do |room|
+      list_rooms.push("#{counter}: #{room.number}")
+      counter += 1
+    end
+    return list_rooms.join("\n")
+  end
 
-#   def find_room_by_number(room_number)
-#     room = @rooms.find {|room| room.number == room_number}
-#     return room
-#   end
+  # Can find specific room if provided the room number
+  def find_room_by_number(room_number)
+    room = @rooms.find {|room| room.number == room_number}
+    return room
+  end
 
 # end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,3 +6,4 @@ require 'minitest/reporters'
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 require_relative '../lib/hotel'
+require_relative '../lib/room'
