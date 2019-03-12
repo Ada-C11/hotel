b1b31499e99595154abebaf80eac9c13fa235520
Tatiana Quintana <tatsqui@gmail.com>
2019-03-11 17:25:14 -0700

    writing more tests for booker class to test valid dates

--- lib/booker.rb
+++ lib/booker.rb
@@ -1,26 +1,39 @@
 require "csv"
-require "date"
+require "chronic"
 require 'time'
+require 'date'
 
 require_relative "room"
 require_relative "reserve"
 
 
 module Hotel
-  class Bookings
-    attr_reader :rooms, :reservations
-
-    def initialize(directory: "./support")
-      # @rooms = Room.load_all(directory: directory)
-      # @reservations = Reserve.load_all(directory: directory)
-     
-      # completed_reservations
+  class Booking
+    attr_reader :check_in, :check_out, :number_of_rooms
+    
+    def initialize(check_in: nil, check_out: nil, number_of_rooms: nil)
+      check_in = Chronic.parse(check_in)
+      check_out = Chronic.parse(check_out)
+   
+      @check_in = check_in
+      @check_out = check_out
+      @number_of_rooms = number_of_rooms
     end
 
-    def completed_reservations
-     # @reservations is an array of all reservations made
+    def verify_date(date) #verify that date entered is a real date
+      unless Date.valid_date?(date)
+        raise ArgumentError, "Invalid date, given #{date}"
+      end
     end
 
-  
+    def make_reservation do
+      #Using the date range given it creates a reservation and books rooms and creates an instance of Reserve. 
+      #This method will eventually have to find available rooms.
+      #What happens if no rooms are available?
+    end
+
   end
 end
+
+# check_in = DateTime.strptime(check_in, '%m-%d-%Y')
+# check_out = DateTime.strptime(check_out, '%m-%d-%Y')
--- spec/booker_spec.rb
+++ spec/booker_spec.rb
@@ -1,17 +1,62 @@
-# require_relative 'spec_helper'
+require_relative 'spec_helper'
+require 'date'
 
-# describe "Bookings" do 
-  # before do 
-  #   @booking = Hotel::Booking.new()
-  # end
-  # describe "initialize" do 
-  #   it "is an instance of a booking" do 
-  #     booking = Hotel::Booker.new()
-  #     expect(booking).must_be_kind_of Hotel::Booker
-  #   end
+describe "Booking" do 
+  before do 
+    @booking = Hotel::Booking.new(
+      check_in: "March 3rd 2019", 
+      check_out: "03-05-2019",
+      number_of_rooms: 1)
+  end
+  describe "initialize" do 
+    it "is an instance of a booking" do 
+      
+      expect(@booking).must_be_kind_of Hotel::Booking
+    end
 
-  #   it "keeps track of a Booking id" do 
-  #     expect(@booking.id).must_respond_to :id
-  #   end
-  # end
-# end
+    it "must accurately record check-in date" do 
+      expect(@booking).must_respond_to :check_in
+      expect(@booking.check_in).must_equal  Chronic.parse("3rd of March 2019")
+    end
+
+
+    it "must accurately record check-out date" do 
+      expect(@booking).must_respond_to :check_out
+      expect(@booking.check_out).must_equal Chronic.parse("5th of March 2019")
+    end
+
+    it "raises an exception for a check-out date that occurs before check-in" do 
+      expect {
+        Hotel::Booking.new(
+          check_in: "March 5th 2019", 
+          check_out: "02-05-2019",
+          number_of_rooms: 1)
+        }.must_raise ArgumentError
+    end
+
+    it "raises an exceptions for an invalid check-in day" do 
+     expect{ Hotel::Booking.new(
+        check_in: "March 100th 2019", 
+        check_out: "03-05-2019",
+        number_of_rooms: 1)
+     }.must_raise ArgumentError
+    end
+
+    it "raises an exceptions for an invalid check-out day" do 
+      expect{ Hotel::Booking.new(
+         check_in: "March 10th 2019", 
+         check_out: "03-205-2019",
+         number_of_rooms: 1)
+      }.must_raise ArgumentError
+     end
+  end
+
+  describe "method for making a reservation" do 
+    it "can reserve a room" do 
+    end
+    it "can reserve multiple rooms" do
+    end
+    it "can calculate the price of a reservation" do 
+    end
+  end
+end
