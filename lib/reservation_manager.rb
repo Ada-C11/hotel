a94827a1c3a6d49e76c5738c73e85a8e9d32ea41
MyriamWD <myriam.waldend@gmail.com>
2019-03-12 16:02:38 -0700

    added code to make the test -raise an argument error when there are no reservations on date- pass

--- lib/reservation_manager.rb
+++ lib/reservation_manager.rb
@@ -19,22 +19,22 @@ module Hotel
       return new_reservation
     end
 
-    # def find_reservation(id:, start_date: nil, end_date: nil)
-    #   found = ""
-    #   criteria = [id, start_date, end_date]
-
-    #   if criteria.include?(id)
-    #     hey = reservations
-    #     found = hey.find(id)
-    #     return found
-    #   end
-    #   # when start_date.class == Time && end_date.class
-    #   # when start_date || end_date
-    #   # end
-
-    #   # @reservations.include?(id)
-    # end
+    def find_reservation(id: nil, date: nil)
+      if id.class == Integer
+        @reservations.find do |reservation|
+          if reservation.id == id
+            return reservation
+          end
+        end
+      elsif date.class == Time
+        @reservations.find do |reservation|
+          if date > reservation.start_date && date < reservation.end_date
+            return reservation
+          else
+            raise ArgumentError, "There are no reservations during that"
+          end
+        end
+      end
+    end
   end
 end
-
-#5:11 last changed not commited yet!!
