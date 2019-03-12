c0fc61c270f9f8b88dc6ef566675e674de5f4fd7
Tatiana Quintana <tatsqui@gmail.com>
2019-03-12 13:10:21 -0700

    adjust reserve to match date_range changes in date validation

--- lib/reserve.rb
+++ lib/reserve.rb
@@ -1,24 +1,50 @@
 require 'date'
+require_relative 'date_range'
 
-class Reservation
-  attr_reader :id, :start_date, :end_date, :room_booked
+class Reservation < DateRange
+  attr_reader :id, :start_date, :end_date, :room_booked, :total_cost
 
-  def initialize id:nil, start_date:, end_date:
+  def initialize id:nil, start_date:, end_date:, room_booked: nil
+    valid_id(id)
+    valid_date?(start_date)
+    valid_date?(end_date)
+    date_range_valid?(start_date, end_date)
+
     @id = id
-    @start_date = check_in
+    @start_date = start_date
     @end_date = end_date
     @room_booked = room_booked
-    @reservations = []
+    @total_cost = total_cost
   end
 
-  def add_reservation(reserve)
-    @reservations << reserve
+  def self.make_reservation(reservation)
+    @reservations = []
+    @reservations << reservation
   end
+
+  def self.all_rooms
+    return @Room.all_rooms
+  end
+
+  def valid_id(id)
+    unless id.instance_of?(Integer) && id > 0 && id <= 20
+      raise ArgumentError, "ID must be a positive number, given #{id}..."
+    end
+  end
+
+  def  valid_date?(date)
+    super
+  end
+
+  def date_range_valid?(check_in, check_out)
+    super
+  end
+
+  def total_cost
+  end
+  
 
   private
-  # def self.all
-  #   CSV.read().map do |line|
-  #     Reservation.new(line[0].to_i, line[1], line[2], line[3].to_i)
-  #   end
-  # end
+  
 end
+
--- spec/reserve_spec.rb
+++ spec/reserve_spec.rb
@@ -2,26 +2,31 @@ require_relative 'spec_helper'
 
 describe "Reserve" do 
   before do 
-    @reserve = Reservation.new(
+    @reserved = Reservation.new(
                       id: 1, 
-                      start_date: "March 10 2019",
-                      end_date: "March 15th 2019", 
+                      start_date: "2019-03-03",
+                      end_date: "2019-05-03", 
                       room_booked: 1)
   end
   describe "initialize" do
     it "knows reserve is an instance of Reservation" do 
-      expect(@reserve).must_be_kind_of Reservation
+      expect(@reserved).must_be_kind_of Reservation
     end
 
     it "knows the reservation id" do
-      expect(@reserve).must_respond_to :id
-      expect(@reserve.id).must_equal 1
+      expect(@reserved).must_respond_to :id
+      expect(@reserved.id).must_equal 1
     end
   end
 
-  describe "add_reservation" do 
+  describe "make_reservation" do 
     before do 
-      @reservations = Reservation.add_reservation
+      @reserved = Reservation.new(
+        id: 1, 
+        start_date: "March 10 2019",
+        end_date: "March 15th 2019", 
+        room_booked: 1)
+      @reservations = Reservation.make_reservation(@reserved)
     end
     it "returns and array of reservations" do 
       expect(@reservations).must_be_kind_of Array
