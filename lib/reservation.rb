7309a0166c24f988d37b1dd31f38887625893ff7
Jansen <v.jansen.martin@gmail.com>
2019-03-11 17:31:29 -0700

    Add method to generate id for Reservation

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -8,7 +8,7 @@ module Hotel
   class Reservation
     attr_reader :check_in, :check_out, :room_number, :total_cost, :id
 
-    def initialize(check_in:, check_out:, room_number: nil, total_cost: nil, id: nil)
+    def initialize(check_in:, check_out:, room_number: nil, total_cost: nil, id: rand(10 ** 3))
       @check_in = Date.parse(check_in)
       @check_out = Date.parse(check_out)
       @room_number = room_number
