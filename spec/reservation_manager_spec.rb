aaadbdcae88609f997e6c3af107d525fba07609a
Margaret Finnan <msfinnan@gmail.com>
2019-03-11 20:26:42 -0700

    Added let to reservation mananger spec

--- spec/reservation_manager_spec.rb
+++ spec/reservation_manager_spec.rb
@@ -1,27 +1,27 @@
 require_relative "spec_helper"
 
 describe "ReservationManager class" do
+  let (:reservation) do
+    ReservationManager.new
+  end
+
   describe "initialize" do
     it "Creates an instance of ReservationManager" do
-      reservation_manager = ReservationManager.new
-      expect(reservation_manager).must_be_instance_of ReservationManager
+      expect(reservation).must_be_instance_of ReservationManager
     end
   end
   describe "make_reservation method" do
     it "make_reservation method can make an instance of reservation" do
-      my_reservation = ReservationManager.new
-      expect(my_reservation.make_reservation).must_be_instance_of Reservation
+      expect(reservation.make_reservation).must_be_instance_of Reservation
     end
     it "make_reservation adds a reservation to an array of reservations" do
-      reservation_manager = ReservationManager.new
-      my_reservation = reservation_manager.make_reservation
-      expect(reservation_manager.reservations_array.include?(my_reservation)).must_equal true
+      my_reservation = reservation.make_reservation
+      expect(reservation.reservations_array.include?(my_reservation)).must_equal true
     end
   end
   describe "view_all_rooms method" do
     it "rooms array includes 20 rooms" do
-      reservation_manager = ReservationManager.new
-      expect(reservation_manager.view_all_rooms.length).must_equal 20
+      expect(reservation.view_all_rooms.length).must_equal 20
     end
   end
 end
