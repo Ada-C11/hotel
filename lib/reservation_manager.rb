0685d9564a486a5d41ccad159bd7d628e4bd379a
MyriamWD <myriam.waldend@gmail.com>
2019-03-12 15:12:48 -0700

    added code to ReservationManager class to pass new test

--- lib/reservation_manager.rb
+++ lib/reservation_manager.rb
@@ -1,22 +1,40 @@
 module Hotel
   class ReservationManager
+    attr_reader :reservations
+
     def initialize
       @reservations = []
     end
 
-    def reservations
-      reservations << new_reservation # Where does reservation come from
-    end
-
     def rooms
-      rooms = (1..20).to_a
-      return rooms
+      @rooms = (1..20).to_a
+      return @rooms
     end
 
-    def create_reservation(start_date, end_date)
+    def create_reservation(start_date, end_date) # should I parse the dates here??? not when I'm creating the reservation in the specs??
+      id = @reservations.length + 1
       room = rooms.sample
-      new_reservation = Reservation.new(start_date, end_date, room)
+      new_reservation = Reservation.new(id, start_date, end_date, room)
+      @reservations << new_reservation
       return new_reservation
     end
+
+    # def find_reservation(id:, start_date: nil, end_date: nil)
+    #   found = ""
+    #   criteria = [id, start_date, end_date]
+
+    #   if criteria.include?(id)
+    #     hey = reservations
+    #     found = hey.find(id)
+    #     return found
+    #   end
+    #   # when start_date.class == Time && end_date.class
+    #   # when start_date || end_date
+    #   # end
+
+    #   # @reservations.include?(id)
+    # end
   end
 end
+
+#5:11 last changed not commited yet!!
