262c731e168674ca11941595d7a12f3ba19f18dd
Erica Norris <norrise120@gmail.com>
2019-03-12 15:50:11 -0700

    Updated make_reservation method so that only available rooms can be reserved

--- lib/hotel_manager.rb
+++ lib/hotel_manager.rb
@@ -15,9 +15,14 @@ module Hotel
     end
 
     def make_reservation(room_number, start_date, end_date)
-      new_reservation = Hotel::Reservation.new(room: find_room_by_number(room_number), start_date: start_date, end_date: end_date)
-      @reservations.push(new_reservation)
-      return new_reservation
+      room = find_room_by_number(room_number)
+      if self.list_available_rooms(start_date, end_date).include?(room)
+        new_reservation = Hotel::Reservation.new(room: room, start_date: start_date, end_date: end_date)
+        @reservations.push(new_reservation)
+        return new_reservation
+      else
+        raise ArgumentError, "Room #{room_number} is unavailable. Must use an available room."
+      end
     end
 
     def list_reservations_by_date(date)
