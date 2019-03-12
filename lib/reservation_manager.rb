15e9cace98dada01b4fcccd1188c6cdfe29f139d
Kimberly-Fasbender <kimberly.fasbender@gmail.com>
2019-03-11 17:06:38 -0700

    added #request_reservation - reservation creation/array storage functionality

--- lib/reservation_manager.rb
+++ lib/reservation_manager.rb
@@ -6,6 +6,21 @@ class ReservationManager
     @reservations = []
   end
 
+  def request_reservation(check_in_date, check_out_date)
+    reservation = Reservation.new(
+      check_in_date: check_in_date,
+      check_out_date: check_out_date,
+      room_number: rand(1..20),
+    )
+
+    @reservations << reservation
+  end
+
+  # def add_reservation(reservation)
+  #   @reservations << reservation
+  # end
+
   # def find_by_date(date)
+  #   @reservations
   # end
 end
