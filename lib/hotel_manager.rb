81d9fe5673ff1407fc440ec3e47cdaab7826cc0c
Erica Norris <norrise120@gmail.com>
2019-03-12 14:17:19 -0700

    Created methods list_available_rooms and find_room_by_number methods and updated constructor and make_reservation to use new method

--- lib/hotel_manager.rb
+++ lib/hotel_manager.rb
@@ -9,13 +9,13 @@ module Hotel
 
     def initialize
       @rooms = ROOM_NUMBERS.map do |number|
-        Hotel::Room.new(room: number)
+        Hotel::Room.new(number)
       end
       @reservations = []
     end
 
-    def make_reservation(room, start_date, end_date)
-      new_reservation = Hotel::Reservation.new(room: room, start_date: start_date, end_date: end_date)
+    def make_reservation(room_number, start_date, end_date)
+      new_reservation = Hotel::Reservation.new(room: find_room_by_number(room_number), start_date: start_date, end_date: end_date)
       @reservations.push(new_reservation)
       return new_reservation
     end
@@ -29,6 +29,24 @@ module Hotel
         end
       end
       return found_reservations
+    end
+
+    def list_available_rooms(start_date, end_date)
+      start_date = Date.parse(start_date)
+      end_date = Date.parse(end_date)
+      available_rooms = @rooms
+      reservations.each do |reservation|
+        if reservation.start_date >= start_date && reservation.end_date <= end_date
+          available_rooms.delete(reservation.room)
+        end
+      end
+      return available_rooms
+    end
+
+    def find_room_by_number(num)
+      @rooms.each do |room|
+        return room if room.room_number == num
+      end
     end
   end
 end
