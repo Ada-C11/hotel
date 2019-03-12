2ece6b29bdde8d9b73acb3b2543e8ecb6983099f
MyriamWD <myriam.waldend@gmail.com>
2019-03-12 16:03:34 -0700

    added test -raise ArgumentError when no reservations on date-

--- spec/reservation_manager_spec.rb
+++ spec/reservation_manager_spec.rb
@@ -6,17 +6,17 @@ describe "ReservationManager" do
   end
   let(:reservation_generator) do
     # Reservation 1
-    start_time = Time.parse("2019-03-11 14:08:45 -0700")
-    end_time = Time.parse("2019-03-15 14:08:45 -0700")
-    manager_new.create_reservation(start_time, end_time)
+    start_date = Time.parse("2019-03-11 14:08:45 -0700")
+    end_date = Time.parse("2019-03-15 14:08:45 -0700")
+    manager_new.create_reservation(start_date, end_date)
     # Reservation 2
-    start_time = Time.parse("2019-03-20 14:08:45 -0700")
-    end_time = Time.parse("2019-03-22 14:08:45 -0700")
-    manager_new.create_reservation(start_time, end_time)
+    start_date = Time.parse("2019-03-20 14:08:45 -0700")
+    end_date = Time.parse("2019-03-22 14:08:45 -0700")
+    manager_new.create_reservation(start_date, end_date)
     # Reservation 3
-    start_time = Time.parse("2019-02-28 14:08:45 -0700")
-    end_time = Time.parse("2019-03-15 14:08:45 -0700")
-    manager_new.create_reservation(start_time, end_time)
+    start_date = Time.parse("2019-02-28 14:08:45 -0700")
+    end_date = Time.parse("2019-03-15 14:08:45 -0700")
+    manager_new.create_reservation(start_date, end_date)
   end
   it "creates a reservation" do
     # should I instantiate Reservation manager?? I think I just need to create Reservation objects and don't initialize the RM class
@@ -34,14 +34,27 @@ describe "ReservationManager" do
     expect(manager_new.reservations.length).must_equal 3
   end
 
-  xit "finds a reservation" do
-    #reservations = manager_new.new ## I don't know how to access the instance yet
-
+  it "finds a reservation by id" do
     reservation_generator
 
     expect(manager_new.find_reservation(id: 1)).must_be_kind_of Hotel::Reservation
+    expect(manager_new.find_reservation(id: 1).start_date).must_equal Time.parse("2019-03-11 14:08:45 -0700")
+    expect(manager_new.find_reservation(id: 3)).must_be_kind_of Hotel::Reservation
+    expect(manager_new.find_reservation(id: 3).start_date).must_equal Time.parse("2019-02-28 14:08:45 -0700")
   end
-  xit "returns the cost for a reservation" do
+
+  it "finds a reservation by date" do
+    reservation_generator
+
+    expect(manager_new.find_reservation(date: Time.parse("2019-03-12 14:08:45 -0700"))).must_be_kind_of Hotel::Reservation
+  end
+
+  it "raises ArgumentError when there are no reservations for date" do
+    reservation_generator
+    expect { manager_new.find_reservation(date: Time.parse("2011-03-12 14:08:45 -0700")) }.must_raise ArgumentError
+  end
+
+  xit "returns the cost by a reservation" do
     reservations = manager_new
     reservation_test = reservations.find_reservation("id")
     expect(reservation_test.cost).must_equal ## A value I need to specify by creating a reservation or something??
