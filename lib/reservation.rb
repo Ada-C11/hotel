1a830478a2b84fce3e454fa84af80c4b9038b586
Heather Izumi <hkizumi@gmail.com>
2019-03-12 16:06:13 -0700

    Fixed initiator parameters

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -1,25 +1,22 @@
 require "date"
 
-class Reservation
-  attr_reader :name, :room_number, :check_in, :check_out
-  ROOM_RATE = 200
+module Hotel
+  class Reservation
+    attr_reader :room_number, :check_in, :check_out
+    ROOM_RATE = 200
 
-  def initialize(name, room_number, check_in, check_out)
-    @name = name
-    @room_number = (1..20).to_a
-    @check_in = Date.parse(check_in)
-    @check_out = Date.parse(check_out)
+    def initialize(room_number, check_in, check_out)
+      @room_number = room_number
+      @check_in = Date.parse(check_in)
+      @check_out = Date.parse(check_out)
 
-    if @check_out < @check_in
-      raise ArgumentError, "Invalid dates, checkout date must be after checkin date."
+      if @check_out < @check_in
+        raise ArgumentError, "Invalid dates, checkout date must be after checkin date."
+      end
     end
-  end
 
-  def reservation_cost
-    (check_out - check_in) * ROOM_RATE
-  end
-
-  def all_rooms
-    room_number
+    def reservation_cost
+      (check_out - check_in) * ROOM_RATE
+    end
   end
 end
--- lib/reservation_manager.rb
+++ lib/reservation_manager.rb
@@ -1,7 +1,36 @@
+require_relative "reservation.rb"
+require "date"
 
+module Hotel
+  class ReservationManager
+    def initialize
+      @reservations = []
+    end
 
-class ReservationManager
-  def initialize(guest_name)
-    @guest_name = guest_name
+    def all_rooms
+      rooms = (1..20).to_a
+      return rooms
+    end
+
+    def make_reservation(room_number, check_in, check_out)
+      new_reservation = Hotel::Reservation.new(room_number,
+                                               check_in,
+                                               check_out)
+
+      @reservations << new_reservation
+
+      return new_reservation
+    end
+
+    def reservations_by_date(date)
+      reservations_by_date = []
+      @reservations.each do |reservation|
+        if date > reservation.check_in && date < reservation.check_out
+          #Right now checkout day is not included because you can check-in on that day
+          reservations_by_date << reservation
+        end
+      end
+      return reservations_by_date
+    end
   end
 end
