68ebed5372ecb0e685dd9345a18df001a10e5b8a
Evelynn Kaplan <evelynnkaplan@gmail.com>
2019-03-11 17:18:12 -0700

    Fixed a bug in valid_room class, the conditional had the class name in lowercase

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -6,10 +6,10 @@ module HotelSystem
     attr_accessor :dates
 
     def initialize(room: nil, room_number: nil, dates: nil)
-      if room
+      if room != nil
         @room = valid_room(room)
         @room_number = room.room_number
-      elsif room_number
+      elsif room_number != nil
         @room_number = valid_room_number(room_number)
       else
         raise ArgumentError, "Please enter either a Room or a room number."
@@ -18,7 +18,7 @@ module HotelSystem
     end
 
     def valid_room(room)
-      if room.class != room
+      if room.class != Room
         raise ArgumentError, "Please enter a Room."
       else
         return room
