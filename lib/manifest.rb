14609a066bbe65b7fc8b1913bd6ad6366da608a2
qqdipps <sdipp007@gmail.com>
2019-03-11 18:18:08 -0700

    added tests for manifest and created list_rooms method.

--- lib/manifest.rb
+++ lib/manifest.rb
@@ -1,15 +1,26 @@
-require_relative "../spec/spec_helper"
+# require_relative "../spec/spec_helper"
 
 module Hotel
   class Manifest
     attr_reader :rooms
     NUMBER_OF_ROOMS = 20
+    COST_OF_ROOM = 200
+    Room = Struct.new(:cost_per_day, :id)
 
     def initialize
-      # NUMBER_OF_ROOMS.times do
-      #   rooms << Room.new
-      # end
+      @rooms = {}
+      NUMBER_OF_ROOMS.times do |i|
+        id = i + 1
+        rooms[Room.new(COST_OF_ROOM, id)] = { dates_booked: [] }
+      end
+    end
 
+    def list_rooms
+      list = ""
+      rooms.each do |room, value|
+        list += "Room number #{room.id} \n"
+      end
+      return list
     end
   end
 end
--- spec/manifest_spec.rb
+++ spec/manifest_spec.rb
@@ -2,7 +2,32 @@ require_relative "spec_helper"
 
 describe "Manifest" do
   let(:manifest) { Hotel::Manifest.new }
-  it "is an instance of RoomManifest" do
-    expect(manifest).must_be_instance_of Hotel::Manifest
+  describe "Manifest#initialize" do
+    it "is an instance of Manifest" do
+      expect(manifest).must_be_instance_of Hotel::Manifest
+    end
+
+    let(:rooms) { manifest.rooms }
+    describe "Check attributes of Manifest" do
+      it "instance variable rooms is an array" do
+        expect(rooms).must_be_instance_of Hash
+      end
+
+      it "rooms contains 20 elements" do
+        expect(rooms.length).must_equal 20
+      end
+
+      it "each element is a struct with :cost_per_day and :id" do
+        rooms.each do |room, v|
+          expect(room).must_respond_to :cost_per_day
+          expect(room).must_respond_to :id
+        end
+      end
+    end
+  end
+  describe "Manifest#list" do
+    it "returns a string" do
+      expect(manifest.list_rooms).must_be_instance_of String
+    end
   end
 end
