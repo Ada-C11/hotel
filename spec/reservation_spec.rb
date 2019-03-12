392dfb75700d2a24708085088f83e0426ecd5f1f
MyriamWD <myriam.waldend@gmail.com>
2019-03-11 17:10:52 -0700

    eliminated extra puts for duration

--- spec/reservation_spec.rb
+++ spec/reservation_spec.rb
@@ -3,15 +3,17 @@ require_relative "spec_helper"
 
 describe "Reservation" do
   let(:reservation_1) do
+    id = 1
     start_time = Time.parse("2019-03-11 14:08:45 -0700")
     end_time = Time.parse("2019-03-15 14:08:45 -0700")
-    duration = end_time - start_time
-    puts duration.to_i
     room_number = 3
-    Hotel::Reservation.new(start_time, end_time, room_number)
+    Hotel::Reservation.new(id, start_time, end_time, room_number)
   end
   it "is an instance of Reservation" do
     expect(reservation_1).must_be_instance_of Hotel::Reservation
+  end
+  it "can generate an id for the reservation" do
+    expect(reservation_1.id).must_be_kind_of Integer
   end
   it "can calculate the amount of nights" do
     expect(reservation_1.total_nights).must_equal 4
