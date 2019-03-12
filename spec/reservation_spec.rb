9a3b05213fd8ae6f5f208134f60786e153606c3b
Margaret Finnan <msfinnan@gmail.com>
2019-03-11 20:20:13 -0700

    added let to test

--- spec/reservation_spec.rb
+++ spec/reservation_spec.rb
@@ -2,21 +2,21 @@ require_relative "spec_helper"
 require "date"
 
 describe "Reservation class" do
+  let (:reservation) do
+    Reservation.new
+  end
+
   describe "initialize" do
     it "Creates an instance of reservation" do
-      reservation = Reservation.new
       expect(reservation).must_be_instance_of Reservation
     end
     it "Reservation class initialized with nil start  date if no date is given" do
-      reservation = Reservation.new
       expect(reservation.start_date).must_equal nil
     end
     it "Reservation class initialized with nil  end date if no date is given" do
-      reservation = Reservation.new
       expect(reservation.end_date).must_equal nil
     end
     it "Reservation id initialized as 0 if no reservation id is entered" do
-      reservation = Reservation.new
       expect(reservation.reservation_id).must_equal 0
     end
   end
@@ -55,7 +55,6 @@ describe "Reservation class" do
   end
   describe "assign_room method" do
     it "assigns a room from @rooms array" do
-      reservation = Reservation.new
       room = reservation.assign_room
       expect(reservation.rooms.include?(room)).must_equal true
     end
