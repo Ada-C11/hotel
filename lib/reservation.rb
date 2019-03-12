c859a4e032fe4354a90dd547e531932ffd978d62
Cyndi Lopez <cyndilopez6@gmail.com>
2019-03-12 11:59:10 -0700

    tweaked total cost method and added code and test to raise an error for an invalid date range

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -6,7 +6,14 @@ module Hotel
       @id = id
       @start_date = start_date
       @end_date = end_date
+
+      raise ArgumentError if end_date <= start_date
+
       @room = room
+    end
+
+    def total_cost
+      return (end_date - start_date).to_i * room.cost
     end
   end
 end
--- spec/reservation_spec.rb
+++ spec/reservation_spec.rb
@@ -22,5 +22,54 @@ describe "Reservation class" do
     it "includes a room object as its room attribute" do
       expect(@reservation.room).must_be_kind_of Hotel::Room
     end
+
+    it "has start date that is instance of Date" do
+      @reservation.start_date.must_be_kind_of Date
+    end
+
+    it "has an end date that is instance of Date" do
+      @reservation.end_date.must_be_kind_of Date
+    end
+
+    it "raises an error for an invalid date range" do
+      @room = Hotel::Room.new(
+        id: 2,
+      )
+
+      expect {
+        @reservation = Hotel::Reservation.new(
+          id: 2,
+          start_date: Date.new(2001, 2, 7),
+          end_date: Date.new(2001, 2, 5),
+          room: @room,
+        )
+      }.must_raise ArgumentError
+    end
+  end
+
+  describe "Total cost" do
+    before do
+      @rooms = []
+      20.times do |k|
+        @rooms << Hotel::Room.new(id: k)
+      end
+      @hotel = Hotel::Hotel.new(
+        id: 1,
+        rooms: @rooms,
+        reservations: [],
+      )
+      @room = Hotel::Room.new(
+        id: 1,
+      )
+      @reservation = Hotel::Reservation.new(
+        id: 1,
+        start_date: Date.new(2001, 2, 3),
+        end_date: Date.new(2001, 2, 5),
+        room: @room,
+      )
+    end
+    it "calculates a reservation's total cost accurately" do
+      expect(@reservation.total_cost).must_equal 400
+    end
   end
 end
