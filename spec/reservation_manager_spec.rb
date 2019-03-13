71eb8b5f2dfc0273b2720d9a1ad14282c19c602b
MyriamWD <myriam.waldend@gmail.com>
2019-03-12 17:43:41 -0700

    I need to modify the test for find_reservation method

--- spec/reservation_manager_spec.rb
+++ spec/reservation_manager_spec.rb
@@ -14,8 +14,8 @@ describe "ReservationManager" do
     end_date = Time.parse("2019-03-22 14:08:45 -0700")
     manager_new.create_reservation(start_date, end_date)
     # Reservation 3
-    start_date = Time.parse("2019-02-28 14:08:45 -0700")
-    end_date = Time.parse("2019-03-15 14:08:45 -0700")
+    start_date = Time.parse("2019-02-27 14:08:45 -0700")
+    end_date = Time.parse("2019-02-28 14:08:45 -0700")
     manager_new.create_reservation(start_date, end_date)
   end
   it "creates a reservation" do
@@ -37,27 +37,32 @@ describe "ReservationManager" do
   it "finds a reservation by id" do
     reservation_generator
 
-    expect(manager_new.find_reservation(id: 1)).must_be_kind_of Hotel::Reservation
+    # expect(manager_new.find_reservation(id: 1)).must_be_kind_of Hotel::Reservation
     expect(manager_new.find_reservation(id: 1).start_date).must_equal Time.parse("2019-03-11 14:08:45 -0700")
-    expect(manager_new.find_reservation(id: 3)).must_be_kind_of Hotel::Reservation
-    expect(manager_new.find_reservation(id: 3).start_date).must_equal Time.parse("2019-02-28 14:08:45 -0700")
+    # expect(manager_new.find_reservation(id: 3)).must_be_kind_of Hotel::Reservation
+    expect(manager_new.find_reservation(id: 3).start_date).must_equal Time.parse("2019-02-27 14:08:45 -0700")
   end
 
   it "finds a reservation by date" do
     reservation_generator
 
-    expect(manager_new.find_reservation(date: Time.parse("2019-03-12 14:08:45 -0700"))).must_be_kind_of Hotel::Reservation
+    expect(manager_new.find_reservation(date: Time.parse("2019-03-13 14:08:45 -0700"))).must_be_kind_of Array  #Hotel::Reservation
+    # expect(manager_new.find_reservation(date: Time.parse("2019-03-21 14:08:45 -0700"))).must_be_kind_of Hotel::Reservation
+    # expect(manager_new.find_reservation(date: Time.parse("2019-02-27 14:08:45 -0700"))).must_be_kind_of Hotel::Reservation
   end
 
   it "raises ArgumentError when there are no reservations for date" do
-    reservation_generator
-    expect { manager_new.find_reservation(date: Time.parse("2011-03-12 14:08:45 -0700")) }.must_raise ArgumentError
+    reservation_generator ## Think about where to raise the argument error
+    expect { manager_new.find_reservation(date: Time.parse("2019-01-01 14:08:45 -0700")) }.must_raise ArgumentError
   end
 
   xit "returns the cost by a reservation" do
-    reservations = manager_new
-    reservation_test = reservations.find_reservation("id")
-    expect(reservation_test.cost).must_equal ## A value I need to specify by creating a reservation or something??
+    reservation_generator
+
+    # expect(manager_new.find_reservation(id: 1).total_cost).must_equal 800
+
+    # expect(manager_new.find_reservation(date: Time.parse("2019-03-12 14:08:45 -0700")).total_cost).must_equal 800
+    expect(manager_new.find_reservation(date: Time.parse("2019-03-20 14:08:45 -0700")).total_cost).must_equal 400 ## A value I need to specify by creating a reservation or something??
   end
   xit "filters reservation for dates" do
     reservations = manager_new
