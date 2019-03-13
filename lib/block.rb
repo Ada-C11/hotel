b603b959b0141fcf3604b6003a89b1acffb7c344
Cyndi Lopez <cyndilopez6@gmail.com>
2019-03-13 12:24:49 -0700

    initialized block class that creates a hotel block and tests for it

---
+++ lib/block.rb
@@ -0,0 +1,14 @@
+module Hotel
+  class Block
+    attr_reader :id, :start_date, :end_date, :collection_rooms, :discounted_rate
+
+    def initialize(id:, start_date:, end_date:, collection_rooms:, discounted_rate:)
+      #   super(id, start_date, end_date)
+      @id = id
+      @start_date = start_date
+      @end_date = end_date
+      @collection_rooms = collection_rooms
+      @discounted_rate = discounted_rate
+    end
+  end
+end
