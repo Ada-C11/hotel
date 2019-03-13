53d88d8503032f0ab8318ad61c1ce8fe3da5e098
Erica Norris <norrise120@gmail.com>
2019-03-13 11:23:06 -0700

    Created make_rooms method and corresponding tests in order to less dependencies between room and hotel manager

--- lib/room.rb
+++ lib/room.rb
@@ -1,10 +1,23 @@
 module Hotel
   class Room
+    ROOM_NUMBERS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
     attr_reader :room_number, :cost_per_night
 
     def initialize(room_number, cost_per_night: 200)
       @room_number = room_number
       @cost_per_night = cost_per_night
+    end
+
+    def self.make_rooms(numbers)
+      rooms = numbers.map do |number|
+        Hotel::Room.new(number)
+      end
+      return rooms
+    end
+
+    def self.make_rooms_standard
+      self.make_rooms(ROOM_NUMBERS)
     end
   end
 end
