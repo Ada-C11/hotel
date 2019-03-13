a748d3718880576b74b9e9c93558353f18a7bef4
Nidhi <nidhixpatel@gmail.com>
2019-03-12 17:44:10 -0700

    modified book_room to select obly available rooms, raise exception for no rooms

--- lib/booker.rb
+++ lib/booker.rb
@@ -22,7 +22,8 @@ module Hotel
         def book_room(start_date: ,end_date: )
             range = get_dates(start_date: start_date,end_date: end_date)
             new_reservation = create_reservation(id: reservations.length + 1,date_range: range, room: nil)
-            booked_room = find_room
+            booked_room = find_room(start_date: start_date, end_date: end_date)
+            raise ArgumentError, "There are no available rooms for these dates!" if booked_room == nil
             new_reservation.room = booked_room
             add_to_room(room: booked_room, reservation: new_reservation)
             add_to_reservation_list(new_reservation)
@@ -39,8 +40,20 @@ module Hotel
             return reservation
         end
 
-        def find_room
-            return rooms[0]
+        def find_room(start_date:, end_date: )
+            range = get_dates(start_date: start_date, end_date: end_date)
+            available_rooms_by_date = range.map{|date| available_rooms(date)}
+            potential_rooms = []
+            available_rooms_by_date.length.times do |i|
+                available_rooms_by_date[0].length.times do |j|
+                    if available_rooms_by_date.all?{|array| array.include?(available_rooms_by_date[0][j]) == true } == true
+                        potential_rooms.push(available_rooms_by_date[0][j])
+                    end
+                end
+            end
+
+            potential_rooms.length > 0 ? selected_room = potential_rooms[0] : selected_room = nil
+            return selected_room
         end
 
         def add_to_room(room: , reservation: )
@@ -55,6 +68,13 @@ module Hotel
             desired_date = Date.iso8601(date.to_s)
             list_by_date = reservations.select{|reservation| reservation.date_range.include?(desired_date) == true}
             return list_by_date
+        end
+
+        def available_rooms(date)
+            reservations_on_date = view_reservations(date)
+            rooms_of_reservations = reservations_on_date.map{|reservation| reservation.room}
+            list_available_rooms = rooms.reject{|room| rooms_of_reservations.include?(room) == true}
+            return list_available_rooms
         end
 
     end
