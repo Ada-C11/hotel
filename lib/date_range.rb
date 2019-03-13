d3128fb794f6aa43b4c5f50b4d6642f7c976f553
Jillianne Ramirez <jillianne.ramirez@gmail.com>
2019-03-13 11:00:17 -0700

    removed error in having instance variables

--- lib/date_range.rb
+++ lib/date_range.rb
@@ -2,7 +2,7 @@ module HotelManagementSystem
     class DateRange
         def self.is_valid?(start_date, end_date)
             is_valid = true
-            if @start_date.nil? || @end_date.nil? || !@end_date.nil? && @end_date < @start_date
+            if start_date.nil? || end_date.nil? || !end_date.nil? && end_date < start_date
                 is_valid = false
             end
             return is_valid
