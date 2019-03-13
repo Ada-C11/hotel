54ef67e6c95e1d0b665c97e88c5c9bf82979aca3
Jillianne Ramirez <jillianne.ramirez@gmail.com>
2019-03-13 10:59:40 -0700

    update relative files

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,6 +7,7 @@ require 'minitest/reporters'
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 # Require_relative your lib files here!
-require_relative '../lib/hotel'
+require_relative '../lib/hotel_manager'
 require_relative '../lib/room'
 require_relative '../lib/reservation'
+require_relative '../lib/date_range'
