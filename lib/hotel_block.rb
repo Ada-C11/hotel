e897c25e0f9a8a5b018e00af1364507f77f6b317
Angela <angela.ohh@gmail.com>
2019-03-13 16:46:51 -0700

    date_range

--- lib/hotel_block.rb
+++ lib/hotel_block.rb
@@ -8,6 +8,15 @@ class Hotel_Block
   def initialize(check_in, check_out, rooms)
     @check_in = Date.parse(check_in)
     @check_out = Date.parse(check_out)
+
+    if rooms < 5
+      raise ArgumentError, "Block must be no greater than 5 rooms. The input was #{rooms}"
+    end
     @rooms = rooms
+  end
+
+  def date_range(check_in, check_out)
+    range = (@check_in...@check_out).to_a
+    return range
   end
 end
--- spec/hotel_block_spec.rb
+++ spec/hotel_block_spec.rb
@@ -1,4 +1,5 @@
 require_relative "spec_helper"
+require "date"
 
 describe "HOTEL BLOCK TESTS" do
   describe "Hotel block class initialization & set up" do
@@ -8,6 +9,17 @@ describe "HOTEL BLOCK TESTS" do
       block_test = Hotel_Block.new(check_in, check_out, 5)
 
       expect(block_test).must_be_kind_of Hotel_Block
+    end
+  end
+
+  describe "#date_range" do
+    it "returns a range of dates for a hotel block" do
+      check_in = "2019-3-15"
+      check_out = "2019-3-20"
+      date_range = (Date.parse(check_in)...Date.parse("2019-3-20")).to_a
+      block_test = Hotel_Block.new(check_in, check_out, 5)
+
+      expect(block_test.date_range(check_in, check_out)).must_equal date_range
     end
   end
 end
