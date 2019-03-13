42142450c22ada6fb5d1f899bebc7c347907bebd
Jillianne Ramirez <jillianne.ramirez@gmail.com>
2019-03-13 15:20:39 -0700

    redone reservation variable

--- spec/reservation_spec.rb
+++ spec/reservation_spec.rb
@@ -13,24 +13,25 @@ describe "Reservation" do
         end
     end
 
-    describe "Reservation behaviors" do
+    describe "Reservation instantiation" do
         before do
-            @reservation2 = HotelManagementSystem::Reservation.new(
+            @reservation = HotelManagementSystem::Reservation.new(
                 start_date: '11th Mar 2019', 
                 end_date: '15th Mar 2019', 
                 room: HotelManagementSystem::Room.new(room: 12))
         end
 
         it "must be instance of Reservation" do
-            expect(@reservation2).must_be_kind_of HotelManagementSystem::Reservation
+            expect(@reservation).must_be_kind_of HotelManagementSystem::Reservation
         end
 
         it "provides the duration of a reservation" do
-            expect(@reservation2.duration).must_equal 4
+            expect(@reservation.duration).must_equal 4
         end
 
         it "give the total cost of a reservation" do
-            expect(@reservation2.total_cost).must_equal 800
+            expect(@reservation.total_cost).must_equal 800
         end
     end
 end
+
