923ee3eb9af8110f41afdf87e937b40655958486
Amy Phung <phung.amy85@gmail.com>
2019-03-13 15:50:29 -0700

    removed extraneous whitespace

--- spec/reservation_spec.rb
+++ spec/reservation_spec.rb
@@ -9,12 +9,12 @@ describe "Reservation class" do
     end
     
     it "establishes the base data structures when instantiated" do
-       @reservation = Hotel::Reservation.new(id: 1, 
-                                            room: 5, 
-                                            start_date:"2019-1-1",
-                                            end_date:"2019-1-10",)
-        
-
+      @reservation = Hotel::Reservation.new(
+          id: 1, 
+          room: 5, 
+          start_date:"2019-1-1",
+          end_date:"2019-1-10"
+          )
         expect(@reservation.id).must_be_kind_of Integer
         expect(@reservation.room).must_be_kind_of Integer
         expect(@reservation.start_date).must_be_kind_of String
@@ -25,11 +25,12 @@ describe "Reservation class" do
   
   describe "Total cost method" do
     before do
-     @reservation = Hotel::Reservation.new(id: 1, 
-                                           room: 5, 
-                                           start_date: "2019-1-1",
-                                           end_date: "2019-1-10"
-                                           )
+     @reservation = Hotel::Reservation.new(
+          id: 1, 
+          room: 5, 
+          start_date: "2019-1-1",
+          end_date: "2019-1-10"
+        )
     end
   
     it "returns an float value" do
@@ -42,26 +43,34 @@ describe "Reservation class" do
   end
   
   
-describe "date range methods" do
-  before do
-   @reservation = Hotel::Reservation.new(id: 10, 
-                                            room: 15, 
-                                            start_date:"January 1, 2019",
-                                            end_date:"2019-01-08",)
+  describe "date range methods" do
+    before do
+      @reservation = Hotel::Reservation.new(
+          id: 10, 
+          room: 15, 
+          start_date:"January 1, 2019",
+          end_date:"2019-01-08"
+      )
+    end
+    
+    it "creates an instance of Date class" do
+      expect(@reservation.date_range).must_be_instance_of DateRange
+    end
+    
+    it "is comprised of a start and end date" do
+      expect(@reservation.date_range.end_date).must_be_instance_of Date
+    end
+    
+    it "correctly transforms the given string date to an instance of Date" do
+      @reservation2 = Hotel::Reservation.new(
+          id: 11, 
+          room: 1, 
+          start_date:"20190101", 
+          end_date: "02-11-2019"
+      )
+        expect(@reservation.date_range.start_date).must_equal 
+          @reservation2.date_range.start_date
+    end
   end
-  
-  it "creates an instance of Date class" do
-    expect(@reservation.date_range).must_be_instance_of DateRange
-  end
-  
-  it "is comprised of a start and end date" do
-    expect(@reservation.date_range.end_date).must_be_instance_of Date
-  end
-  
-  it "correctly transforms the given string date to an instance of Date" do
-    @reservation2 = Hotel::Reservation.new(id: 11, room: 1, start_date:"20190101", end_date: "02-11-2019")
-    expect(@reservation.date_range.start_date).must_equal @reservation2.date_range.start_date
-  end
-end
   
 end
