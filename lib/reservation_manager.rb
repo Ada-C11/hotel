9e6cbdd3af14855d0d446b00cda1da454079ccd6
Heather Izumi <hkizumi@gmail.com>
2019-03-13 15:15:53 -0700

    Updated reservations_by_date and available_rooms methods to accept a date range, updated tests

--- lib/reservation_manager.rb
+++ lib/reservation_manager.rb
@@ -1,5 +1,6 @@
 require_relative "reservation.rb"
 require "date"
+require "pry"
 
 module Hotel
   class ReservationManager
@@ -22,22 +23,29 @@ module Hotel
       return new_reservation
     end
 
-    def reservations_by_date(date)
+    def reservations_by_date(start_date, end_date)
+      s_date = Date.parse(start_date)
+      e_date = Date.parse(end_date)
       reservations_by_date = []
       @reservations.each do |reservation|
-        if date > reservation.check_in && date < reservation.check_out
-          #Right now checkout day is not included because you can check-in on that day
+        if e_date > reservation.check_in && e_date <= reservation.check_out
           reservations_by_date << reservation
+        elsif s_date >= reservation.check_in && s_date <= reservation.check_out
+          reservations_by_date << reservation
+        elsif s_date <= reservation.check_in && e_date >= reservation.check_out
+          reservations_by_date << reservation
+
+          #Right now checkout day is not included because you can check-in on that day
         end
       end
       return reservations_by_date
     end
 
-    def available_rooms(date)
-      p_date = Date.parse(date)
-      #this returns the room number for the first room unavailable room
-      # unavail_rooms = reservations_by_date(p_date)[0].room_number
-      unavail_rooms = reservations_by_date(p_date).map do |reservation|
+    def available_rooms(start_date, end_date)
+      # Do I need this?
+      # p_date = Date.parse(date)
+
+      unavail_rooms = reservations_by_date(start_date, end_date).map do |reservation|
         reservation.room_number
       end
 
--- spec/reservation_manager_spec.rb
+++ spec/reservation_manager_spec.rb
@@ -3,8 +3,8 @@ require "awesome_print"
 
 describe "Reservation_manager instantiation" do
   it "is an instance of Reservation Manager" do
-    guest = Hotel::ReservationManager.new
-    expect(guest).must_be_kind_of Hotel::ReservationManager
+    manager = Hotel::ReservationManager.new
+    expect(manager).must_be_kind_of Hotel::ReservationManager
   end
 end
 
@@ -24,33 +24,42 @@ describe "make_reservation method" do
 end
 
 describe "reservations_by_date method" do
-  it "can list all the reservations for a specified date" do
-    manager = Hotel::ReservationManager.new
+  # describe "Manager" do
+  let (:manager) {
+    Hotel::ReservationManager.new
+  }
+
+  it "can list all the reservations for a specified date range" do
+    # manager = Hotel::ReservationManager.new
     reservation1 = manager.make_reservation(6, "march 15, 2019", "march 20, 2019")
     reservation2 = manager.make_reservation(7, "march 17, 2019", "march 22, 2019")
-    date = Date.parse("march 18, 2019")
-    list_reservations = manager.reservations_by_date(date)
+    start_date = "march 14, 2019"
+    end_date = "march 16, 2019"
+    list_reservations = manager.reservations_by_date(start_date, end_date)
     ap list_reservations
-    expect(list_reservations.length).must_equal 2
+
+    expect(list_reservations.length).must_equal 1
   end
 end
 
 describe "available_rooms method" do
-  it "can return available rooms for a specified date" do
+  it "can return available rooms for a specified date range" do
     manager = Hotel::ReservationManager.new
     reservation1 = manager.make_reservation(2, "march 15, 2019", "march 20, 2019")
     reservation2 = manager.make_reservation(3, "march 17, 2019", "march 22, 2019")
-    date = "march 18, 2019"
-    vacant_rooms = manager.available_rooms(date)
-    expect(vacant_rooms.length).must_equal 18
+    start_date = "march 14, 2019"
+    end_date = "march 16, 2019"
+    vacant_rooms = manager.available_rooms(start_date, end_date)
+    expect(vacant_rooms.length).must_equal 19
   end
 
   it "does not include the booked rooms" do
     manager = Hotel::ReservationManager.new
     reservation1 = manager.make_reservation(2, "march 15, 2019", "march 20, 2019")
     reservation2 = manager.make_reservation(3, "march 17, 2019", "march 22, 2019")
-    date = "march 18, 2019"
-    vacant_rooms = manager.available_rooms(date)
-    expect(vacant_rooms).wont_include 3
+    start_date = "march 14, 2019"
+    end_date = "march 16, 2019"
+    vacant_rooms = manager.available_rooms(start_date, end_date)
+    expect(vacant_rooms).wont_include 2
   end
 end
