5d95e43855c80681489b4c168b459869bd63130e
Angela <angela.ohh@gmail.com>
2019-03-13 14:50:21 -0700

    changed #make_reservation so reservations assign from AVAILABLE rooms

--- lib/reservation_manager.rb
+++ lib/reservation_manager.rb
@@ -1,10 +1,12 @@
 require "pry"
+require "date"
 
 class Reservation_Manager
-  attr_reader :rooms, :all_reservations
+  attr_reader :rooms, :all_reservations, :available_rooms
 
   def initialize
     @all_reservations = all_reservations || []
+    # @available_rooms = (1..20).map { |i| i }
     # @all_rooms = all_rooms
   end
 
@@ -15,24 +17,58 @@ class Reservation_Manager
 
   ##TODO: break up method into mini methods (SRP)
   def make_reservation(check_in, check_out)
-    # parse_dates(check_in, check_out)
-    check_in = Date.parse(check_in)
-    check_out = Date.parse(check_out)
-    if check_out - check_in <= 0
-      raise ArguementError, "Check out time is not after check in time. Inputted check in date was #{check_in} and check out date was #{check_out}"
-    end
+    fixed_check_in = Date.parse(check_in)
+    fixed_check_out = Date.parse(check_out)
 
     ## TO DO: think about changing id
     reservation = Reservation.new(1)
-    reservation.check_in = check_in
-    reservation.check_out = check_out
-    if find_available_rooms.length > 0
-      reservation.room = find_available_rooms[0]
+    reservation.check_in = fixed_check_in
+    reservation.check_out = fixed_check_out
+    if reservation.check_out - reservation.check_in <= 0
+      raise ArguementError, "Check out time is not after check in time. Inputted check in date was #{check_in} and check out date was #{check_out}"
+    end
+
+    #change @available_rooms to reflect the reservation being made for that date
+    available_rooms = find_available_rooms(check_in, check_out)
+    if @available_rooms.length > 0
+      reservation.room = available_rooms[0]
     else
       raise ArgumentError, "There are no available rooms at the moment. Please try again later!"
     end
-    reservation_date_range = (reservation.check_in...reservation.check_out).to_a
+
+    # change available room used to not available. but make available after date passes.
+    # reservation_date_range = (reservation.check_in...reservation.check_out).to_a
     all_reservations << reservation
+    return reservation
+  end
+
+  def find_available_rooms(finding_check_in, finding_check_out)
+    @available_rooms = (1..20).map { |i| i }
+    # binding.pry
+    date1 = Date.parse(finding_check_in)
+    date2 = Date.parse(finding_check_out)
+    given_date_range = (date1...date2).to_a
+    # binding.pry
+
+    unavailable_rooms = []
+    all_reservations.each do |reservation|
+      day_in = reservation.check_in
+      day_out = reservation.check_out
+      reserve_date_range = (day_in...day_out).to_a
+      combined_ranges = (given_date_range + reserve_date_range).flatten
+      # binding.pry
+      if combined_ranges.length != combined_ranges.uniq.length
+        unavailable_rooms << reservation
+      end
+    end
+
+    if unavailable_rooms.length > 0
+      unavailable_rooms.each do |reservation|
+        available_rooms.delete_if { |room_num| room_num == reservation.room }
+      end
+    end
+
+    return available_rooms
   end
 
   def find_reservation_date(check_in, check_out)
@@ -51,33 +87,6 @@ class Reservation_Manager
       end
     end
     return found_reservations
-  end
-
-  def find_available_rooms(check_in, check_out)
-    available_rooms = (1..20).map { |i| i }
-    date1 = Date.parse(check_in)
-    date2 = Date.parse(check_out)
-    given_date_range = (date1...date2).to_a
-
-    unavailable_rooms = []
-    all_reservations.each do |reservation|
-      day_in = reservation.check_in
-      day_out = reservation.check_out
-      reserve_date_range = (day_in...day_out).to_a
-      combined_ranges = (given_date_range + reserve_date_range).flatten
-
-      if combined_ranges.length != combined_ranges.uniq.length
-        unavailable_rooms << reservation
-      end
-    end
-
-    if unavailable_rooms.length > 0
-      unavailable_rooms.each do |reservation|
-        available_rooms.delete_if { |room_num| room_num == reservation.room }
-      end
-    end
-
-    return available_rooms
   end
 
   # def all_rooms
--- spec/reservation_mananger_spec.rb
+++ spec/reservation_mananger_spec.rb
@@ -37,6 +37,34 @@ describe "RESERVATION MANAGER TESTS" do
 
       expect(test_manager.all_reservations.length).must_equal 3
     end
+
+    it "can make reservation when check in is another check out" do
+      test_manager = Reservation_Manager.new
+      test_manager.make_reservation("2019-3-15", "2019-3-20")
+      test_manager.make_reservation("2019-3-20", "2019-3-21")
+
+      check_rooms = test_manager.find_available_rooms("2019-3-15", "2019-3-21")
+      first_reserve_room = test_manager.all_reservations[0].room
+      second_reserve_room = test_manager.all_reservations[1].room
+
+      puts "These are all unavailable rooms"
+      test_manager.all_reservations.each do |reservation|
+        puts "#{reservation.room}"
+      end
+
+      expect(check_rooms.length).must_equal 19
+      expect(first_reserve_room).must_equal second_reserve_room
+    end
+
+    it "must have different rooms if check in is day before another check out" do
+      test_manager = Reservation_Manager.new
+      reserve_1 = test_manager.make_reservation("2019-3-15", "2019-3-20")
+      reserve_2 = test_manager.make_reservation("2019-3-19", "2019-3-21")
+
+      puts "reserve 1 room: #{reserve_1}"
+      puts "reserve 2 room: #{reserve_2}"
+      expect(reserve_1.room).wont_equal reserve_2.room
+    end
   end
 
   describe "#find_reservation_date" do
@@ -64,17 +92,17 @@ describe "RESERVATION MANAGER TESTS" do
       available_rooms = (1..20).map { |i| i }
 
       expect(check_rooms).must_be_kind_of Array
-      expect(check_rooms).must_equal available_rooms
+      expect(check_rooms.length).must_equal 20
     end
 
-    it "returns list of available rooms - dates coincide" do
+    it "returns list of available rooms - dates overlap" do
       test_manager = Reservation_Manager.new
-      check_in = "2019-3-18"
-      check_out = "2019-3-21"
       test_manager.make_reservation("2019-3-15", "2019-3-20")
       test_manager.make_reservation("2019-3-13", "2019-3-19")
+      check_in = "2019-3-18"
+      check_out = "2019-3-21"
+
       check_rooms = test_manager.find_available_rooms(check_in, check_out)
-      available_rooms = (1..20).map { |i| i }
 
       expect(check_rooms.length).must_equal 18
     end
