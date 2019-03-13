ff6e72e7878923f359679f12042bbdbced1117fd
Jillianne Ramirez <jillianne.ramirez@gmail.com>
2019-03-13 13:03:41 -0700

    tests added and corrected for reservation class

--- spec/reservation_spec.rb
+++ spec/reservation_spec.rb
@@ -1,8 +1,36 @@
 require_relative "spec_helper"
 
 describe "Reservation" do
-    it "works" do
-        expect(true).must_equal true
+    describe "ArgumentErrors are raised" do
+        it "raises ArgumentError if dates are invalid" do
+            expect{ HotelManagementSystem::Reservation.new(
+                start_date: '15th Mar 2019', end_date: '11th Mar 2019', room: 2) }.must_raise ArgumentError
+        end
 
+        it "raises ArgumentError if room is not provided" do
+            expect{ HotelManagementSystem::Reservation.new(
+                start_date: '11th Mar 2019', end_date: '15th Mar 2019') }.must_raise ArgumentError
+        end
+    end
+
+    describe "Reservation behaviors" do
+        before do
+            @reservation2 = HotelManagementSystem::Reservation.new(
+                start_date: '11th Mar 2019', 
+                end_date: '15th Mar 2019', 
+                room: HotelManagementSystem::Room.new(room: 12))
+        end
+
+        it "must be instance of Reservation" do
+            expect(@reservation2).must_be_kind_of HotelManagementSystem::Reservation
+        end
+
+        it "provides the duration of a reservation" do
+            expect(@reservation2.duration).must_equal 4
+        end
+
+        it "give the total cost of a reservation" do
+            expect(@reservation2.total_cost).must_equal 800
+        end
     end
 end
