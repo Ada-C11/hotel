49837b2e6963edf19669880e09c9b6461752893c
Jillianne Ramirez <jillianne.ramirez@gmail.com>
2019-03-13 15:55:32 -0700

    tests added for behavior of rooms with reservations

--- spec/room_spec.rb
+++ spec/room_spec.rb
@@ -1,40 +1,54 @@
 require_relative "spec_helper"
 
-describe "Room class" do
+describe "Room" do
     describe "Room instantiation" do
         before do
-            @room = HotelManagementSystem::Room.new(
-              room_number: 4,
-            )
+            @room = HotelManagementSystem::Room.new(room: 4)
+        end
+
+        it "creates an instance of a room" do
+            expect (@room).must_be_kind_of HotelManagementSystem::Room
         end
 
         it "throws an argument error with an invalid room number" do
-            expect { HotelManagementSystem::Room.new(room_number: 21, status: :AVAILABLE) }.must_raise ArgumentError
-            expect { HotelManagementSystem::Room.new(room_number: 0) }.must_raise ArgumentError
+            expect { HotelManagementSystem::Room.new(room: 21) }.must_raise ArgumentError
+            expect { HotelManagementSystem::Room.new(room: 0) }.must_raise ArgumentError
         end
-
-        it "throws an argument error with an invalid status" do
-            expect { HotelManagementSystem::Room.new(room_number: 8, status: :NO) }.must_raise ArgumentError
-        end
-
-        it "has a default status of :AVAILABLE" do
-            expect {@room.status}.must_equal :AVAILABLE
-        end
-
-
-
-
 
         it "has a room number" do
-        end
-
-        it "has a status" do
-        end
-
-        it "can be assigned a guest" do
+            expect(@room.room).must_equal 4
         end
 
         it "has a cost" do
+            expect(@room.cost).must_equal 200
+        end
+    end
+
+    describe "Room reservations" do
+        before do
+            @room = HotelManagementSystem::Room.new(room: 4)
+            @reservation1 = HotelManagementSystem::Reservation.new(
+                start_date: '11th Mar 2019', 
+                end_date: '15th Mar 2019', 
+                room: HotelManagementSystem::Room.new(room: 4))
+            @reservation2 = HotelManagementSystem::Reservation.new(
+                start_date: '18th Mar 2019', 
+                end_date: '25th Mar 2019', 
+                room: HotelManagementSystem::Room.new(room: 4))
+        end
+
+        it "lists own reservations" do
+            @room.add_reservation(@reservation1)
+            expect(@room.reservations).must_be_kind_of Array 
+        end
+
+        it "can add a reservation to own reservations" do
+            before_adding = @room.reservations.length
+            @room.add_reservation(@reservation1)
+            after_adding1 = @room.reservations.length
+            @room.add_reservation(@reservation2)
+            after_adding2 = @room.reservations.length
+            expect(after_adding2 - before_adding).must_equal 2
         end
     end
 end
