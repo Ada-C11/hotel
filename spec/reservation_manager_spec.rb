86470638f59180e77be1341464e48ba2f671f8ba
Heather Izumi <hkizumi@gmail.com>
2019-03-12 20:33:22 -0700

    Updated Reservation Manager class and tests for Wave 1

--- spec/reservation_manager_spec.rb
+++ spec/reservation_manager_spec.rb
@@ -13,3 +13,22 @@ describe "all_rooms method" do
     expect(rooms.all_rooms.length).must_equal 20
   end
 end
+
+describe "make_reservation method" do
+  it "can make a new reservation" do
+    manager = Hotel::ReservationManager.new
+    new_reservation = manager.make_reservation(6, "march 15, 2019", "march 20, 2019")
+    expect(new_reservation.room_number).must_equal 6
+  end
+end
+
+describe "reservations_by_date method" do
+  it "can list all the reservations for a specified date" do
+    manager = Hotel::ReservationManager.new
+    reservation1 = manager.make_reservation(6, "march 15, 2019", "march 20, 2019")
+    reservation2 = manager.make_reservation(7, "march 17, 2019", "march 22, 2019")
+    date = Date.parse("march 18, 2019")
+    list_reservations = manager.reservations_by_date(date)
+    expect(list_reservations.length).must_equal 2
+  end
+end
