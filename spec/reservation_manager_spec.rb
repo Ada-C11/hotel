dac853ca13666febe354f623ea6857c3b3ccd468
Heather Izumi <hkizumi@gmail.com>
2019-03-12 20:18:01 -0700

    Reservation Manager recovered files with Wave 1 methods, not all tests

--- spec/reservation_manager_spec.rb
+++ spec/reservation_manager_spec.rb
@@ -2,7 +2,14 @@ require_relative "spec_helper"
 
 describe "Reservation_manager instantiation" do
   it "is an instance of Reservation Manager" do
-    guest = ReservationManager.new("Pfeiffer")
-    expect(guest).must_be_kind_of ReservationManager
+    guest = Hotel::ReservationManager.new
+    expect(guest).must_be_kind_of Hotel::ReservationManager
+  end
+end
+
+describe "all_rooms method" do
+  it "can list all rooms in the hotel" do
+    rooms = Hotel::ReservationManager.new
+    expect(rooms.all_rooms.length).must_equal 20
   end
 end
