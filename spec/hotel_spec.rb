fcf3255ff8a1d0e6675868d3246b42e8a70e88d8
sjscotton <sj.scotton@gmail.com>
2019-03-14 16:39:31 -0700

    Added another file to last commit

--- spec/hotel_spec.rb
+++ spec/hotel_spec.rb
@@ -61,35 +61,35 @@ describe "Hotel" do
       # @hotel.book_reservation(@room_three, @another_first_day, @another_second_day)
       # @reservation = HotelSystem::Reservation.new(room: room, arrive_day: arrive_day, depart_day: depart_day)
     end
-    describe "hotel#book_reservation" do
-      it "Adds reservation to reservations array" do
-        expect(@hotel.reservations.length).must_equal 2
-        expect(@hotel.reservations.first).must_be_kind_of HotelSystem::Reservation
-      end
+    # describe "hotel#book_reservation" do
+    #   it "Adds reservation to reservations array" do
+    #     expect(@hotel.reservations.length).must_equal 2
+    #     expect(@hotel.reservations.first).must_be_kind_of HotelSystem::Reservation
+    #   end
 
-      it "Raises an ArgumentError if trying to book a room that is unavailable" do
-        expect {
-          @hotel.book_reservation(@room, @arrive_day, @depart_day)
-        }.must_raise ArgumentError
-      end
+    #   it "Raises an ArgumentError if trying to book a room that is unavailable" do
+    #     expect {
+    #       @hotel.book_reservation(@room, @arrive_day, @depart_day)
+    #     }.must_raise ArgumentError
+    #   end
 
-      it "Accepts booking a room with a reservation that starts on the day another ends " do
-        hotel_reservation = @hotel.book_reservation(@room, @depart_day, "2019-02-18")
-        expect(@hotel.reservations).must_include hotel_reservation
-        expect(@room.reservations).must_include hotel_reservation
-      end
+    #   it "Accepts booking a room with a reservation that starts on the day another ends " do
+    #     hotel_reservation = @hotel.book_reservation(@room, @depart_day, "2019-02-18")
+    #     expect(@hotel.reservations).must_include hotel_reservation
+    #     expect(@room.reservations).must_include hotel_reservation
+    #   end
 
-      it "Accepts booking a room with a reservation that ends on the day another starts" do
-        hotel_reservation = @hotel.book_reservation(@room, "2019-02-01", @arrive_day)
-        expect(@hotel.reservations).must_include hotel_reservation
-        expect(@room.reservations).must_include hotel_reservation
-      end
+    #   it "Accepts booking a room with a reservation that ends on the day another starts" do
+    #     hotel_reservation = @hotel.book_reservation(@room, "2019-02-01", @arrive_day)
+    #     expect(@hotel.reservations).must_include hotel_reservation
+    #     expect(@room.reservations).must_include hotel_reservation
+    #   end
 
-      it "Adds reservation to the rooms reservation array" do
-        expect(@room.reservations.length).must_equal 1
-        expect(@room.reservations.first).must_be_kind_of HotelSystem::Reservation
-      end
-    end
+    #   it "Adds reservation to the rooms reservation array" do
+    #     expect(@room.reservations.length).must_equal 1
+    #     expect(@room.reservations.first).must_be_kind_of HotelSystem::Reservation
+    #   end
+    # end
 
     describe "#reservations_by_date" do
       before do
@@ -141,28 +141,116 @@ describe "Hotel" do
         end
       end
     end
-    describe "get_available_rooms" do
-      it "Returns an array of Rooms" do
-        available_rooms = @hotel.get_available_rooms("2017-3-15", "2017-3-18")
-        expect(available_rooms).must_be_kind_of Array
-        expect(available_rooms.first).must_be_kind_of HotelSystem::Room
-        expect(available_rooms.length).must_equal 3
+  end
+
+  describe "get_available_rooms" do
+    before do
+      @room = HotelSystem::Room.new(id: 1)
+      @hotel.rooms << @room
+
+      @way_before = "2016-11-07"
+      @before = "2016-11-11"
+      @start = "2016-11-13"
+      @first_mid = "2016-11-15"
+      @second_mid = "2016-11-17"
+      @end = "2016-11-19"
+      @after = "2016-11-21"
+      @way_after = "2016-11-25"
+
+      @hotel.book_reservation(@room, @start, @end)
+    end
+    it "Returns an array of Rooms" do
+      available_rooms = @hotel.get_available_rooms("2017-3-15", "2017-3-18")
+      expect(available_rooms).must_be_kind_of Array
+      expect(available_rooms.first).must_be_kind_of HotelSystem::Room
+      expect(available_rooms.length).must_equal 1
+    end
+
+    it "Returns rooms that have no conflicting reservations" do
+      available_rooms = @hotel.get_available_rooms(@way_before, @before)
+      expect(available_rooms.length).must_equal 1
+    end
+
+    it "Returns rooms that are avaiable, but were occupied the previous night" do
+    end
+
+    it "Doesn't return rooms that have conflicting reservations" do
+      available_rooms = @hotel.get_available_rooms("2019-2-12", "2019-2-17")
+      expect(available_rooms.length).must_equal 1
+    end
+  end
+
+  describe "hotel#book_reservation" do
+    before do
+      @room = HotelSystem::Room.new(id: 1)
+      # @room_two = HotelSystem::Room.new(id: 2)
+      @hotel.rooms << @room
+      # @hotel.rooms << @room_two
+
+      @way_before = "2016-11-07"
+      @before = "2016-11-11"
+      @start = "2016-11-13"
+      @first_mid = "2016-11-15"
+      @second_mid = "2016-11-17"
+      @end = "2016-11-19"
+      @after = "2016-11-21"
+      @way_after = "2016-11-25"
+
+      @hotel.book_reservation(@room, @start, @end)
+      # @hotel.book_reservation(@room_two, @start, @end)
+    end
+    it "Adds reservation to hotel reservations array" do
+      expect(@hotel.reservations.length).must_equal 1
+      expect(@hotel.reservations.first).must_be_kind_of HotelSystem::Reservation
+    end
+
+    it "Adds reservation to the rooms reservation array" do
+      expect(@room.reservations.length).must_equal 1
+      expect(@room.reservations.first).must_be_kind_of HotelSystem::Reservation
+    end
+
+    describe "Raises an ArgumentError if trying to book a room that is unavailable" do
+      it "Rejects a reservation with the same start and end date" do
+        expect {
+          @hotel.book_reservation(@room, @start, @end)
+        }.must_raise ArgumentError
       end
 
-      it "Returns rooms that have no conflicting reservations" do
-        available_rooms = @hotel.get_available_rooms("2017-3-15", "2017-3-18")
-        expect(available_rooms.length).must_equal 3
+      it "Rejects a reservation with a start date before and the end date in the middle of another reservation" do
+        expect {
+          @hotel.book_reservation(@room, @start, @first_mid)
+        }.must_raise ArgumentError
+      end
+      it "Rejects a reservation with a start date in the middle and the end after another reservation" do
+        expect {
+          @hotel.book_reservation(@room, @start, @first_mid)
+        }.must_raise ArgumentError
       end
 
-      it "Returns rooms that are avaiable, but were occupied the previous night" do
+      it "Rejects a reservation with a start date before and the end date after the end of another reservation" do
+        expect {
+          @hotel.book_reservation(@room, @start, @first_mid)
+        }.must_raise ArgumentError
+      end
+    end
+    describe "Accepts available rooms" do
+      it "Accepts booking a room with a reservation that starts on the day another ends " do
+        hotel_reservation = @hotel.book_reservation(@room, @way_before, @before)
+        expect(@room.reservations).must_include hotel_reservation
+      end
+      it "Accepts booking a room with a reservation that ends on the day another starts" do
+        hotel_reservation = @hotel.book_reservation(@room, @after, @way_after)
+        expect(@room.reservations).must_include hotel_reservation
       end
 
-      it "Doesn't return rooms that have conflicting reservations" do
-        available_rooms = @hotel.get_available_rooms("2019-2-12", "2019-2-17")
-        expect(available_rooms.length).must_equal 1
+      it "Accepts booking a room with a reservation that ends on the day another starts" do
+        hotel_reservation = @hotel.book_reservation(@room, @after, @way_after)
+        expect(@hotel.reservations).must_include hotel_reservation
+        expect(@room.reservations).must_include hotel_reservation
       end
     end
   end
+
   describe "Managing Blocks" do
     before do
       @room = HotelSystem::Room.new(id: 1)
