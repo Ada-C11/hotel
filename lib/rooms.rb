8c85c537aa90ccc7bce3d298d78923706589e8c7
Kate <kateannnichols@gmail.com>
2019-03-14 10:12:16 -0700

    wave 0

---
+++ lib/rooms.rb
@@ -0,0 +1,19 @@
+#there are 20 identical rooms, each costing $200/night
+#there are 
+require 'reservations.rb'
+
+Class rooms
+    @rooms = rooms
+    @one_night_cost = 200
+    @total_cost = total_cost
+    @total_rooms = 20
+    @rooms_available = []
+    @date = date 
+
+    def 
+        #will list all the rooms in the hotel 
+    end
+
+    def 
+        #will list all of the available rooms in the hotel
+    end 
