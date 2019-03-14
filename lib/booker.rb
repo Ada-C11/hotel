ac678d6c3c62d63e0b98cdee06fefaf0839a542f
Tatiana Quintana <tatsqui@gmail.com>
2019-03-13 20:55:11 -0700

    removed unnecessary requires from top of document.

--- lib/booker.rb
+++ lib/booker.rb
@@ -1,5 +1,3 @@
-require "csv"
-require "chronic"
 require "time"
 require "date"
 
@@ -16,17 +14,18 @@ class RoomBooker < DateRange
   end
 
   def make_reservation(check_in, check_out)
-    room = find_room_id
-    @reserved = Reservation.new(id: @reservations.length + 1, check_in: check_in, check_out: check_out, room_booked: room)
+    room = find_available_room
+    reserved = Reservation.new(id: @reservations.length + 1, check_in: check_in, check_out: check_out, room_booked: room)
 
-    add_reservation(@reserved)
+    @reservations << reserved
   end
 
   def find_available_room
     @all_rooms.each do |room|
       if room.bookings.empty?
-        return room.id
+        return room
       end
+      # find by date
     end
   end
 
@@ -38,17 +37,23 @@ class RoomBooker < DateRange
     return @all_rooms
   end
 
-  def add_reservation(res)
-    @reservations << res
-  end
-
   def list_all_rooms
     @all_rooms.each do |room|
       puts "Room number: #{room.id}, price: #{room.price}"
     end
   end
 
-  def list_reservations
-    @reservations
+  def search_bookings(date)
+   find_by_dates = @reservations.map do |res_dates|
+    if (res_dates.check_in..res_dates.check_out).include?(date)
+      res_dates
+    end
+   end
+  
+    if find_by_dates.empty?
+      puts "No dates "
+    else
+      return find_by_dates
+    end
   end
 end
