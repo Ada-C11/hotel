9dbdadac3999932ea78ed1118ec30d96218d9d0a
Carla Bosco <carlabosco@gmail.com>
2019-03-15 11:38:30 -0700

    Added files for block class

---
+++ spec/block_spec.rb
@@ -0,0 +1,10 @@
+require 'minitest/autorun'
+require 'minitest/reporters'
+require_relative 'spec_helper'
+require_relative '../lib/block'
+
+describe 'Block' do
+  it 'blocks a number of rooms' do
+    
+  end
+end
