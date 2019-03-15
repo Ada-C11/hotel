fede4b80bb01b2681f5735770623a65e35c28b72
Kate Sandler <kate@katekatekate.net>
2019-03-14 22:47:24 -0700

    Reasoning with monsters in available? method. Success not guaranteed.

--- lib/registry.rb
+++ lib/registry.rb
@@ -1,44 +1,42 @@
-require 'pry'
+
 module Hotel
   COST = 200
   ROOMS = [*1..20]
   class Registry
-    attr_reader :reservations
+    attr_accessor :reservations
 
     def initialize
       @reservations = []
     end
 
     def reserve_room(check_in, duration)
-      new_res = Reservation.new(check_in, duration)
-
-      @reservations << new_res
-      @reservations
+      request = Reservation.new(check_in, duration)
+      if request.available?(@check_in, @check_out)
+        request.room = @open_rms.first
+        @reservations << request
+      else
+      raise Errors::BookingConflict, "Request includes unavailable dates."
+      end
     end
 
-    def openings(range)
-      conflicts = find_in_range(range)
-      open_rms = @rooms.reject do |room|
-        conflicts.find do |entry|
-          entry.rm_id == room[:rm_id]
+    private
+
+    @open_rms = []
+    @conflicts = find_in_range(date_range)
+
+    def available?(check_out, check_out)
+      @open_rms << ROOMS.reject do |room|
+        @conflicts.find do |conflict|
+          conflict.room == room[:room]
         end
       end
-      open_rms
     end
 
-    def find_in_range(give_range)
-      in_range = @reservations.select do |entry|
-        entry.span.overlaps?(given_range)
+    def find_in_range(request)
+      in_range = @reservations.range.select do |range|
+        request.check_in.during?(range) || request.check_out.during?(range)
       end
       in_range
-    end
-
-    def res_list
-      @reservations.inspect
-    end
-
-    def overlaps?(reserved_dates)
-      !(reserved_dates & self).empty?
     end
 
     def find_by_date(date)
@@ -49,4 +47,11 @@ module Hotel
       by_date
     end
   end
+end
+
+module Errors
+
+class BookingConflict < StandardError; end
+class ValidationError < StandardError; end
+
 end
