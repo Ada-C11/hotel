9f1d095ec4c0220ed3b0463e0f158f7af82af108
ranuseh <rosalyn.anuseh@gmail.com>
2019-03-15 10:39:34 -0700

    completed make booking method to all requirements for wave 2. added find room by room number method and added additional parameter to find booking by date

--- lib/reservation_system.rb
+++ lib/reservation_system.rb
@@ -1,90 +1,77 @@
 module HotelBooking
   class ReservationSystem
-
-    def initialize
-      @hotel = Hotel.new(hotel_name: 'Wyndham', number_of_rooms: 20)
+    def initialize(hotel_name, number_of_rooms)
+      @hotel = Hotel.new(hotel_name: hotel_name, number_of_rooms: number_of_rooms)
     end
 
     # Can find specific room if provided the booking number
     def find_room_by_booking_number(booking_number)
-      room = @hotel.bookings.find { |booking| booking.reference_number == booking_number}
-      return room
+      @hotel.bookings.find { |b| b.reference_number == booking_number }
     end
 
     # Can find specific bookings if provided the room number
     def find_bookings_by_room_number(room_number)
-      bookings = @hotel.bookings.select { |booking| booking.room.number == room_number}
-      return bookings
+      @hotel.bookings.select { |b| b.room.number == room_number }
+    end
+
+    # Can find room by room number
+    def find_room_by_room_number(room_number)
+      @hotel.rooms.find { |r| r.number == room_number }
     end
 
     # Can find specific bookings for a date when provided date
-    def find_booking_by_date(date)
-      bookings = @hotel.bookings.select do |booking|
-        booking.start_date <= date && booking.end_date >= date
+    def find_booking_by_date(start_date, end_date)
+      bookings = @hotel.bookings.select do |b|
+        b.start_date <= start_date && b.end_date >= end_date
       end
-      return bookings
+      bookings
     end
 
     # Can find available rooms if given a start and end date
     def get_available_rooms(start_date, end_date)
-
-      all_bookings = find_booking_by_date(start_date)
-      unavailable_bookings = all_bookings.select { |booking| booking.end_date <= end_date}
-
-      occpuied_rooms = []
-      unavailable_bookings.each do |booking|
-        occpuied_rooms.push(booking.room)
-      end
-
-      available_rooms = []
-      @hotel.rooms.each do |room|
-        unless occpuied_rooms.include?(room)
-          available_rooms.push(room)
-        end
-      end
-      return available_rooms
+      unavailable_bookings = find_booking_by_date(start_date, end_date)
+      
+      occpuied_rooms = unavailable_bookings.map { |booking| booking.room }
+      
+      @hotel.rooms.reject { |room| occpuied_rooms.include?(room) }
     end
 
     # Returns true or false if specific room is available for given date
     def available?(start_date, end_date, room_number)
-
       rooms_available = get_available_rooms(start_date, end_date)
-      room_numbers = rooms_available
 
-      if room_numbers.include?(room_number)
-        return true
-      else
-        return false
-      end
+      room_numbers = rooms_available.map { |room| room.number }
+
+      room_numbers.include?(room_number)
     end
 
-    # maybe make a booking block
-
-    # Makes your booking when given date range
-    # add number of rooms and price set default unless its is block
-
-    def make_booking(start_date, end_date)
+    def make_booking(start_date, end_date, room_number: nil)
+      selected_room = nil
 
       if start_date > end_date
         raise ArgumentError.new("Invalid date range")
       end
 
-      available_rooms = get_available_rooms(start_date, end_date)
-      selected_room = available_rooms.first
-
-      if available?(start_date, end_date, selected_room)
-        booking_details = {
-          reference_number: @hotel.bookings.length + 1,
-          room: selected_room,
-          start_date: start_date,
-          end_date: end_date,
-          price: 200
-        }
-        new_booking = Booking.new(booking_details)
-
-        @hotel.add_booking(new_booking)
-        return new_booking
+      if room_number.nil?
+        available_rooms = get_available_rooms(start_date, end_date)
+        selected_room = available_rooms.first
+      elsif available?(start_date, end_date, room_number)
+        selected_room = find_room_by_room_number(room_number)
+      else
+        raise ArgumentError.new("Room is not available")
       end
+
+      booking_details = {
+        reference_number: @hotel.bookings.length + 1,
+        room: selected_room,
+        start_date: start_date,
+        end_date: end_date,
+        price: 200
+      }
+      new_booking = Booking.new(booking_details)
+
+      @hotel.add_booking(new_booking)
+      new_booking
     end
   end
 end
--- spec/reservation_system_spec.rb
+++ spec/reservation_system_spec.rb
@@ -4,9 +4,9 @@ describe "ReservationSystem class" do
   describe 'Rooms and bookings methods' do
 
     it "can find room by booking number" do
-      reservation_system = HotelBooking::ReservationSystem.new
+      reservation_system = HotelBooking::ReservationSystem.new("Wyndham", 20)
 
-      booking = reservation_system.make_booking('2001-02-03', '2001-02-07' )
+      booking = reservation_system.make_booking('2001-02-03', '2001-02-07')
 
       room = reservation_system.find_room_by_booking_number(booking.reference_number)
 
@@ -14,114 +14,112 @@ describe "ReservationSystem class" do
     end
 
     it "can find booking by room number" do
-      reservation_system = HotelBooking::ReservationSystem.new
+      reservation_system = HotelBooking::ReservationSystem.new("Wyndham", 20)
 
       booking = reservation_system.make_booking('2001-02-03', '2001-02-07')
-
       bookings = reservation_system.find_bookings_by_room_number(booking.room.number)
 
       expect(bookings).must_equal [booking]
     end
 
     it "can find bookings by date" do
-      reservation_system = HotelBooking::ReservationSystem.new
+      reservation_system = HotelBooking::ReservationSystem.new("Wyndham", 20)
       booking = reservation_system.make_booking('2001-02-03', '2001-02-07')
 
-      bookings = reservation_system.find_booking_by_date(booking.start_date)
+      bookings = reservation_system.find_booking_by_date(booking.start_date, booking.end_date)
       
       expect(bookings).must_equal [booking]
     end
   end
 
+
   describe 'Get available rooms' do
     it "Returns an array of available rooms" do
-      reservation_system = HotelBooking::ReservationSystem.new
+      reservation_system = HotelBooking::ReservationSystem.new("Wyndham", 20)
       bookings = reservation_system.get_available_rooms('2001-02-03', '2001-02-07')
 
       expect(bookings).must_be_kind_of Array
     end
 
     it "Throws an error if there are no reservations" do
-      reservation_system = HotelBooking::ReservationSystem.new
+      reservation_system = HotelBooking::ReservationSystem.new("Wyndham", 20)
       bookings = reservation_system.get_available_rooms('2001-02-03', '2001-02-07')
 
       expect(bookings).must_be_kind_of Array
     end
 
     it "Does includes rooms that start before range and ends before range" do
-      reservation_system = HotelBooking::ReservationSystem.new
+      reservation_system = HotelBooking::ReservationSystem.new("Wyndham", 20)
       bookings = reservation_system.get_available_rooms('2001-02-03', '2001-02-07')
 
       expect(bookings).must_be_kind_of Array
     end
 
     it "Does includes rooms that start before range and ends in range" do
-      reservation_system = HotelBooking::ReservationSystem.new
+      reservation_system = HotelBooking::ReservationSystem.new("Wyndham", 20)
       bookings = reservation_system.get_available_rooms('2001-02-03', '2001-02-07')
 
       expect(bookings).must_be_kind_of Array
     end
 
-
-
     it "Does not include rooms that start before start date and end during range" do
-      reservation_system = HotelBooking::ReservationSystem.new
+      reservation_system = HotelBooking::ReservationSystem.new("Wyndham", 20)
       bookings = reservation_system.get_available_rooms('2001-02-03', '2001-02-07')
 
       expect(bookings).must_be_kind_of Array
     end
 
     it "Does not include rooms that start during range and ends after range" do
-      reservation_system = HotelBooking::ReservationSystem.new
+      reservation_system = HotelBooking::ReservationSystem.new("Wyndham", 20)
       bookings = reservation_system.get_available_rooms('2001-02-03', '2001-02-07')
 
       expect(bookings).must_be_kind_of Array
     end
 
     it "Does not include rooms that overlap with range" do
-      reservation_system = HotelBooking::ReservationSystem.new
+      reservation_system = HotelBooking::ReservationSystem.new("Wyndham", 20)
       bookings = reservation_system.get_available_rooms('2001-02-03', '2001-02-07')
 
       expect(bookings).must_be_kind_of Array
     end
 
     it "Does not include rooms that start before range and ends in range" do
-      reservation_system = HotelBooking::ReservationSystem.new
+      reservation_system = HotelBooking::ReservationSystem.new("Wyndham", 20)
       bookings = reservation_system.get_available_rooms('2001-02-03', '2001-02-07')
 
       expect(bookings).must_be_kind_of Array
     end
 
     it "Does not include rooms that start in range and ends after range" do
-      reservation_system = HotelBooking::ReservationSystem.new
+      reservation_system = HotelBooking::ReservationSystem.new("Wyndham", 20)
       bookings = reservation_system.get_available_rooms('2001-02-03', '2001-02-07')
 
       expect(bookings).must_be_kind_of Array
     end
 
     it "Does not include rooms that start and end anywhere in range" do
-      reservation_system = HotelBooking::ReservationSystem.new
+      reservation_system = HotelBooking::ReservationSystem.new("Wyndham", 20)
       bookings = reservation_system.get_available_rooms('2001-02-03', '2001-02-07')
 
       expect(bookings).must_be_kind_of Array
     end
 
     it "Does not include rooms that start before range and ends after range" do
-      reservation_system = HotelBooking::ReservationSystem.new
+      reservation_system = HotelBooking::ReservationSystem.new("Wyndham", 20)
       bookings = reservation_system.get_available_rooms('2001-02-03', '2001-02-07')
 
       expect(bookings).must_be_kind_of Array
     end
 
     it "Does not include rooms that start in range and ends in range" do
-      reservation_system = HotelBooking::ReservationSystem.new
+      reservation_system = HotelBooking::ReservationSystem.new("Wyndham", 20)
       bookings = reservation_system.get_available_rooms('2001-02-03', '2001-02-07')
 
       expect(bookings).must_be_kind_of Array
     end
 
     it "Does not include rooms that start in range and ends in range" do
-      reservation_system = HotelBooking::ReservationSystem.new
+      reservation_system = HotelBooking::ReservationSystem.new("Wyndham", 20)
       bookings = reservation_system.get_available_rooms('2001-02-03', '2001-02-07')
 
       expect(bookings).must_be_kind_of Array
