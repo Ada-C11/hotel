e7d95eebb4b0fac59ab873e0c45108049c9bddec
Kate Sandler <kate@katekatekate.net>
2019-03-15 00:22:45 -0700

    Adds method to reset reservations array.

--- lib/registry.rb
+++ lib/registry.rb
@@ -38,18 +38,26 @@ module Hotel
       end
       in_range
     end
-
-#     def find_by_date(date)
-#       date = Date.parse(date)
-#       by_date = @reservations.select do |entry|
-#         entry.date.find_in_range(date)
-#       end
-#       by_date
-#     end
-#   end
-# end
-
+  end
+end
 module Errors
   class BookingConflict < StandardError; end
   class ValidationError < StandardError; end
+end
+
+module TinyGoat
+  def hungry?(_goat)
+    puts "Does Scribbles look hungry? ENTER <Y> or <N>: "
+    hungry = gets chomp.to_upcase
+    hungry == "Y"
+  end
+
+  def feed_all_reservations_to_small_goat
+    puts "There are a few goats here. <ENTER>"
+    puts "Only one of them is small, though. <ENTER>"
+    puts "There's a little nametag. <ENTER>"
+    puts "The goat is named Scribbles."
+    @reservations = [] if goat.hungry?
+    puts "Scribbles the Goat: 'Maeehhh. Mneeeaaehh.'"
+  end
 end
