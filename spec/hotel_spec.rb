bf491864ffdc31e9a9678dd61ef53d1a9de096e5
Cyndi Lopez <cyndilopez6@gmail.com>
2019-03-14 19:33:43 -0700

    added tests for reserve_hotel_block, raising argument error if trying to reserve room or hotel block when room is set aside in hotel block

--- spec/hotel_spec.rb
+++ spec/hotel_spec.rb
@@ -33,17 +33,12 @@ describe "Hotel class" do
 
   describe "#reserve_room" do
     it "can create a reservation" do
-      #year, month, day
-      reservation_id = 333
-      new_reservation = @hotel.reserve_room(reservation_id, Date.new(2001, 3, 5), Date.new(2001, 3, 7), 0)
+      new_reservation = @hotel.reserve_room(Date.new(2001, 3, 5), Date.new(2001, 3, 7), 0)
       expect(new_reservation).must_be_kind_of Hotel::Reservation
     end
 
     it "adds the reservation to the Hotel's list of reservations" do
-      #year, month, day
-      reservation_id = 333
-      new_reservation = @hotel.reserve_room(reservation_id, Date.new(2001, 3, 5), Date.new(2001, 3, 7), 0)
-      @hotel.add_reservation(new_reservation)
+      new_reservation = @hotel.reserve_room(Date.new(2001, 3, 5), Date.new(2001, 3, 7), 0)
       expect(@hotel.reservations.length).must_equal 1
     end
   end
@@ -59,112 +54,141 @@ describe "Hotel class" do
   end
 
   describe "#access_reservations" do
-    # reserve_room(reservation_id,start_date, end_date,room_ID)
-    # before do
-    #   @rooms = []
-    #   20.times do |k|
-    #     @rooms << Hotel::Room.new(id: k)
-    #   end
-    #   @hotel = Hotel::Hotel.new(
-    #     id: 1,
-    #     rooms: @rooms,
-    #     reservations: [],
-    #   )
-    it "checks the date range ok" do
-      new_reservation = @hotel.reserve_room(333, Date.new(2001, 3, 5), Date.new(2001, 3, 7), 0)
-      new_reservation2 = @hotel.reserve_room(334, Date.new(2001, 3, 5), Date.new(2001, 3, 7), 1)
-      @hotel.add_reservation(new_reservation)
-      @hotel.add_reservation(new_reservation2)
-      new_reservation.room.add_reservation(new_reservation)
-      new_reservation2.room.add_reservation(new_reservation2)
-      expect(@hotel.access_reservations(Date.new(2001, 3, 6))[0].id).must_equal 333
-    end
-
-    it "Access the list of reservations by date" do
+    it "accesses a reservation correctly" do
+      new_reservation = @hotel.reserve_room(Date.new(2001, 3, 5), Date.new(2001, 3, 7), 0)
+      new_reservation2 = @hotel.reserve_room(Date.new(2001, 3, 5), Date.new(2001, 3, 7), 1)
+      expect(@hotel.access_reservations(Date.new(2001, 3, 6))[0].room.id).must_equal 0
     end
   end
 
   describe "#available_rooms" do
     before do
-      new_reservation = @hotel.reserve_room(333, Date.new(2001, 3, 5), Date.new(2001, 3, 7), 0)
-      new_reservation2 = @hotel.reserve_room(334, Date.new(2001, 3, 5), Date.new(2001, 3, 7), 1)
-      new_reservation3 = @hotel.reserve_room(335, Date.new(2001, 3, 9), Date.new(2001, 3, 15), 2)
-      new_reservation4 = @hotel.reserve_room(336, Date.new(2001, 3, 4), Date.new(2001, 3, 12), 12)
-
-      @hotel.add_reservation(new_reservation)
-      @hotel.add_reservation(new_reservation2)
-      @hotel.add_reservation(new_reservation3)
-      @hotel.add_reservation(new_reservation4)
-
-      new_reservation.room.add_reservation(new_reservation)
-      new_reservation2.room.add_reservation(new_reservation2)
-      new_reservation3.room.add_reservation(new_reservation3)
-      new_reservation4.room.add_reservation(new_reservation4)
+      new_reservation = @hotel.reserve_room(Date.new(2001, 3, 4), Date.new(2001, 3, 12), 12)
     end
-
-    it "returns a list of rooms available for a given date range within the reservation date range " do
+    it "desired reservation dates equal to existing reservation dates" do
+      start_date = Date.new(2001, 3, 4)
+      end_date = Date.new(2001, 3, 12)
+      expect(@hotel.available_rooms(start_date, end_date)).must_equal [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 15, 16, 17, 18, 19]
+    end
+    it "desired reservation within existing reservation dates" do
       start_date = Date.new(2001, 3, 6)
       end_date = Date.new(2001, 3, 8)
-      expect(@hotel.available_rooms(start_date, end_date)).must_equal [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 15, 16, 17, 18, 19]
+      expect(@hotel.available_rooms(start_date, end_date)).must_equal [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 15, 16, 17, 18, 19]
     end
 
-    it "returns a list of rooms available for a given date range outside most reservation date ranges" do
+    it "desired reservation starts before and ends during existing reservation " do
       # new_reservation5 = @hotel.reserve_room(337, Date.new(2001, 3, 4), Date.new(2001, 3, 12), 15)
       # @hotel.add_reservation(new_reservation5)
-      start_date = Date.new(2001, 3, 3)
+      start_date = Date.new(2001, 3, 2)
       end_date = Date.new(2001, 3, 6)
-      expect(@hotel.available_rooms(start_date, end_date)).must_equal [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 15, 16, 17, 18, 19]
+      expect(@hotel.available_rooms(start_date, end_date)).must_equal [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 15, 16, 17, 18, 19]
     end
 
-    it "returns a list of rooms available for a given date range with start date at the end date of existing reservations" do
-      # new_reservation5 = @hotel.reserve_room(337, Date.new(2001, 3, 4), Date.new(2001, 3, 12), 15)
-      # @hotel.add_reservation(new_reservation5)
+    it "desired reservation starts before and ends on existing reservation" do
+      start_date = Date.new(2001, 3, 2)
+      end_date = Date.new(2001, 3, 12)
+      expect(@hotel.available_rooms(start_date, end_date)).must_equal [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 15, 16, 17, 18, 19]
+    end
+
+    it "desired reservation starts during and ends after existing reservation" do
+      start_date = Date.new(2001, 3, 6)
+      end_date = Date.new(2001, 3, 15)
+      expect(@hotel.available_rooms(start_date, end_date)).must_equal [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 15, 16, 17, 18, 19]
+    end
+
+    it "desired reservation starts before and ends after existing reservation" do
+      start_date = Date.new(2001, 3, 2)
+      end_date = Date.new(2001, 3, 15)
+      expect(@hotel.available_rooms(start_date, end_date)).must_equal [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 15, 16, 17, 18, 19]
+    end
+
+    it "desired reservation starts during and ends on existing reservation" do
+      start_date = Date.new(2001, 3, 6)
+      end_date = Date.new(2001, 3, 12)
+      expect(@hotel.available_rooms(start_date, end_date)).must_equal [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 15, 16, 17, 18, 19]
+    end
+
+    it "desired reservation begins and ends before existing reservation" do
+      start_date = Date.new(2001, 3, 2)
+      end_date = Date.new(2001, 3, 4)
+      expect(@hotel.available_rooms(start_date, end_date)).must_equal [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
+    end
+
+    it "desired reservation begins at end date of existing reservation and continues " do
+      start_date = Date.new(2001, 3, 12)
+      end_date = Date.new(2001, 3, 15)
+      expect(@hotel.available_rooms(start_date, end_date)).must_equal [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
+    end
+
+    it "desired reservations begins and ends before existing reservation" do
+      start_date = Date.new(2001, 3, 1)
+      end_date = Date.new(2001, 3, 3)
+      expect(@hotel.available_rooms(start_date, end_date)).must_equal [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
+    end
+
+    it "desired reservation to begin and end after existing reservation ends" do
+      start_date = Date.new(2001, 3, 13)
+      end_date = Date.new(2001, 3, 16)
+      expect(@hotel.available_rooms(start_date, end_date)).must_equal [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
+    end
+
+    it "existing reservation begins and ends reservation" do
       start_date = Date.new(2001, 3, 7)
       end_date = Date.new(2001, 3, 10)
-      expect(@hotel.available_rooms(start_date, end_date)).must_equal [0, 1, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 15, 16, 17, 18, 19]
+      expect(@hotel.available_rooms(start_date, end_date)).must_equal [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 15, 16, 17, 18, 19]
     end
 
     it "raises an Argument Error if tries to reserve room that is not available for a given day" do
-      expect { @hotel.reserve_room(338, Date.new(2001, 3, 5), Date.new(2001, 3, 17), 0) }.must_raise ArgumentError
+      expect { @hotel.reserve_room(Date.new(2001, 3, 5), Date.new(2001, 3, 17), 12) }.must_raise ArgumentError
     end
   end
 
-  #   describe "#hotel_block" do
-  #     before do
-  #       new_reservation = @hotel.reserve_room(333, Date.new(2001, 3, 5), Date.new(2001, 3, 7), 0)
-  #       new_reservation2 = @hotel.reserve_room(334, Date.new(2001, 3, 5), Date.new(2001, 3, 7), 1)
-  #       new_reservation3 = @hotel.reserve_room(335, Date.new(2001, 3, 9), Date.new(2001, 3, 15), 2)
-  #       new_reservation4 = @hotel.reserve_room(336, Date.new(2001, 3, 4), Date.new(2001, 3, 12), 12)
+  describe "#reserve_hotel_block" do
+    before do
+      new_reservation = @hotel.reserve_room(Date.new(2001, 3, 5), Date.new(2001, 3, 7), 0)
 
-  #       @hotel.add_reservation(new_reservation)
-  #       @hotel.add_reservation(new_reservation2)
-  #       @hotel.add_reservation(new_reservation3)
-  #       @hotel.add_reservation(new_reservation4)
+      @collection_rooms = [@rooms[0], @rooms[1], @rooms[2], @rooms[3], @rooms[4]]
+      start_date = Date.new(2001, 2, 5)
+      end_date = Date.new(2001, 2, 10)
+      @discounted_rate = 150
+      @hotel_block_reserved = @hotel.reserve_hotel_block(1, start_date, end_date, @collection_rooms, @discounted_rate)
+      expect(@hotel_block_reserved).must_be_kind_of Hotel::Block
+      @collection_rooms.each do |room|
+        room.add_block(@hotel_block_reserved)
+      end
+    end
+    it "raises an argument error if tries to create Hotel Block and one of the rooms is reserved for the given date range" do
+      start_date = Date.new(2001, 3, 5)
+      end_date = Date.new(2001, 3, 10)
+      expect { @hotel.reserve_hotel_block(1, start_date, end_date, @collection_rooms, @discounted_rate) }.must_raise ArgumentError
+    end
 
-  #       new_reservation.room.add_reservation(new_reservation)
-  #       new_reservation2.room.add_reservation(new_reservation2)
-  #       new_reservation3.room.add_reservation(new_reservation3)
-  #       new_reservation4.room.add_reservation(new_reservation4)
-  #     end
+    it "raises an error if tries to reserve a room that is set aside in a hotel block" do
+      start_date = Date.new(2001, 2, 6)
+      end_date = Date.new(2001, 2, 10)
+      expect { @hotel.reserve_room(start_date, end_date, @collection_rooms[0].id) }.must_raise ArgumentError
+    end
 
-  #     it "creates a Hotel Block that is added to #hotel_block array" do
-  #       # can be a hash
+    it "raises an error if tries to create a hotel block that includes a room that is already set aside in another hotel block for one of those days" do
+      @new_collection = [@collection_rooms[0], @rooms[5]]
+      start_date = Date.new(2001, 2, 6)
+      end_date = Date.new(2001, 2, 10)
+      expect { @hotel.reserve_hotel_block(1, start_date, end_date, @new_collection, @discounted_rate) }.must_raise ArgumentError
+    end
 
-  #       start_date = Date.new(2001, 2, 5)
-  #       end_date = Date.new(2001, 2, 10)
-  #       collection_rooms = [2, 3, 4, 5]
-  #       discounted_rate = 150
-  #       hotel_block_reserved = @hotel.reserve_hotel_block(1, start_date, end_date, collection_rooms, discounted_rate)
-  #       expect(@hotel.hotel_block.length).must_equal 1
-  #       expect(@hotel.hotel_block[0][:collection_rooms]).must_equal collection_rooms
-  #     end
+    it "is able to set aside a hotel block when the end date of one hotel block overlaps with the start date of another hotel block" do
+      @new_collection = [@collection_rooms[0], @rooms[5]]
+      start_date = Date.new(2001, 2, 10)
+      end_date = Date.new(2001, 2, 17)
+      expect (@hotel.reserve_hotel_block(1, start_date, end_date, @new_collection, @discounted_rate)).must_be_kind_of Hotel::Block
+    end
 
-  #     it "raises an argument error if tries to create Hotel Block and one of the rooms is unavailable for the given data range" do
-  #       start_date = Date.new(2001, 3, 5)
-  #       end_date = Date.new(2001, 3, 10)
-  #       collection_rooms = [0, 1, 2, 3, 4, 5]
-  #       discounted_rate = 150
-  #       expect { @hotel.reserve_hotel_block(1, start_date, end_date, collection_rooms, discounted_rate) }.must_raise ArgumentError
-  #     end
-  #   end
+    describe "#add_hotel_block" do
+      it "adds block to hotel instance variable @hotel_block" do
+        @hotel.add_hotel_block(@hotel_block_reserved)
+        expect(@hotel.hotel_blocks.length).must_equal 1
+        expect(@hotel.hotel_blocks[0]).must_be_kind_of Hotel::Block
+      end
+    end
+  end
 end
