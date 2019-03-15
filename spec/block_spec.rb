7e164a85daec24a40ddad165c3139f5ca128547c
Carla Bosco <carlabosco@gmail.com>
2019-03-15 14:39:03 -0700

    Set up method to block rooms

--- spec/block_spec.rb
+++ spec/block_spec.rb
@@ -5,6 +5,9 @@ require_relative '../lib/block'
 
 describe 'Block' do
   it 'blocks a number of rooms' do
-    
+    @booker = BookingCentral.new
+    new_block = Block.new(check_in:'2019-05-01', check_out: '2019-05-05', rooms: [1, 2, 3, 4, 5], discount_rate: 180)
+    expect((@booker.list_available_rooms('2019-05-01', '2019-05-05')).count).must_equal 15
+
   end
 end
