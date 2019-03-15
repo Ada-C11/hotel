ab2abafd06831a397acd6dc9dd1924953519eebc
Kate Sandler <kate@katekatekate.net>
2019-03-14 22:28:53 -0700

    Banished imps and created range variable in Reservation

--- lib/reservations.rb
+++ lib/reservations.rb
@@ -1,10 +1,11 @@
 module Hotel
   class Reservation
-    attr_reader :total, :check_in, :duration
+    attr_reader :check_in, :check_out, :range, :duration, :total
 
     def initialize(check_in, duration)
-      @date_range = DateRanges.new(check_in) + (check_in + @duration)
-      @check_in = check_in
+      @check_in = Date.parse(check_in)
+      @check_out = (Date.parse(check_in) + duration)
+      @range = (@check_in..@check_out)
       @duration = duration
       @total = COST * @duration
     end
