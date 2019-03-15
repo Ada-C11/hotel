c09eda234c1e61f2062a10c77d50df4141314bbe
Kate Sandler <kate@katekatekate.net>
2019-03-14 22:55:48 -0700

    Puts optional argument in for @room. Maybe I'll be back with a @block. Maybe.

--- lib/reservations.rb
+++ lib/reservations.rb
@@ -1,13 +1,14 @@
 module Hotel
   class Reservation
-    attr_reader :check_in, :check_out, :range, :duration, :total
+    attr_reader :check_in, :check_out, :range, :duration, :total, :room
 
-    def initialize(check_in, duration)
+    def initialize(check_in, duration, room = nil)
       @check_in = Date.parse(check_in)
       @check_out = (Date.parse(check_in) + duration)
       @range = (@check_in..@check_out)
       @duration = duration
       @total = COST * @duration
+      @room = room
     end
   end
 end
