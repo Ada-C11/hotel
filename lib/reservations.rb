388408673bb67532574bf6a0419a1f99a77e5a91
Kate Sandler <kate@katekatekate.net>
2019-03-14 21:58:06 -0700

    Puts out logic fire in reserve_room method.

--- lib/reservations.rb
+++ lib/reservations.rb
@@ -1,11 +1,10 @@
-require "datespans.rb"
-
 module Hotel
   class Reservation
     attr_reader :total, :check_in, :duration
 
     def initialize(check_in, duration)
-      @date_range = DateRanges.new(check_in) + @duration
+      @date_range = DateRanges.new(check_in) + (check_in + @duration)
+      @check_in = check_in
       @duration = duration
       @total = COST * @duration
     end
