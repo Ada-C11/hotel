a70b2d93b6662455b02170a2d6545a9aa8f5a76a
ranuseh <rosalyn.anuseh@gmail.com>
2019-03-15 12:42:54 -0700

    dry code for booking and hotel spec

--- spec/booking_spec.rb
+++ spec/booking_spec.rb
@@ -1,76 +1,46 @@
-require_relative "spec_helper"
+require_relative 'spec_helper'
 require 'date'
 
 describe 'Booking class' do
+  before do
+    @booking = HotelBooking::Booking.new(
+      reference_number: 1,
+      room: HotelBooking::Room.new(1),
+      start_date: '2001-02-03',
+      end_date: '2001-02-07',
+      price: 200
+      )
+  end
+  
   describe 'Booking instantiation' do
     it 'creates an instance of a booking' do
-      booking = HotelBooking::Booking.new(
-        reference_number: 1,
-        room: HotelBooking::Room.new(1),
-        start_date: Date.parse('2001-02-03'),
-        end_date: Date.parse('2001-02-07'),
-        price: 200
-        )
-
-      expect(booking).must_be_kind_of HotelBooking::Booking
+      expect(@booking).must_be_kind_of HotelBooking::Booking
     end
-  
 
-    it "is set up for specific attributes and data types" do
-      booking = HotelBooking::Booking.new(
-        reference_number: 1,
-        room: HotelBooking::Room.new(1),
-        start_date: Date.parse('2001-02-03'),
-        end_date: Date.parse('2001-02-07'),
-        price: 200
-        )
-      expect(booking.room).must_be_kind_of HotelBooking::Room
-      expect(booking.start_date).must_be_kind_of String
-      expect(booking.end_date).must_be_kind_of String
-      expect(booking.price).must_equal 200
+    it 'is set up for specific attributes and data types' do
+      expect(@booking.room).must_be_kind_of HotelBooking::Room
+      expect(@booking.start_date).must_be_kind_of String
+      expect(@booking.end_date).must_be_kind_of String
+      expect(@booking.price).must_equal 200
     end
   end
 
   describe 'booking duration' do
-    it "totals the duration of booking in days" do
-      booking = HotelBooking::Booking.new(
-        reference_number: 1,
-        room: HotelBooking::Room.new(1),
-        start_date: Date.parse('2001-02-03'),
-        end_date: Date.parse('2001-02-07'),
-        price: 200
-        )
-      expect(booking.booking_duration).must_equal 4
+    it 'totals the duration of booking in days' do
+      expect(@booking.booking_duration).must_equal 4
     end
-end
+  end
 
   describe 'booking cost' do
-    it "given a booking, can calculate total cost" do
-      booking = HotelBooking::Booking.new(
-        reference_number: 1,
-        room: HotelBooking::Room.new(1),
-        start_date: Date.parse('2001-02-03'),
-        end_date: Date.parse('2001-02-07'),
-        price: 200
-        )
-      expect(booking.booking_cost).must_equal 800
+    it 'given a booking, can calculate total cost' do
+      expect(@booking.booking_cost).must_equal 800
     end
   end
 
   describe 'overlap?' do
-    it "parse string into date object" do
-      booking = HotelBooking::Booking.new(
-        reference_number: 1,
-        room: HotelBooking::Room.new(1),
-        start_date: '2001-02-03',
-        end_date: '2001-02-07',
-        price: 200
-        )
-
-        result = booking.overlaps?('2001-02-03', '2001-02-07')
+    it 'parse string into date object' do
+      result = @booking.overlaps?('2001-02-03', '2001-02-07')
       expect(result).must_equal true
     end
   end
-
 end
-
--- spec/hotel_spec.rb
+++ spec/hotel_spec.rb
@@ -1,42 +1,42 @@
-require_relative "spec_helper"
+require_relative 'spec_helper'
 
 describe 'Hotel class' do
+  before do
+    @hotel = HotelBooking::Hotel.new(hotel_name: 'Wyndham', number_of_rooms: 20)
+  end
+  
   describe 'Hotel instantiation' do
     it 'creates an instance of hotel' do
-      hotel = HotelBooking::Hotel.new(hotel_name: 'Wyndham', number_of_rooms: 20)
-      expect(hotel).must_be_kind_of HotelBooking::Hotel
+      expect(@hotel).must_be_kind_of HotelBooking::Hotel
     end
 
-    it "is set up for specific attributes and data types" do
-      hotel = HotelBooking::Hotel.new(hotel_name: 'Wyndham', number_of_rooms: 20)
-      expect(hotel.hotel_name).must_be_kind_of String
-      expect(hotel.number_of_rooms).must_be_kind_of Integer
-      expect(hotel.rooms).must_be_kind_of Array
-      expect(hotel.bookings).must_be_kind_of Array
+    it 'is set up for specific attributes and data types' do
+      expect(@hotel.hotel_name).must_be_kind_of String
+      expect(@hotel.number_of_rooms).must_be_kind_of Integer
+      expect(@hotel.rooms).must_be_kind_of Array
+      expect(@hotel.bookings).must_be_kind_of Array
     end
 
-    it "sets rooms to an empty array if not provided" do
+    it 'sets rooms to an empty array if not provided' do
       hotel = HotelBooking::Hotel.new(hotel_name: 'Wyndham', number_of_rooms: 0)
       expect(hotel.rooms).must_be_kind_of Array
       expect(hotel.rooms.length).must_equal 0
     end
 
-    it "sets bookings to an empty array if not provided" do
-      hotel = HotelBooking::Hotel.new(hotel_name: 'Wyndham', number_of_rooms: 0)
-      expect(hotel.bookings).must_be_kind_of Array
-      expect(hotel.bookings.length).must_equal 0
+    it 'sets bookings to an empty array upon instantiation' do
+      expect(@hotel.bookings).must_be_kind_of Array
+      expect(@hotel.bookings.length).must_equal 0
     end
 
-    it "stores an instance of a room" do
+    it 'stores an instance of a room' do
       hotel = HotelBooking::Hotel.new(hotel_name: 'Wyndham', number_of_rooms: 0)
-      room = HotelBooking::Room.new(1)
+      room = HotelBooking::Room.new(0)
 
       hotel.add_room(room)
       expect(hotel.rooms.first).must_equal room
     end
 
-    it "stores an instance of a booking" do
-      hotel = HotelBooking::Hotel.new(hotel_name: 'Wyndham', number_of_rooms: 0)
+    it 'stores an instance of a booking' do
       booking = HotelBooking::Booking.new(
         reference_number: 1,
         room: 1,
@@ -45,8 +45,8 @@ describe 'Hotel class' do
         price: 200
         )
 
-        hotel.add_booking(booking)
-      expect(hotel.bookings.first).must_equal booking
+        @hotel.add_booking(booking)
+      expect(@hotel.bookings.first).must_equal booking
     end
 
   end
