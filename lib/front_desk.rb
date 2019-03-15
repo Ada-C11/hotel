6c89668acbecdac8b64672adbfc714d1b6d300e2
Faiza Husain <faiza.ahsan1222@gmail.com>
2019-03-15 01:48:54 -0700

    Added a new reservation_template method to FrontDesk. Modified make_reservation method to check for overlapping dates for room reservations and to raise error if dates overlap.

--- lib/front_desk.rb
+++ lib/front_desk.rb
@@ -2,8 +2,6 @@ require_relative "reservation"
 
 module Hotel
   class FrontDesk
-    include Hotel
-
     attr_reader :all_rooms, :reservations
     attr_accessor :all_rooms, :reservations
 
@@ -20,30 +18,45 @@ module Hotel
       return @reservations
     end
 
-    def make_reservation(booking_ref, room_number, start_date, end_date)
-      reserve = Hotel::Reservation.new(
-        booking_ref: booking_ref,
-        room_number: room_number,
-        start_date: start_date,
-        end_date: end_date,
+    def reservation_template(new_booking_ref, new_room_number, new_start_date, new_end_date)
+      booking = Hotel::Reservation.new(
+        booking_ref: new_booking_ref,
+        room_number: new_room_number,
+        start_date: new_start_date,
+        end_date: new_end_date,
         total_cost: @total_cost,
       )
-
-      return reserve
+      return booking
     end
+
+    def make_reservation(new_booking_ref, new_room_number, new_start_date, new_end_date)
+      if @reservations.length == 0
+        booking = reservation_template(new_booking_ref, new_room_number, new_start_date, new_end_date)
+      else
+        for i in (0...@reservations.length)
+          if (@reservations[i].room_number == new_room_number)
+            if ((new_start_date >= @reservations[i].start_date) && (new_start_date < @reservations[i].end_date)) ||
+               ((new_end_date >= @reservations[i].end_date) && (new_end_date < @reservations[i].end_date))
+              raise ArgumentError, "This room is unavailable."
+            else
+              booking = reservation_template(new_booking_ref, new_room_number, new_start_date, new_end_date)
+            end # inner if
+          else
+            booking = reservation_template(new_booking_ref, new_room_number, new_start_date, new_end_date)
+          end # outer if
+        end # for end
+      end
+
+      return booking
+    end # method end
 
     def add_reservation(room_reservation)
       @reservations.push(room_reservation)
-      puts "VVVVVVVVVVVVVV"
-      puts @reservations
-      puts "^^^^^^^^^^^^^^"
     end
 
     def get_reservation_by_date(start_date, end_date)
       reservations_by_date = []
-      puts "vvvvvvvvvvvvvvvv"
-      puts @reservations
-      puts "^^^^^^^^^^^^^^^"
+
       for i in (0...@reservations.length)
         if (@reservations[i].start_date == start_date) || (@reservations[i].end_date == end_date)
           reservations_by_date.push(@reservations[i])
@@ -52,30 +65,33 @@ module Hotel
       return reservations_by_date
     end
 
-    def is_room_available(start_date, end_date)
+    def room_availablity(start_date, end_date)
       puts "is_room_available:"
+
       reservations_at_date = get_reservation_by_date(start_date, end_date)
+
       puts "CURRENT ROOMS"
       puts "room_in_question: #{reservations_at_date}"
-      #puts current_reservations[0].room_number
       puts "^^^^^^^^^^^^^^^"
 
       puts "@RESERVATIONS"
       puts "#{@reservations.length} reservations"
       puts "^^^^^^^^^^^^^^^"
+
       available_rooms = []
+
       for room_idx in (0..@all_rooms.length - 1)
         room_booked = false
         for res_idx in (0..reservations_at_date.length - 1)
-          resrved_room = reservations_at_date[res_idx].room_number
+          reserved_room = reservations_at_date[res_idx].room_number
           room_in_question = @all_rooms[room_idx]
-          if resrved_room == room_in_question
+          if (reserved_room == room_in_question)
             room_booked = true
             break
           end
         end
 
-        if room_booked == false
+        if (room_booked == false)
           available_rooms << @all_rooms[room_idx]
         end
       end
@@ -90,37 +106,7 @@ module Hotel
       return available_rooms
     end
 
-    # reserved_rooms = get_reservation_by_date(start_date, end_date).map { |reservation|
-    #   reservations_by_date[reservation].room_number
-    # }
-
-    # available_rooms = @all_rooms.reject { |rm_num| reserved_rooms.include? rm_num }
-
-    # add_reservation(make_reservation(@reservations.length + 1, 1, Date.new(2019, 3, 3), Date.new(2019, 3, 15)))
-
-    # add_reservation(Hotel.make_reservation(@reservations.length + 1, 2, Date.today + 1, Date.new(2019, 3, 18)))
-
-    # add_reservation(Hotel.make_reservation(@reservations.length + 1, 2, Date.today + 1, Date.new(2019, 3, 17)))
-
-    # add_reservation(Hotel.make_reservation(@reservations.length + 1, 2, Date.today + 1, Date.new(2019, 3, 16)))
-
-    # add_reservation(Hotel.make_reservation(@reservations.length + 1, 2, Date.today + 1, Date.new(2019, 3, 15)))
-
-    # date_reserves = get_reservation_by_date(Date.today + 1)
-
-    # puts date_reserves[0].start_date
-    # puts date_reserves[0].end_date
-
-    # # puts @@reservations[0].total_cost
-    # # puts @@reservations[1].total_cost
-    # # puts @@reservations[2].total_cost
-    # puts @@reservations[3].total_cost
-
-    # puts @@reservations.length
-
-    # for i in (0...@@reservations.length)
-    #   puts @@reservations[i].end_date
-    # end
+    # concierge = make_reservation(1001, 3, Date.today, Date.today + 5)
 
   end # Class end
 end # Hotel end
--- spec/front_desk_spec.rb
+++ spec/front_desk_spec.rb
@@ -9,7 +9,7 @@ describe "class FrontDesk" do
     before do
       @rooms = build_front_desk
     end
-
+    # TEST 1
     it "will say all_rooms is an array with a length of 20" do
       expect(@rooms.all_rooms_array).must_be_kind_of Array
       expect(@rooms.all_rooms_array.length).must_equal 20
@@ -20,6 +20,7 @@ describe "class FrontDesk" do
     before do
       @new_reservation = build_front_desk
     end
+    # TEST 2
     it "adds reservation to reservations array" do
       reservation_1 = @new_reservation.make_reservation(
         1201,
@@ -27,9 +28,10 @@ describe "class FrontDesk" do
         Date.new(2019, 3, 20),
         Date.new(2019, 3, 22),
       )
-      puts "VVVVVVVVVVVVVV"
-      puts reservation_1
-      puts "^^^^^^^^^^^^^^"
+      # puts "VVVVVVVVVVVVVV"
+      # puts "reservation_1: #{reservation_1}"
+      # puts "^^^^^^^^^^^^^^"
+
       @new_reservation.add_reservation(reservation_1)
       expect(@new_reservation.reservations_array.length).must_be :>, 0
       expect(@new_reservation.reservations_array[0].total_cost).must_equal 400
@@ -40,6 +42,7 @@ describe "class FrontDesk" do
     before do
       @specific_reservation = build_front_desk
     end
+    # TEST 3
     it "will get a reservation by a date" do
       reservation_2 = @specific_reservation.make_reservation(
         1011,
@@ -47,42 +50,67 @@ describe "class FrontDesk" do
         Date.new(2019, 4, 8),
         Date.new(2019, 4, 10),
       )
-      puts "VVVVVVVVVVVVVV"
-      puts reservation_2
-      puts "^^^^^^^^^^^^^^"
+      # puts "VVVVVVVVVVVVVV"
+      # puts reservation_2
+      # puts "^^^^^^^^^^^^^^"
       @specific_reservation.add_reservation(reservation_2)
 
       by_date = @specific_reservation.get_reservation_by_date(Date.new(2019, 4, 8), Date.new(2019, 4, 10))
 
-      puts "BY DATE"
-      puts by_date
-      puts "^^^^^^^^^^^^^^"
+      # puts "BY DATE"
+      # puts by_date
+      # puts "^^^^^^^^^^^^^^"
       expect(by_date).must_be_kind_of Array
       expect(by_date[0].start_date).must_equal Date.new(2019, 4, 8)
     end
   end
 
-  describe "is_room_available method" do
+  describe "room_availablity method" do
     before do
       @check_rooms = build_front_desk
     end
-
+    # TEST 4
     it "will return array of available rooms" do
       reservation_3 = @check_rooms.make_reservation(
         1001,
         3,
         Date.new(2019, 4, 8),
-        Date.new(2019, 4, 10),
+        Date.new(2019, 4, 21),
       )
       @check_rooms.add_reservation(reservation_3)
 
-      all_available_rooms = @check_rooms.is_room_available(
+      all_available_rooms = @check_rooms.room_availablity(
         Date.new(2019, 4, 8),
-        Date.new(2019, 4, 10)
+        Date.new(2019, 4, 21)
       )
 
       expect(all_available_rooms).must_be_kind_of Array
       expect(all_available_rooms.length).must_equal 19
+    end
+  end
+
+  # TEST 5
+  describe "make reservation for a given date range" do
+    before do
+      @avoid_double_booking = build_front_desk
+
+      @avoid_double_booking.add_reservation(@avoid_double_booking.make_reservation(
+        3000,
+        3,
+        Date.new(2019, 7, 1),
+        Date.new(2019, 7, 5)
+      ))
+    end
+
+    it "will raise an error if a reserved room's dates overlap" do
+      expect {
+        @avoid_double_booking.make_reservation(
+          3001,
+          3,
+          Date.new(2019, 7, 1),
+          Date.new(2019, 7, 5)
+        )
+      }.must_raise ArgumentError
     end
   end
 end
