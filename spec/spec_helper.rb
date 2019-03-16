babab8bb325665f93d424f7387c171f92f950413
Heather Izumi <hkizumi@gmail.com>
2019-03-15 21:23:22 -0700

    updated spec helper for block_room class

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,4 +12,4 @@ Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 # Require_relative your lib files here!
 require_relative "../lib/reservation_manager.rb"
 require_relative "../lib/reservation.rb"
-require_relative "../lib/room.rb"
+require_relative "../lib/block_room.rb"
