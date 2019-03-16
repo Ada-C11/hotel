6f37ecf736d028e4777eb407f809e59201484177
qqdipps <sdipp007@gmail.com>
2019-03-16 03:39:40 -0700

    added tests of confirmation id generation

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -7,5 +7,12 @@ module Hotel
     def initialize(check_in:, check_out:)
       super(check_in: check_in, check_out: check_out)
     end
+
+    private
+
+    #Override
+    def generate_confirmation_id(prefix: "R")
+      return prefix + self.class.confirmation_number_generator.to_s
+    end
   end
 end
