3a3fa35cfa160f1cfb2b078013c48b211cf6d555
Mello-Cello <celloelle@gmail.com>
2019-03-15 20:02:46 -0700

    Added calculations for reservation date range, nights of stay, and cost of the reservation. Wrote tests for same. All passing.

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -2,29 +2,33 @@ require "date"
 
 module Hotel
   class Reservation
-    attr_reader :ckin_date, :ckout_date
+    attr_reader :ckin_date, :ckout_date, :date_range_string_array, :num_nights_of_stay, :base_cost
 
     def initialize(ckin_date, ckout_date)
       @ckin_date = ckin_date
       @ckout_date = ckout_date
+      @date_range_string_array = (ckin_date..ckout_date).map(&:to_s)
+      # This creates an array of strings for each date in the range. For example:
+      # ['2019-06-11', '2019-06-12', '2019-06-13', '2019-06-14']
+      @num_nights_of_stay = (date_range_string_array.length - 1)
+      @base_cost = num_nights_of_stay * Room::COST_PER_NIGHT
 
-      begin
-        compare_dates = ckin_date <=> ckout_date
-      rescue
-        raise Exception if ckin_date >= ckout_date
-      end
+      raise ArgumentError, "Check out date must be after check in date" if ckin_date >= ckout_date
     end
+  end
+
+  def calculate_length_of_stay
   end
 end
 
 # ^^^^^ write code here ^^^^^
 
-# start_date = Date.new(2018, 6, 3)
-# end_date = Date.new(2018, 6, 7)
+start_date = Date.new(2018, 6, 3)
+end_date = Date.new(2018, 6, 7)
 
-# puts start_date <=> end_date
+puts start_date <=> end_date
 
-# range = (start_date..end_date).map(&:to_s)
-# p range
+range = (start_date..end_date).map(&:to_s)
+p range
 
-# puts Date.parse(range[0])
+puts Date.parse(range[0])
--- spec/reservation_spec.rb
+++ spec/reservation_spec.rb
@@ -4,15 +4,26 @@ describe "Reservation Class" do
   let (:this_res) {
     check_in = Date.new(2019, 06, 03)
     check_out = Date.new(2019, 06, 07)
-    test_res = Hotel::Reservation.new(check_in, check_out)
+    this_res = Hotel::Reservation.new(check_in, check_out)
   }
   it "Initialize a Reservation" do
     expect(this_res).must_be_instance_of Hotel::Reservation
   end
 
-  it "Makes a reservation for given dates" do
+  it "Make a reservation for given dates" do
     check_in_date = this_res.ckin_date.to_s
     expect(check_in_date).must_equal "2019-06-03"
+  end
+
+  it "Raise argument error if check in date is >= (after or the same as) check out date" do
+    check_in = Date.new(2019, 06, 03)
+    check_out = Date.new(2019, 06, 03)
+
+    expect { Hotel::Reservation.new(check_in, check_out) }.must_raise ArgumentError
+  end
+
+  it "Calculate and store base cost for a reservation" do
+    expect(this_res.base_cost).must_equal 800
   end
   # TESTING DATE CONVERSION
   # INSTANCE OF DATE OBJECT TO STRING
