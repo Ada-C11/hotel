9b79a0f2c7e2d453a2035ef74d4111e6d36fdb4e
Elise Pham <elise.pham88@gmail.com>
2019-03-15 21:23:42 -0700

    added more tests for scheduler class

--- lib/scheduler.rb
+++ lib/scheduler.rb
@@ -1,9 +1,9 @@
 class Scheduler
   attr_reader :room_map
 
-  def initialize
+  def initialize(num_rooms)
     @room_map = Hash.new
-    20.times do |i|
+    num_rooms.times do |i|
       room_id = i + 1
       @room_map[room_id] = Room.new(room_id)
     end
@@ -15,5 +15,25 @@ class Scheduler
 
   def reserve_room(room_id, date_range)
     @room_map[room_id].reserve(date_range)
+  end
+
+  def get_reservations(date)
+    list = Array.new
+    @room_map.each_key { |id|
+      @room_map[id].get_reservations_on_date(date).each do |reservation|
+        list << reservation.room_id
+      end
+    }
+    return list
+  end
+
+  def get_available_rooms(date_range)
+    list = Array.new
+    @room_map.each_key { |id|
+      if @room_map[id].is_available?(date_range)
+        list << @room_map[id].room_id
+      end
+    }
+    return list
   end
 end
--- spec/scheduler_spec.rb
+++ spec/scheduler_spec.rb
@@ -4,7 +4,7 @@ require 'date'
 describe "Scheduler class" do
   describe "initialize" do
     let(:scheduler1) {
-      Scheduler.new
+      Scheduler.new(20)
     }
 
     it "returns an instance of scheduler" do
@@ -19,7 +19,7 @@ describe "Scheduler class" do
 
   describe "get_room_ids method" do
     let(:scheduler2) {
-      Scheduler.new
+      Scheduler.new(20)
     }
 
     it "returns a list of ids for all rooms in the hotel" do
@@ -33,14 +33,66 @@ describe "Scheduler class" do
 
   describe "reserve_room method" do
     let(:scheduler3) {
-      Scheduler.new
+      Scheduler.new(1)
     }
 
     it "adds a new reservation to a given room" do
-      list_length_before = scheduler3.room_map[20].reservations.length
+      list_length_before = scheduler3.room_map[1].reservations.length
       date_range = Time_Interval.new(Date.parse("2019-06-12"), Date.parse("2019-06-15"))
-      scheduler3.reserve_room(20, date_range)
-      expect(scheduler3.room_map[20].reservations.length).must_equal list_length_before + 1
+      scheduler3.reserve_room(1, date_range)
+      expect(scheduler3.room_map[1].reservations.length).must_equal list_length_before + 1
     end
   end
+
+  describe "get_reservations method" do
+    let(:scheduler4) {
+      Scheduler.new(3)
+    }
+
+    it "returns a list of ids of reserved rooms for a given date" do
+      date_range1 = Time_Interval.new(Date.parse("2019-06-12"), Date.parse("2019-06-15"))
+      scheduler4.reserve_room(1, date_range1)
+      scheduler4.reserve_room(2, date_range1)
+      scheduler4.reserve_room(3, date_range1)
+
+      room_ids = [1, 2, 3]
+      return_list = scheduler4.get_reservations(Date.parse("2019-06-12"))
+      expect(scheduler4.get_reservations(Date.parse("2019-06-12"))).must_equal room_ids
+    end
+  end
+
+  describe "get_available_rooms method" do
+    it "returns a list of one room_id when two rooms are booked in a three room hotel" do
+      scheduler = Scheduler.new(3)
+      scheduler.reserve_room(1, Time_Interval.new(Date.parse("2019-12-10"), Date.parse("2019-12-15")))
+      scheduler.reserve_room(2, Time_Interval.new(Date.parse("2019-12-10"), Date.parse("2019-12-15")))
+
+      return_list = scheduler.get_available_rooms(Time_Interval.new(Date.parse("2019-12-10"), Date.parse("2019-12-15")))
+
+      expect(return_list.length).must_equal 1
+      expect(return_list[0]).must_equal 3
+    end
+
+    it "returns a list of two room_ids when one room is booked in a three room hotel" do
+      scheduler = Scheduler.new(3)
+      scheduler.reserve_room(1, Time_Interval.new(Date.parse("2019-12-10"), Date.parse("2019-12-15")))
+
+      return_list = scheduler.get_available_rooms(Time_Interval.new(Date.parse("2019-12-10"), Date.parse("2019-12-15")))
+
+      expect(return_list.length).must_equal 2
+      expect(return_list[0]).must_equal 2
+      expect(return_list[1]).must_equal 3
+    end
+
+    it "returns an empty list when no room is available in a three room hotel" do
+      scheduler = Scheduler.new(3)
+      scheduler.reserve_room(1, Time_Interval.new(Date.parse("2019-12-10"), Date.parse("2019-12-15")))
+      scheduler.reserve_room(2, Time_Interval.new(Date.parse("2019-12-10"), Date.parse("2019-12-15")))
+      scheduler.reserve_room(3, Time_Interval.new(Date.parse("2019-12-10"), Date.parse("2019-12-15")))
+      return_list = scheduler.get_available_rooms(Time_Interval.new(Date.parse("2019-12-10"), Date.parse("2019-12-15")))
+
+      expect(return_list.length).must_equal 0
+    end
+  end
+
 end
