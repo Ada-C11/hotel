b5129785265ef2b6fec1e4c265fa2d643f112777
Angela <angela.ohh@gmail.com>
2019-03-15 17:14:11 -0700

    added ability to make reservation for hotel block

--- lib/reservation_manager.rb
+++ lib/reservation_manager.rb
@@ -2,13 +2,18 @@ require "pry"
 require "date"
 
 class Reservation_Manager
-  attr_reader :rooms, :all_reservations, :available_rooms, :all_blocks
+  attr_reader :rooms, :all_reservations, :available_rooms
+  attr_accessor :all_block_reservations
 
   def initialize
     @all_reservations = all_reservations || []
-    @all_blocks = all_blocks || []
+
+    #this is simply a list of rooms, regardless of availability
     # @available_rooms = (1..20).map { |i| i }
-    # @all_rooms = all_rooms
+
+    #these are not actual reservations but rooms set aside
+    #TODO: change variable name to reflect above
+    @all_block_reservations = all_block_reservations || []
   end
 
   def parse_dates(date_1, date_2)
@@ -17,56 +22,77 @@ class Reservation_Manager
   end
 
   ##TODO: break up method into mini methods (SRP)
-  def make_reservation(check_in, check_out)
+  def make_reservation(check_in, check_out, is_hotel_blocker: false, block_id: nil)
+    # TODO: BETTER NAMES FOR CHECK IN/OUT TIME
     fixed_check_in = Date.parse(check_in)
     fixed_check_out = Date.parse(check_out)
 
-    ## TO DO: think about changing id
-    reservation = Reservation.new(1)
-    reservation.check_in = fixed_check_in
-    reservation.check_out = fixed_check_out
-    ##TODO: should people be able to make a reservation for one day, same checkin/checkout date?
-    if reservation.check_out - reservation.check_in <= 0
-      raise ArguementError, "Check out time is not after check in time. Inputted check in date was #{check_in} and check out date was #{check_out}"
-    end
+    if is_hotel_blocker == true
+      #reservation_id for hotel block rooms are all the same
+      reservation = Reservation.new(block_id, is_hotel_blocker: true)
 
-    available_rooms = find_available_rooms(check_in, check_out)
-    if available_rooms.length > 0
-      reservation.room = available_rooms[0]
-    else
-      raise ArgumentError, "There are no available rooms at the moment. Please try again later!"
+      # find the hotel block date range user is trying to reserve for
+      # reassign checkin/checkout date to match block dates
+      all_block_reservations.each do |block_reservation|
+        if block_id == block_reservation.reservation_id
+          reservation.check_in = block_reservation.check_in
+          reservation.check_out = block_reservation.check_out
+        end
+      end
+
+      #reassign room for hotel blocker and erase hold for room
+      rooms_for_block = all_block_reservations.map { |block_instance| block_instance.room }
+      reservation.room = rooms_for_block[0]
+      all_block_reservations.shift
+    else #IF RESERVATION IS OUTSIDE OF BLOCK
+      reservation = Reservation.new(1)
+      reservation.check_in = fixed_check_in
+      reservation.check_out = fixed_check_out
+
+      available_rooms = find_available_rooms(check_in, check_out)
+      if available_rooms.length > 0
+        reservation.room = available_rooms[0]
+      else
+        raise ArgumentError, "There are no available rooms at the moment. Please try again later!"
+      end
     end
 
     all_reservations << reservation
     return reservation
   end
 
-  def make_hotel_block(check_in, check_out, room_num)
-    hotel_block = Hotel_Block.new(check_in, check_out, room_num)
-    available_block_rooms = find_available_rooms(check_in, check_out)
-
-    block_rooms = available_block_rooms.take(room_num)
-    # binding.pry
-
-    #
-
-    #this adds blocked rooms into @all_rooms
-    if available_rooms.length > 0
-      block_rooms.each do |room|
-        all_blocks << room
-      end
+  def make_hotel_block(block_id, check_in, check_out, room_num)
+    # fixed_check_in = Date.parse(check_in)
+    # fixed_check_out = Date.parse(check_out)
+    if room_num > 5
+      raise ArgumentError, "You cannot make a hotel block of larger than 5 rooms at at time."
     end
 
-    return block_rooms
+    #find list of available rooms.
+    available_block_rooms = find_available_rooms(check_in, check_out)
 
-    # returns rooms in the block
-    # updates available_rooms to not include these blocks
-    # must still be reservable for the length of time
+    #grabs number of rooms from available room list - for hotel block users only if there are enough rooms left
+    if available_block_rooms.length >= room_num
+      block_rooms = available_block_rooms.take(room_num)
+      # [1,2,3,4,5]
+
+      #adds blocked rooms into all_block_reservations array
+      block_rooms.each do |num|
+        new_block_room = Reservation.new(block_id, check_in: check_in, check_out: check_out, is_hotel_blocker: true)
+        new_block_room.room = num
+        all_block_reservations << new_block_room
+        # binding.pry
+      end
+    else
+      raise ArgumentError, "Sorry, we don't have enough rooms available for your rooms in a block."
+    end
+
+    # should return room numbers
+    return block_rooms
   end
 
   def find_available_rooms(finding_check_in, finding_check_out)
-    @available_rooms = (1..20).map { |i| i }
-    # binding.pry
+    available_rooms = (1..20).map { |i| i }
     date1 = Date.parse(finding_check_in)
     date2 = Date.parse(finding_check_out)
     if date1 == date2
@@ -75,11 +101,13 @@ class Reservation_Manager
       given_date_range = (date1...date2).to_a
     end
 
+    #making make_reservation rooms unavailable
     unavailable_rooms = []
     all_reservations.each do |reservation|
       day_in = reservation.check_in
       day_out = reservation.check_out
       reserve_date_range = (day_in...day_out).to_a
+
       combined_ranges = (given_date_range + reserve_date_range).flatten
 
       if combined_ranges.length != combined_ranges.uniq.length
@@ -93,7 +121,39 @@ class Reservation_Manager
       end
     end
 
+    #find hotel block rooms for specific date
+    #hotel_block is array of rooms blocked per dates
+    hotel_block = []
+    all_block_reservations.each do |reservation|
+      day_in = reservation.check_in
+      day_out = reservation.check_out
+      reserve_date_range = (day_in...day_out).to_a
+      combined_ranges = (given_date_range + reserve_date_range).flatten
+
+      if combined_ranges.length != combined_ranges.uniq.length
+        hotel_block << reservation
+      end
+    end
+
+    #make hotel_block rooms unavailable
+    #take away hotel block rooms from the available_rooms
+    if hotel_block.length > 0
+      hotel_block.each do |block_reservation|
+        available_rooms.delete_if { |room_num| room_num == block_reservation.room }
+      end
+    end
+
     return available_rooms
+  end
+
+  def find_block_availability(block_id)
+    block_rooms = []
+    all_block_reservations.each do |held_reservations|
+      if block_id == held_reservations.reservation.id
+        block_rooms << held_reservations.room
+      end
+    end
+    return block_rooms
   end
 
   def find_reservation_date(check_in, check_out)
@@ -111,6 +171,7 @@ class Reservation_Manager
         found_reservations << reservation
       end
     end
+
     return found_reservations
   end
 
--- spec/reservation_mananger_spec.rb
+++ spec/reservation_mananger_spec.rb
@@ -2,6 +2,7 @@ require_relative "spec_helper"
 describe "RESERVATION MANAGER TESTS" do
   describe "Reservation Manager class initialization & set up" do
     it "will return an instance of Reservation_Manager" do
+      # skip
       test_manager = Reservation_Manager.new
       expect(test_manager).must_be_kind_of Reservation_Manager
     end
@@ -9,6 +10,7 @@ describe "RESERVATION MANAGER TESTS" do
 
   describe "#make_reservation in Reservation Manager" do
     it "will contain a collection of Reservation instances" do
+      # skip
       test_manager = Reservation_Manager.new
       check_in = "2019-3-15"
       check_out = "2019-3-20"
@@ -20,6 +22,7 @@ describe "RESERVATION MANAGER TESTS" do
       expect(test_manager.all_reservations[0]).must_be_kind_of Reservation
     end
     it "one new reservation updates all_reservations" do
+      # skip
       #initialize
       test_manager = Reservation_Manager.new
 
@@ -29,6 +32,7 @@ describe "RESERVATION MANAGER TESTS" do
     end
 
     it "multiple reservations returns the correct number of reservations" do
+      # skip
       test_manager = Reservation_Manager.new
 
       test_manager.make_reservation("2019-3-15", "2019-3-20")
@@ -39,6 +43,7 @@ describe "RESERVATION MANAGER TESTS" do
     end
 
     it "can make reservation when check in is another check out" do
+      # skip
       test_manager = Reservation_Manager.new
       test_manager.make_reservation("2019-3-15", "2019-3-20")
       test_manager.make_reservation("2019-3-20", "2019-3-21")
@@ -57,18 +62,20 @@ describe "RESERVATION MANAGER TESTS" do
     end
 
     it "must have different rooms if check in is day before another check out" do
+      # skip
       test_manager = Reservation_Manager.new
       reserve_1 = test_manager.make_reservation("2019-3-15", "2019-3-20")
       reserve_2 = test_manager.make_reservation("2019-3-19", "2019-3-21")
 
-      puts "reserve 1 room: #{reserve_1}"
-      puts "reserve 2 room: #{reserve_2}"
+      # puts "reserve 1 room: #{reserve_1}"
+      # puts "reserve 2 room: #{reserve_2}"
       expect(reserve_1.room).wont_equal reserve_2.room
     end
   end
 
   describe "#find_reservation_date" do
     it "will return collection of Reservations when given a specific date range" do
+      # skip
       test_manager = Reservation_Manager.new
 
       test_manager.make_reservation("2019-3-15", "2019-3-20")
@@ -81,11 +88,32 @@ describe "RESERVATION MANAGER TESTS" do
       expect(test_manager.find_reservation_date(check_in, check_out)).must_include test_manager.all_reservations[-2]
       expect(test_manager.find_available_rooms(check_in, check_out).length).must_equal 18
     end
+
+    ### THIS IS NOT WORKINGGGGGG!!!!!
+    it "can find reservations made via regular and hotel block reservation" do
+      skip
+      test_manager = Reservation_Manager.new
+      # check_in = Date.parse("2019-3-15")
+      # check_out = Date.parse("2019-3-20")
+
+      puts "I'M HEREEEEEEE!"
+      room_num = 5
+      block_id = 7
+      test_manager.make_hotel_block(rand(1..6), "2019-3-15", "2019-3-20", room_num)
+      test_manager.make_reservation("2019-3-15", "2019-3-20")
+      test_manager.make_reservation("2019-3-15", "2019-3-20", is_hotel_blocker: true, block_id: block_id)
+
+      # test_manager.make_reservation("2019-3-15", "2019-3-20")
+
+      puts "This is all_reservations: #{test_manager.all_reservations}"
+      expect(test_manager.find_reservation_date("2019-3-15", "2019-3-20").length).must_equal 2
+    end
     # TODO: think of more tests for #find_reservation?
   end
 
   describe "#find_available_rooms" do
     it "returns list of available rooms - dates don't coincide" do
+      # skip
       test_manager = Reservation_Manager.new
       check_in = "2019-4-5"
       check_out = "2019-4-7"
@@ -98,6 +126,7 @@ describe "RESERVATION MANAGER TESTS" do
     end
 
     it "returns list of available rooms - dates overlap" do
+      # skip
       test_manager = Reservation_Manager.new
       test_manager.make_reservation("2019-3-15", "2019-3-20")
       test_manager.make_reservation("2019-3-13", "2019-3-19")
@@ -128,9 +157,10 @@ describe "RESERVATION MANAGER TESTS" do
       check_in = "2019-4-5"
       check_out = "2019-4-5"
       room_num = 5
-      testing_block = test_manager.make_hotel_block(check_in, check_out, room_num)
+      block_id = rand(1..6)
+      testing_block = test_manager.make_hotel_block(block_id, check_in, check_out, room_num)
 
-      puts "this is testing_block: #{testing_block}"
+      # puts "this is testing_block: #{testing_block}"
       expect(testing_block.length).must_equal 5
       expect(testing_block).must_be_kind_of Array
     end
@@ -140,23 +170,204 @@ describe "RESERVATION MANAGER TESTS" do
       check_in = "2019-4-5"
       check_out = "2019-4-10"
       room_num = 5
-      testing_block1 = test_manager.make_hotel_block(check_in, check_out, room_num)
+      block_id = rand(1..6)
+      test_manager.make_hotel_block(block_id, check_in, check_out, room_num)
 
       check_in = "2019-3-5"
       check_out = "2019-3-10"
       room_num = 5
-      testing_block2 = test_manager.make_hotel_block(check_in, check_out, room_num)
+      test_manager.make_hotel_block(block_id, check_in, check_out, room_num)
 
-      expect(test_manager.all_blocks.length).must_equal 10
+      expect(test_manager.all_block_reservations.length).must_equal 10
     end
-    # it "removes the blocked rooms from the list of available rooms" do
-    #   test_manager = Reservation_Manager.new
-    #   check_in = "2019-4-5"
-    #   check_out = "2019-4-5"
-    #   room_num = 5
-    #   testing_block = test_manager.make_hotel_block(check_in, check_out, room_num)
+    it "removes the blocked rooms from the list of available rooms" do
+      test_manager = Reservation_Manager.new
+      check_in = "2019-4-5"
+      check_out = "2019-4-10"
+      room_num = 5
+      block_id = rand(1..6)
+      test_manager.make_hotel_block(block_id, check_in, check_out, room_num)
 
-    #   expect(test_manager.find_available_rooms(check_in, check_out).length).must_equal 15
-    # end
+      check_in = "2019-3-5"
+      check_out = "2019-3-10"
+      room_num = 5
+      test_manager.make_hotel_block(block_id, check_in, check_out, room_num)
+
+      date1 = "2019-4-5"
+      date2 = "2019-4-10"
+
+      expect(test_manager.find_available_rooms(date1, date2).length).must_equal 15
+    end
+
+    it "cannot make a reservation for hotel block room when not part of that party" do
+      test_manager = Reservation_Manager.new
+
+      check_in = "2019-4-5"
+      check_out = "2019-4-10"
+      room_num = 1
+      block_id = rand(1..6)
+      test_manager.make_hotel_block(block_id, check_in, check_out, room_num)
+      check_in = "2019-3-5"
+      check_out = "2019-3-10"
+      room_num = 5
+      test_manager.make_hotel_block(block_id, check_in, check_out, room_num)
+
+      outsider_reservation = test_manager.make_reservation(check_in, check_out)
+      expect(outsider_reservation).wont_equal test_manager.all_block_reservations[1].room
+    end
+
+    it "will raise argument error if hotel blocks have taken up all rooms and outsider tries to reserve" do
+      test_manager = Reservation_Manager.new
+
+      check_in = "2019-4-5"
+      check_out = "2019-4-10"
+      room_num = 5
+      block_id = rand(1..6)
+
+      4.times do
+        test_manager.make_hotel_block(block_id, check_in, check_out, room_num)
+      end
+
+      expect { test_manager.make_reservation("2019-4-9", "2019-4-11") }.must_raise ArgumentError
+    end
+
+    it "will raise argument error if not enough rooms for hotel block" do
+      test_manager = Reservation_Manager.new
+
+      check_in = "2019-4-5"
+      check_out = "2019-4-10"
+      room_num = 5
+      block_id = 3
+
+      4.times do
+        test_manager.make_hotel_block(block_id, check_in, check_out, room_num)
+      end
+
+      expect { test_manager.make_hotel_block("2019-4-9", "2019-4-11") }.must_raise ArgumentError
+    end
+  end
+
+  describe "make reservation for hotel blocked room" do
+    it "cannot make a reservation for hotel blocked room if not part of block" do
+      test_manager = Reservation_Manager.new
+
+      #assign a hotel block
+      check_in = "2019-4-5"
+      check_out = "2019-4-10"
+      room_num = 5
+      block_id = rand(1..6)
+      4.times do
+        test_manager.make_hotel_block(block_id, check_in, check_out, room_num)
+      end
+
+      expect { test_manager.make_reservation(check_in, check_out) }.must_raise ArgumentError
+    end
+
+    it "can make a reservation for hotel blocked room if part of block" do
+      test_manager = Reservation_Manager.new
+
+      #assign a hotel block
+      check_in = "2019-4-5"
+      check_out = "2019-4-10"
+      room_num = 5
+      3.times do
+        test_manager.make_hotel_block(rand(1..6), check_in, check_out, room_num)
+      end
+      test_manager.make_hotel_block(7, check_in, check_out, room_num)
+
+      expect(test_manager.make_reservation(check_in, check_out, is_hotel_blocker: true, block_id: 7)).must_be_kind_of Reservation
+    end
+
+    it "will have a discounted total rate if room booked is part of hotel block" do
+      test_manager = Reservation_Manager.new
+
+      #assign a hotel block
+      check_in = "2019-4-5"
+      check_out = "2019-4-10"
+      room_num = 5
+      block_id = 7
+      3.times do
+        test_manager.make_hotel_block(rand(1..6), check_in, check_out, room_num)
+      end
+      test_manager.make_hotel_block(block_id, check_in, check_out, room_num)
+
+      #make reservation within that block
+      block_test_reserve = test_manager.make_reservation(check_in, check_out, is_hotel_blocker: true, block_id: block_id)
+
+      #check block_test_reserve cost
+      expect(block_test_reserve.cost(block_test_reserve.duration)).must_equal (5 * 150)
+    end
+
+    it "will reassign the dates of hotel blocker to set duration regardless of their input" do
+      test_manager = Reservation_Manager.new
+
+      #assign a hotel block
+      check_in = "2019-4-5"
+      check_out = "2019-4-10"
+      room_num = 5
+      block_id = 7
+      3.times do
+        test_manager.make_hotel_block(rand(1..6), check_in, check_out, room_num)
+      end
+      test_manager.make_hotel_block(block_id, check_in, check_out, room_num)
+
+      new_in_date = "2019-4-6"
+      new_out_date = "2019-4-8"
+
+      #make reservation within that block
+      block_test_reserve = test_manager.make_reservation(new_in_date, new_out_date, is_hotel_blocker: true, block_id: block_id)
+
+      #check new checkin/checkout dates
+      expect(block_test_reserve.check_in).must_equal Date.parse(check_in)
+      expect(block_test_reserve.check_out).must_equal Date.parse(check_out)
+    end
+
+    it "will reassign the room of hotel blocker to available room" do
+      test_manager = Reservation_Manager.new
+
+      #assign a hotel block
+      check_in = "2019-4-5"
+      check_out = "2019-4-10"
+      room_num = 3
+      block_id = 7
+      test_manager.make_hotel_block(block_id, check_in, check_out, room_num)
+
+      # make list of rooms available to hotel blockers
+      room_options = []
+      test_manager.all_block_reservations.each do |reservation|
+        room_options << reservation.room
+      end
+
+      #make reservation within that block
+      new_in_date = "2019-4-6"
+      new_out_date = "2019-4-8"
+      block_test_reserve1 = test_manager.make_reservation(new_in_date, new_out_date, is_hotel_blocker: true, block_id: block_id)
+      block_test_reserve2 = test_manager.make_reservation(new_in_date, new_out_date, is_hotel_blocker: true, block_id: block_id)
+      block_test_reserve3 = test_manager.make_reservation(new_in_date, new_out_date, is_hotel_blocker: true, block_id: block_id)
+
+      puts "BLOCKED RESERAVTION ROOMS!!!!!!"
+      puts block_test_reserve1.room
+      puts block_test_reserve2.room
+      puts block_test_reserve3.room
+
+      expect(block_test_reserve1.room).wont_equal block_test_reserve2.room
+      expect(room_options).must_include block_test_reserve1.room
+      expect(test_manager.all_block_reservations.length).must_equal 0
+    end
+  end
+  describe "finding block availability" do
+    it "returns pending reservations for block" do
+      test_manager = Reservation_Manager.new
+
+      check_in = "2019-4-5"
+      check_out = "2019-4-10"
+      room_num = 5
+      block_id = 8
+      test_manager.make_hotel_block(block_id, check_in, check_out, room_num)
+      test_manager.make_reservation(check_in, check_out, is_hotel_blocker: true, block_id: 8)
+      test_manager.make_reservation(check_in, check_out, is_hotel_blocker: true, block_id: 8)
+
+      expect(test_manager.all_block_reservations.length).must_equal 3
+    end
   end
 end
