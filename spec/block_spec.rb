971e6cb5dfc016be3f8d472889c4ec7313455946
MyriamWD <myriam.waldend@gmail.com>
2019-03-17 14:27:46 -0700

    added test Argument error when creating a block if rooms are in another block

--- spec/block_spec.rb
+++ spec/block_spec.rb
@@ -3,21 +3,15 @@ describe "Block" do
   it "creates a block" do
     start_date = Time.parse("2019-02-27 14:08:45 -0700")
     end_date = Time.parse("2019-02-28 14:08:45 -0700")
+    id = 2
     rooms = [1, 4, 6, 16, 19]
     rate_discount = 10
-    block = Hotel::Block.new(start_date, end_date, rooms, rate_discount)
+    block = Hotel::Block.new(id, start_date, end_date, rooms, rate_discount)
     expect(block).must_be_instance_of Hotel::Block
     expect(block.start_date).must_be_instance_of Time
     expect(block.end_date).must_be_instance_of Time
     expect(block.rooms).must_be_instance_of Array
     expect(block.rate_discount).must_be_kind_of Integer
-  end
-  it "raises ArgumentError if date range is invalid" do
-    start_date = Time.parse("2019-03-27 14:08:45 -0700")
-    end_date = Time.parse("2019-02-28 14:08:45 -0700")
-    rooms = [1, 4, 6, 16, 19]
-    rate_discount = 10
-    expect { Hotel::Block.new(start_date, end_date, rooms, rate_discount) }.must_raise ArgumentError
   end
 end
 # As a user of the hotel system,
