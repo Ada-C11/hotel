a58a5d5a2a7dee5f04a11fbcc044ae34f9273897
Ngoc Le <ngocle@outlook.com>
2019-03-17 14:43:30 -0700

    Added reservations_by_date function for ReservationTracker class/tests & remove uneccessary attr_accessor

--- lib/reservation_tracker.rb
+++ lib/reservation_tracker.rb
@@ -5,24 +5,35 @@ require "pry"
 
 class ReservationTracker
   attr_reader :total_room
-  attr_accessor :room, :reservation, :date, :reservations
+  attr_accessor :reservations, :reservations_per_room
 
   def initialize
-    @room = room
-    @reservation = reservation
-    @date = date
-    @total_room = total_room
+    #@total_room = total_room
     @reservations = []
+    @reservations_per_room = {}
   end
 
   def total_room
     return @total_room = (1..20).to_a
   end
 
+  def reservations_by_date(date)
+    res_date = Date.parse(date)
+    reservations_at_date = @reservations.select { |reservation| reservation.start_date == res_date }
+    return reservations_at_date
+  end
+
+  def list_avail_room(start_date, end_date)
+  end
+
   def add_reservation(name, room_id, start_date, end_date)
     @new_reservation = Reservation.new(name: name, room_id: room_id, start_date: start_date, end_date: end_date)
     # include a is_valid method here to check if the reservation is valid before adding it to a list of reservation
     @reservations << @new_reservation
+    if @reservations_per_room[room_id] == nil
+      @reservations_per_room[room_id] = []
+    end
+    @reservations_per_room[room_id] << @new_reservation
   end
 end
 
--- spec/reservation_tracker_spec.rb
+++ spec/reservation_tracker_spec.rb
@@ -1,17 +1,31 @@
 require "spec_helper"
-describe "ReservationTracker#add_reservation" do
+describe "ReservationTracker" do
   def build_reservation_tracker
     return ReservationTracker.new
   end
 
   before do
     @tracker = build_reservation_tracker
-    @tracker.add_reservation("Jane", 2, "2019-4-11", "2019-4-14")
-    @tracker.add_reservation("Matt", 2, "2019-1-11", "2019-12-11")
+    @tracker.add_reservation("Jane", 1, "2019-4-11", "2019-4-14")
+    @tracker.add_reservation("Matt", 2, "2019-4-11", "2019-4-13")
     @tracker.add_reservation("Ngoc", 3, "2019-12-9", "2019-12-23")
   end
-  it "return the right number of reservations" do
-    res_no = @tracker.reservations.length
-    expect(res_no).must_equal 3
+  describe "ReservationTracker#add_reservation" do
+    it "return the right number of reservations" do
+      res_no = @tracker.reservations.length
+      expect(res_no).must_equal 3
+    end
+  end
+
+  describe "ReservationTracker#reservations_by_date" do
+    it "return the right number of reservations by date" do
+      list_of_reservation = @tracker.reservations_by_date("2019-4-11")
+      expect(list_of_reservation.length).must_equal 2
+    end
+
+    it "return the name on the reservation with the start_date the same as the given date" do
+      list_of_reservation = @tracker.reservations_by_date("2019-4-11")
+      expect(list_of_reservation[1].name).must_equal "Matt"
+    end
   end
 end
