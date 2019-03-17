0d2df6c890081cfc29f79cec8177cc515b401f58
Heather Izumi <hkizumi@gmail.com>
2019-03-17 11:08:17 -0700

    Removed unused BlockRoom class and updated spec_helper file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,4 +12,3 @@ Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 # Require_relative your lib files here!
 require_relative "../lib/reservation_manager.rb"
 require_relative "../lib/reservation.rb"
-require_relative "../lib/block_room.rb"
