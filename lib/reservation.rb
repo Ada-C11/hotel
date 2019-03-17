dbeead25152d14adbbcc897113f8d92fea52d9b4
Heather Izumi <hkizumi@gmail.com>
2019-03-16 20:25:23 -0700

    Net zero change from last commit I think

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -2,14 +2,13 @@ require "date"
 
 module Hotel
   class Reservation
-    attr_reader :ROOM_RATE, :room_number, :check_in, :check_out, :block_id
+    attr_reader :room_number, :check_in, :check_out, :block_id
     ROOM_RATE = 200
 
     def initialize(room_number, check_in, check_out, block_id: nil)
       @room_number = room_number
       @check_in = Date.parse(check_in)
       @check_out = Date.parse(check_out)
-      #   @block_id = block_id
 
       if @check_out < @check_in
         raise ArgumentError, "Invalid dates, checkout date must be after checkin date."
