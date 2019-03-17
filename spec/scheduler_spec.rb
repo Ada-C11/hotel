6fb88541e7a782ddbdc39c271d6fd9f6509dd3ae
Elise Pham <elise.pham88@gmail.com>
2019-03-17 12:31:27 -0700

    Refactor reserve_room_in_block method test

--- spec/scheduler_spec.rb
+++ spec/scheduler_spec.rb
@@ -195,15 +195,12 @@ describe "Scheduler class" do
   end
 
   describe "reserve_room_in_block method" do
-    before do
+    it "returns a Reservation object" do
       @scheduler = Hotel::Scheduler.new(5)
       room_ids = [1, 2, 3]
       discounted_rate = 180
       duration = Hotel::Time_Interval.new(Date.parse("2019-03-14"), Date.parse("2019-03-18"))
       @block_id = @scheduler.create_block(duration, room_ids, discounted_rate)
-    end
-
-    it "returns a Reservation object" do
       reservation = @scheduler.reserve_room_in_block(@block_id, 1)
       expect(reservation).must_be_instance_of Hotel::Reservation
     end
