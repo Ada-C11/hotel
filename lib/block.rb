bff23c90fd5e38c5abbf08bcea52157a48ea11a5
Nara <naraa_520@yahoo.com>
2019-03-16 18:31:07 -0700

    Added a block class and block test files.

---
+++ lib/block.rb
@@ -0,0 +1,17 @@
+require_relative 'date_range'
+require_relative 'room'
+require_relative 'hotel_dispatcher'
+
+module Hotel
+  class Block
+    attr_accessor :start_date, :end_date, :date_range, :rooms, :discounted_rate
+    def initialize(start_date:, end_date:, rooms:, discounted_rate:)
+      @date_range = Hotel::DateRange.new(start_date, end_date)
+      @rooms = rooms
+      @discounted_rate = discounted_rate
+      @reservations = []
+      @rooms.each {|room| room.room_rate = discounted_rate}
+      raise ArgumentError.new("A block can contain a maximum of 5 rooms") if @rooms.length > 5 
+    end
+  end
+end
