debb9a87b91368abb28f414a27b0efd01b516382
Mello-Cello <celloelle@gmail.com>
2019-03-16 18:35:07 -0700

    Added a test for 'Display a list of rooms not reserved on a given date range'. It's failing as desired.

--- spec/manager_spec.rb
+++ spec/manager_spec.rb
@@ -1,43 +1,88 @@
 require_relative "spec_helper.rb"
+describe "Manager Spec" do
+  describe "Hotel Manager Setup" do
+    let (:this_manager) { Hotel::Manager.new }
+    it "Make 20 rooms" do
+      # p this_manager.rooms_reservations_hash
+      expect(this_manager.rooms_reservations_hash.length).must_equal 20
+    end
 
-describe "Hotel Manager Setup" do
-  let (:this_manager) { this_manager = Hotel::Manager.new }
-  it "Make 20 rooms" do
-    # p this_manager.rooms_reservations_hash
-    expect(this_manager.rooms_reservations_hash.length).must_equal 20
+    it "List all rooms" do
+      skip
+      expect(this_manager.list_rooms).must_include 13
+      expect(this_manager.list_rooms).wont_include 21
+      expect(this_manager.list_rooms).wont_include 0
+    end
+
+    it "Add a new reservation to a room's reservations array" do
+      check_in = Date.new(2019, 06, 03)
+      check_out = Date.new(2019, 06, 07)
+      room = 1
+      this_manager.make_res_for_room(check_in, check_out, room)
+      expect(this_manager.rooms_reservations_hash[1][0]).must_be_instance_of Hotel::Reservation
+    end
+
+    it "Show a list of reservations on a given date" do
+      check_in1 = Date.new(2019, 06, 03)
+      check_out1 = Date.new(2019, 06, 07)
+      room1 = 1
+      this_manager.make_res_for_room(check_in1, check_out1, room1)
+
+      check_in2 = Date.new(2019, 06, 05)
+      check_out2 = Date.new(2019, 06, 10)
+      room2 = 8
+      this_manager.make_res_for_room(check_in2, check_out2, room2)
+
+      date_to_check = Date.new(2019, 06, 06)
+
+      res_on_date = this_manager.list_reservations_for_date(date_to_check)
+
+      expect(res_on_date[0][0]).must_equal 1
+      expect(res_on_date[1][0]).must_equal 8
+    end
   end
 
-  it "List all rooms" do
-    skip
-    expect(this_manager.list_rooms).must_include 13
-    expect(this_manager.list_rooms).wont_include 21
-    expect(this_manager.list_rooms).wont_include 0
-  end
+  describe "Room Availability" do
+    let (:this_manager) { Hotel::Manager.new }
+    # generates this_manager.rooms_reservations_hash
 
-  it "Add a new reservation to a room's reservations array" do
-    check_in = Date.new(2019, 06, 03)
-    check_out = Date.new(2019, 06, 07)
-    room = 1
-    new_res = this_manager.make_res_for_room(check_in, check_out, room)
-    expect(this_manager.rooms_reservations_hash[1][0]).must_be_instance_of Hotel::Reservation
-  end
+    let (:res1) {
+      check_in1 = Date.new(2019, 06, 03)
+      check_out1 = Date.new(2019, 06, 07)
+      room1 = 1
+      this_manager.make_res_for_room(check_in1, check_out1, room1)
+    }
 
-  it "Show a list of reservations on a given date" do
-    check_in1 = Date.new(2019, 06, 03)
-    check_out1 = Date.new(2019, 06, 07)
-    room1 = 1
-    new_res1 = this_manager.make_res_for_room(check_in1, check_out1, room1)
+    let (:res2) {
+      check_in2 = Date.new(2019, 06, 05)
+      check_out2 = Date.new(2019, 06, 10)
+      room2 = 8
+      res2 = this_manager.make_res_for_room(check_in2, check_out2, room2)
+    }
 
-    check_in2 = Date.new(2019, 06, 05)
-    check_out2 = Date.new(2019, 06, 10)
-    room2 = 8
-    new_res2 = this_manager.make_res_for_room(check_in2, check_out2, room2)
+    let (:res3) {
+      check_in3 = Date.new(2019, 06, 11)
+      check_out3 = Date.new(2019, 06, 12)
+      room3 = 1
+      res3 = this_manager.make_res_for_room(check_in3, check_out3, room3)
+    }
 
-    date_to_check = Date.new(2019, 06, 06)
+    # let (:test_dates) {}
 
-    res_on_date = this_manager.list_reservations_for_date(date_to_check)
+    it "Display a list of rooms not reserved on a given date range" do
+      # ARRANGE
+      this_manager
+      res1
+      res2
+      res3
+      ck_in = Date.new(2019, 6, 8)
+      ck_out = Date.new(2019, 6, 9)
 
-    expect(res_on_date[0][0]).must_equal 1
-    expect(res_on_date[1][0]).must_equal 8
+      # ACT
+      avail_rooms = this_manager.find_avail_rooms_for_dates(ck_in, ck_out)
+
+      # ASSERT
+      expect(avail_rooms).must_equal [1, 2, 3, 4, 5, 6, 7, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+    end
   end
 end
--- spec/reservation_spec.rb
+++ spec/reservation_spec.rb
@@ -4,7 +4,7 @@ describe "Reservation Class" do
   let (:this_res) {
     check_in = Date.new(2019, 06, 03)
     check_out = Date.new(2019, 06, 07)
-    this_res = Hotel::Reservation.new(check_in, check_out)
+    Hotel::Reservation.new(check_in, check_out)
   }
   it "Initialize a Reservation" do
     expect(this_res).must_be_instance_of Hotel::Reservation
@@ -13,6 +13,7 @@ describe "Reservation Class" do
   it "Make a reservation for given dates" do
     check_in_date = this_res.ckin_date.to_s
     expect(check_in_date).must_equal "2019-06-03"
+    expect(this_res.num_nights_of_stay).must_equal 4
   end
 
   it "Raise argument error if check in date is >= (after or the same as) check out date" do
@@ -25,6 +26,16 @@ describe "Reservation Class" do
   it "Calculate and store base cost for a reservation" do
     expect(this_res.base_cost).must_equal 800
   end
+
+  # it "Calculate length of stay" do
+  #   this_manager = Hotel::Manager.new
+  #   check_in = Date.new(2019, 06, 03)
+  #   check_out = Date.new(2019, 06, 07)
+  #   new_res = Hotel::Reservation.new(check_in, check_out)
+
+  #   length = new_res.calculate_length_of_stay
+  #   expect(length).must_equal 4
+  # end
 
   # TESTING DATE CONVERSION
   # INSTANCE OF DATE OBJECT TO STRING
