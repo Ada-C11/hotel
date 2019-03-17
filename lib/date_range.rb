5cae5ff6456a93d8446118fe62a654b57e5fd83b
Nara <naraa_520@yahoo.com>
2019-03-17 14:45:12 -0700

    Added attr_reader for start_date and end_date

--- lib/date_range.rb
+++ lib/date_range.rb
@@ -5,6 +5,7 @@ require_relative 'block'
 
 module Hotel
   class DateRange
+    attr_reader :start_date, :end_date
     def initialize(start_date, end_date)
       @start_date = Date.parse(start_date)
       @end_date = Date.parse(end_date)
