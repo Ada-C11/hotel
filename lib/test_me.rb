ebfa47ced15624c3337f8fe4cd550b60a6f6a787
Tatiana Quintana <tatsqui@gmail.com>
2019-03-17 10:42:58 -0700

    creating block class

--- lib/test_me.rb
+++ lib/test_me.rb
@@ -20,22 +20,5 @@ class BlockParty
   def booked_on(check_in:, check_out:)
     @bookings << (Date.parse(check_in)..Date.parse(check_out))
   end
-
-  def room_available?(check_in:, check_out:)
-    @bookings.each do |booked|
-      if booked == nil
-        return true
-      end
-      if (check_in..check_out).include?(booked)
-        return false
-      end
-      if check_out == booked.first || check_in == booked.last
-        return true
-      elsif booked.include?(check_in) || booked.include?(check_out) || check_in <= booked.first && check_out >= booked.last
-        return false
-      end
-    end
-    return true
-  end
 end
 
