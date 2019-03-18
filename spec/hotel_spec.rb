d4645e866cb84bee5a3cbf70547bc4cd5f93e0e2
Pauline Sauget <paulinevsauget@gmail.com>
2019-03-17 23:14:21 -0700

    Added Hotel#list_available_rooms tests

--- spec/hotel_spec.rb
+++ spec/hotel_spec.rb
@@ -2,55 +2,68 @@ require_relative "./spec_helper"
 
 describe "Hotel" do
   before do
-    @test_hotel = BookingSystem::Hotel.new
-    @test_room = BookingSystem::Room.new(room_num: 1337)
+    rooms = (1..20)
+    @test_hotel = BookingSystem::Hotel.new(rooms: rooms)
+    @jan1 = Date.new(2020, 1, 1)
     @checkin = Date.new(2020, 1, 2)
+    @jan3 = Date.new(2020, 1, 3)
     @checkout = Date.new(2020, 1, 4)
+    @jan5 = Date.new(2020, 1, 5)
   end
 
   describe "initialize" do
     it "creates an instance of Hotel" do
       expect(@test_hotel).must_be_kind_of BookingSystem::Hotel
     end
+
+    it "creates an array of Rooms numbered 1 through 20" do
+      expect(@test_hotel.rooms).must_be_kind_of Array
+      expect(@test_hotel.rooms.length).must_equal 20
+      expect(@test_hotel.rooms.first).must_be_kind_of BookingSystem::Room
+      expect(@test_hotel.rooms.first.room_num).must_equal 1
+      expect(@test_hotel.rooms.last).must_be_kind_of BookingSystem::Room
+      expect(@test_hotel.rooms.last.room_num).must_equal 20
+    end
   end
 
   describe "add_room" do
     it "adds room into Hotel's array of rooms" do
-      @test_hotel.add_room(@test_room)
+      test_room = BookingSystem::Room.new(room_num: 1337)
+      @test_hotel.add_room(test_room)
       expect(@test_hotel.rooms).must_be_kind_of Array
-      expect(@test_hotel.rooms[0]).must_be_kind_of BookingSystem::Room
-      expect(@test_hotel.rooms.count).must_equal 1
-      expect(@test_hotel.rooms[-1].room_num).must_equal @test_room.room_num
+      expect(@test_hotel.rooms.first).must_be_kind_of BookingSystem::Room
+      expect(@test_hotel.rooms.count).must_equal 21
+      expect(@test_hotel.rooms.last.room_num).must_equal test_room.room_num
     end
   end
 
   describe "list_all_rooms" do
     it "returns an empty array if there is no room in the hotel" do
-      expect(@test_hotel.list_all_rooms).must_be_kind_of Array
-      expect(@test_hotel.list_all_rooms.length).must_equal 0
+      rooms_not_specified = BookingSystem::Hotel.new
+      expect(rooms_not_specified.list_all_rooms).must_be_kind_of Array
+      expect(rooms_not_specified.list_all_rooms.length).must_equal 0
     end
 
     it "returns an array of all the rooms in the hotel" do
-      @test_hotel.add_room(@test_room)
       all_rooms = @test_hotel.list_all_rooms
       expect(all_rooms).must_be_kind_of Array
-      expect(all_rooms.count).must_equal 1
+      expect(all_rooms.count).must_equal 20
     end
   end
 
   describe "new_reservation" do
     before do
-      @test_hotel.new_reservation(@test_room, @checkin, @checkout)
+      @test_hotel.new_reservation(@test_hotel.rooms.first, @checkin, @checkout)
     end
 
     it "adds new reservation to Hotel's reservations" do
-      expect(@test_hotel.reservations[0]).must_be_kind_of BookingSystem::Reservation
+      expect(@test_hotel.reservations.first).must_be_kind_of BookingSystem::Reservation
       expect(@test_hotel.reservations.length).must_equal 1
     end
 
     it "adds new reservation to Room's reservations" do
       expect(@test_hotel.reservations[0]).must_be_kind_of BookingSystem::Reservation
-      expect(@test_room.reservations.length).must_equal 1
+      expect(@test_hotel.rooms.first.reservations.length).must_equal 1
     end
   end
 
@@ -61,7 +74,7 @@ describe "Hotel" do
     end
 
     it "returns an array of all reservations on a given date" do
-      @test_hotel.new_reservation(@test_room, @checkin, @checkout)
+      @test_hotel.new_reservation(@test_hotel.rooms.first, @checkin, @checkout)
       reservations = @test_hotel.list_by_date(@checkin)
       expect(reservations).must_be_kind_of Array
       expect(reservations[0]).must_be_kind_of BookingSystem::Reservation
@@ -69,76 +82,30 @@ describe "Hotel" do
     end
   end
 
-  describe "overlap?" do
-    before do
-      @test_hotel.new_reservation(@test_room, @checkin, @checkout)
-    end
-
-    it "returns true for a room with no reservation on a given day" do
-      expect(@test_hotel.overlap?(Date.new(1776, 7, 4))).must_equal false
-    end
-
-    it "returns false for a room that is booked on a given day" do
-      expect(@test_hotel.overlap?(@checkin)).must_equal true
-    end
-  end
-
   describe "list_available_rooms" do
     before do
-      @before_checkin = Date.new(2020, 1, 1)
-      @in_between = Date.new(2020, 1, 3)
-      @after_checkout = Date.new(2020, 1, 5)
+      @test_res = @test_hotel.new_reservation(@test_hotel.rooms.first, @checkin, @checkout)
     end
 
-    it "raises an ArgumentError if there is no room in the hotel" do
+     it "returns an array of all available rooms in the hotel" do
+      same_days = @test_hotel.list_available_rooms(@checkin, @checkout)
+      expect(same_days).must_be_kind_of Array
+      expect(same_days.count).must_equal 19
+      
+      diff_days = @test_hotel.list_available_rooms(@jan1, @checkin)
+      expect(diff_days).must_be_kind_of Array
+      expect(diff_days.count).must_equal 20
+    end
+
+    it "raises an ArgumentError if there is no room available in the hotel" do
+      # New hotel, with 1 room, and conflicting resolution
+      new_hotel = BookingSystem::Hotel.new
+      new_room = BookingSystem::Room.new(room_num: 1337)
+      new_hotel.add_room(new_room)
+      new_res = new_hotel.new_reservation(new_room, @checkin, @checkout)
       expect do
-        @test_hotel.list_available_rooms(@checkin)
+        new_hotel.list_available_rooms(@checkin, @checkout)
       end.must_raise ArgumentError
     end
-
-    it "raises an ArgumentError for overlaping booking" do
-      # starts before, ends during
-      # starts during, ends after
-      # starts on checkin, ends on checkout
-      # starts before, ends on checkout
-      # starts on checkin, ends after
-      # starts during, ends during
-      # starts before and ends after
-      # starts during, ends on checkout
-      # starts on checkin, ends during
-    end
-
-    it "returns array of available rooms for OK cases" do
-      # starts before, ends on checkin
-      # starts before, ends before
-      # starts on checkout, ends after
-      # starts after, ends after
-    end
-
-    # it "raises an ArgumentError if there is no available room in the hotel" do
-    #   @test_hotel.add_room(@test_room)
-    #   # Conflicting date
-    #   @test_hotel.new_reservation(@test_room, @checkin, @checkout)
-    #   expect do
-    #     @test_hotel.list_available_rooms(@checkin)
-    #   end.must_raise ArgumentError
-    # end
-
-    # it "returns an array of all available rooms in the hotel" do
-    #   @test_hotel.add_room(@test_room)
-
-    #   @test_hotel.new_reservation(@test_room, @checkin + 100, @checkout + 100)
-    #   available_rooms = @test_hotel.list_available_rooms(@checkin)
-    #   expect(available_rooms).must_be_kind_of Array
-    #   expect(available_rooms.count).must_equal 1
-
-    #   # Now has two available rooms
-    #   new_room_with_no_reservation = BookingSystem::Room.new(room_num: 1)
-    #   @test_hotel.add_room(new_room_with_no_reservation)
-    #   two_available_rooms = @test_hotel.list_available_rooms(@checkin)
-    #   expect(two_available_rooms).must_be_kind_of Array
-    #   expect(two_available_rooms.count).must_equal 2
-    #   # -1's room_num must match
-    # end
   end
 end
