7d0ebf2177c501c1d6d0c5de170efc5bd675d216
Grace Shea <grace.m.shea@gmail.com>
2019-03-17 20:16:42 -0700

    test shells added to room_spec

--- spec/room_spec.rb
+++ spec/room_spec.rb
@@ -1,2 +1,20 @@
 require "simplecov"
 SimpleCov.start
+
+require_relative "spec_helper"
+
+describe "Room class" do
+  describe "Room initialize" do
+    before do
+    end
+
+    it "is an instance of Room" do
+    end
+
+    it "can return a room number" do
+    end
+
+    it "returns an array of reservations" do
+    end
+  end
+end
