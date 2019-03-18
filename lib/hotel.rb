75987127944dc7a02073f96c34175b91a23df8bb
Cyndi Lopez <cyndilopez6@gmail.com>
2019-03-17 22:48:19 -0700

    added #find_reservation method and fixed how to reserve room in a hotel block. also now raises argument error if #rooms in a block is greater than 5

--- lib/hotel.rb
+++ lib/hotel.rb
@@ -9,13 +9,13 @@ module Hotel
       @hotel_blocks = []
     end
 
-    def reserve_room(start_date, end_date, room_id)
+    def reserve_room(start_date, end_date, room_id, block_id = nil, discount = nil)
       #check if room available during those days
       rooms_available = available_rooms(start_date, end_date)
-      raise ArgumentError if !(rooms_available.include?(room_id))
+      raise ArgumentError if !(rooms_available.include?(room_id)) && block_id == nil
       # find room object with room ID
       room = find_room(room_id)
-      new_reservation = Reservation.new(id: create_reservation_id, start_date: start_date, end_date: end_date, room: room)
+      new_reservation = Reservation.new(id: create_reservation_id, start_date: start_date, end_date: end_date, room: room, block_id: block_id, discount: discount)
       add_reservation(new_reservation)
       room.add_reservation(new_reservation)
 
@@ -27,6 +27,11 @@ module Hotel
       return reservation_id
     end
 
+    def create_block_id
+      block_id = hotel_blocks.length + 1
+      return block_id
+    end
+
     def find_room(room_ID)
       @rooms.detect { |room| room.id == room_ID }
     end
@@ -35,40 +40,62 @@ module Hotel
       reservations << reservation
     end
 
-    def access_reservations(date)
+    def access_reservations(date) # this is wrong
       list_reservations = @reservations.select { |reservation| (Range.new(reservation.start_date, reservation.end_date)).include?(date) }
       return list_reservations.map { |reservation| reservation }
     end
 
+    def find_reservation(room_id, block_id)
+      return reservations.detect { |reservation| reservation.room.id == room_id && reservation.block_id == block_id }
+    end
+
     def available_rooms(start_date, end_date)
-      # for reservation purposes, to check if that room can be reserved
       avail_rooms = []
       rooms.each do |room|
-        avail_rooms << room if room.isAvailable?(start_date, end_date)
+        avail_rooms << room.id if room.isAvailable?(start_date, end_date)
       end
-      return avail_rooms.map { |room| room.id } #return object?
+      return avail_rooms #return object instead?
     end
 
     def reserve_hotel_block(id, start_date, end_date, collection_rooms, discounted_rate)
+      raise ArgumentError, "Max number of rooms is 5" if collection_rooms.length > 5
       collection_rooms.each do |room|
-        raise ArgumentError if !room.isAvailable?(start_date, end_date)
+        raise ArgumentError, "Room not available" if !room.isAvailable?(start_date, end_date)
       end
-      return Block.new(id: id, start_date: start_date, end_date: end_date, collection_rooms: collection_rooms, discounted_rate: discounted_rate)
+      new_block = Block.new(id: id, start_date: start_date, end_date: end_date, collection_rooms: collection_rooms, discounted_rate: discounted_rate)
+      collection_rooms.each do |room|
+        room.add_block(new_block)
+      end
+      add_hotel_block(new_block)
+      return new_block
     end
 
-    def available_rooms_block(start_date, end_date, block_id)
+    def available_rooms_block(block_id)
       available_rooms = []
-      block = find_block(block_id)
-      block.collection_rooms.each do |room|
-        available_rooms << room if !available_rooms.include?(room.id)
+      block_select = find_block(block_id)
+      block_select.collection_rooms.each do |room|
+        if find_reservation(room.id, block_id).nil?
+          available_rooms << room.id
+        end
       end
+      return available_rooms
+    end
+
+    def find_block(block_id)
+      hotel_blocks.detect { |hotel_block| hotel_block.id == block_id }
     end
 
     def add_hotel_block(block)
       hotel_blocks << block
     end
 
-    def reserve_room_hotel_block
+    def reserve_room_hotel_block(room_id, block_id)
+      if available_rooms_block(block_id).include?(room_id)
+        block = find_block(block_id)
+        reserve_room(block.start_date, block.end_date, room_id, block_id)
+      else
+        puts "can't reserve room in block"
+      end
     end
   end
 end
