3dc6c5c0055b58cd1682f87c07f786d2f505228c
Erica Norris <norrise120@gmail.com>
2019-03-17 23:15:40 -0700

    Updated HotelManager to pass tests in spec file

--- lib/hotel_manager.rb
+++ lib/hotel_manager.rb
@@ -12,6 +12,7 @@ module Hotel
       @blocks = []
     end
 
+    #reservation methods
     def make_reservation(room_number, start_date, end_date)
       room = find_room_by_number(room_number)
       if self.list_available_rooms(start_date, end_date).include?(room)
@@ -21,15 +22,6 @@ module Hotel
       else
         raise ArgumentError, "Room #{room_number} is unavailable. Must use an available room."
       end
-    end
-
-    def make_block(room_numbers, start_date, end_date, discount_rate)
-      start_date, end_date = parse_dates(start_date, end_date)
-      block_rooms = room_numbers.map { |number| find_room_by_number(number) }
-      validate_block(block_rooms, start_date, end_date)
-      new_block = Hotel::Block.new(rooms: block_rooms, start_date: start_date, end_date: end_date, discount_rate: discount_rate)
-      @blocks.push(new_block)
-      return new_block
     end
 
     def list_reservations_by_date(date)
@@ -43,6 +35,25 @@ module Hotel
       return found_reservations
     end
 
+    #block methods
+    def make_block(room_numbers, start_date, end_date, discount_rate)
+      start_date, end_date = parse_dates(start_date, end_date)
+      block_rooms = room_numbers.map { |number| find_room_by_number(number) }
+      validate_block(block_rooms, start_date, end_date)
+      new_block = Hotel::Block.new(rooms: block_rooms, start_date: start_date, end_date: end_date, discount_rate: discount_rate)
+      @blocks.push(new_block)
+      return new_block
+    end
+
+    def reserve_from_block(block, room_number)
+      room = find_room_by_number(room_number)
+      block.book_room(room)
+      new_reservation = Hotel::Reservation.new(room: room, start_date: block.start_date, end_date: block.end_date)
+      @reservations.push(new_reservation)
+      return new_reservation
+    end
+
+    #room methods
     def list_available_rooms(start_date, end_date)
       start_date, end_date = parse_dates(start_date, end_date)
       available_rooms = @rooms
@@ -53,7 +64,9 @@ module Hotel
       end
       blocks.each do |block|
         if block.start_date >= start_date && block.end_date <= end_date
-          available_rooms.delete(block.rooms)
+          block.rooms.each do |room|
+            available_rooms.delete(room)
+          end
         end
       end
       return available_rooms
@@ -68,9 +81,6 @@ module Hotel
     private
 
     def validate_block(rooms, start_date, end_date)
-      unless rooms.length <= 5
-        raise ArgumentError, "The max number of rooms for a block is 5."
-      end
       available_rooms = list_available_rooms(start_date, end_date)
       unless rooms.all? { |room| available_rooms.include?(room) }
         raise ArgumentError, "All rooms for block must be available."
@@ -78,15 +88,16 @@ module Hotel
     end
 
     def parse_dates(start_date, end_date)
-      start_date = Date.parse(start_date)
-      end_date = Date.parse(end_date)
+      start_date = Date.parse(start_date) if start_date.is_a? String
+      end_date = Date.parse(end_date) if end_date.is_a? String
+      validate_dates(start_date, end_date)
       return start_date, end_date
     end
 
-    # def self.validate_dates(start_date, end_date)
-    #   unless end_date > start_date
-    #     raise ArgumentError, "End date must be after start date"
-    #   end
-    # end
+    def validate_dates(start_date, end_date)
+      unless end_date > start_date
+        raise ArgumentError, "End date must be after start date"
+      end
+    end
   end
 end
