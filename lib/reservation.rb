0aa9a394881cd9e3ee955ae8dd625ad449b72bc2
Cyndi Lopez <cyndilopez6@gmail.com>
2019-03-17 22:46:18 -0700

    changed discount to fraction and changed how total cost calculated to include block

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -1,18 +1,24 @@
 module Hotel
   class Reservation
-    attr_reader :id, :start_date, :end_date, :room, :total_cost
+    attr_reader :id, :start_date, :end_date, :room, :total_cost, :block_id, :discount
 
-    def initialize(id:, start_date:, end_date:, room:, total_cost: nil)
+    def initialize(id:, start_date:, end_date:, room:, total_cost: nil, block_id: nil, discount: nil)
       @id = id
       @start_date = start_date
       @end_date = end_date
       raise ArgumentError if end_date <= start_date
       @room = room
       @total_cost = calc_total_cost
+      @block_id = block_id
+      @discount = discount
     end
 
     def calc_total_cost
-      return (end_date - start_date).to_i * room.cost
+      cost = (end_date - start_date).to_i * room.cost
+      if !block_id.nil?
+        cost = cost - cost * discount
+      end
+      return cost
     end
   end
 end
