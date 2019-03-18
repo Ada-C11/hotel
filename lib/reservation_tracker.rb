6271af796729d0172ece641529b725b19f38d3b1
Ngoc Le <ngocle@outlook.com>
2019-03-17 18:32:27 -0700

    Added function to output a list of available room given a date range & raise Argument Error when an unavailable room is booked

--- lib/reservation_tracker.rb
+++ lib/reservation_tracker.rb
@@ -8,13 +8,9 @@ class ReservationTracker
   attr_accessor :reservations, :reservations_per_room
 
   def initialize
-    #@total_room = total_room
+    @total_room = (1..20).to_a
     @reservations = []
     @reservations_per_room = {}
-  end
-
-  def total_room
-    return @total_room = (1..20).to_a
   end
 
   def reservations_by_date(date)
@@ -23,15 +19,41 @@ class ReservationTracker
     return reservations_at_date
   end
 
+  def is_overlap?(start_date, end_date, resservation)
+    return resservation.end_date > start_date && resservation.start_date < end_date
+  end
+
+  def is_date_range_available?(start_date, end_date, reservations)
+    reservations.each do |reservation|
+      if is_overlap?(start_date, end_date, reservation)
+        return false
+      end
+    end
+    return true
+  end
+
   def list_avail_room(start_date, end_date)
+    start_date = Date.parse(start_date)
+    end_date = Date.parse(end_date)
+    available_room = []
+    @total_room.each do |room_id|
+      if @reservations_per_room[room_id] == nil || is_date_range_available?(start_date, end_date, @reservations_per_room[room_id])
+        available_room << room_id
+      end
+    end
+    return available_room
   end
 
   def add_reservation(name, room_id, start_date, end_date)
-    @new_reservation = Reservation.new(name: name, room_id: room_id, start_date: start_date, end_date: end_date)
-    # include a is_valid method here to check if the reservation is valid before adding it to a list of reservation
-    @reservations << @new_reservation
-    if @reservations_per_room[room_id] == nil
-      @reservations_per_room[room_id] = []
+    available_room = list_avail_room(start_date, end_date)
+    if !available_room.include? room_id
+      raise ArgumentError, "The room is not available"
+    else
+      @new_reservation = Reservation.new(name: name, room_id: room_id, start_date: start_date, end_date: end_date)
+      @reservations << @new_reservation
+      if @reservations_per_room[room_id] == nil
+        @reservations_per_room[room_id] = []
+      end
     end
     @reservations_per_room[room_id] << @new_reservation
   end
--- spec/reservation_tracker_spec.rb
+++ spec/reservation_tracker_spec.rb
@@ -15,6 +15,9 @@ describe "ReservationTracker" do
       res_no = @tracker.reservations.length
       expect(res_no).must_equal 3
     end
+    it "raise Argument Error when an unavailable room is reserved for a given day" do
+      expect { @tracker.add_reservation("Ngoc", 3, "2019-12-9", "2019-12-23") }.must_raise ArgumentError
+    end
   end
 
   describe "ReservationTracker#reservations_by_date" do
@@ -26,6 +29,18 @@ describe "ReservationTracker" do
     it "return the name on the reservation with the start_date the same as the given date" do
       list_of_reservation = @tracker.reservations_by_date("2019-4-11")
       expect(list_of_reservation[1].name).must_equal "Matt"
+    end
+  end
+
+  describe "ReservationTracker#list_available_rooms" do
+    it "return the correct number of available room(s)" do
+      available_rooms = @tracker.list_avail_room("2019-1-12", "2019-1-15")
+      expect(available_rooms.length).must_equal 20
+    end
+
+    it "return the correct number of available room(s)" do
+      available_rooms = @tracker.list_avail_room("2019-4-11", "2019-4-15")
+      expect(available_rooms.length).must_equal 18
     end
   end
 end
