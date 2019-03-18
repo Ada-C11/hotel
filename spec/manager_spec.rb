c9b06ce132441890f5e33fcbf7cd35a81bfdb37a
Mello-Cello <celloelle@gmail.com>
2019-03-17 17:48:12 -0700

    Finalized tests for reserving a room. Many edge cases accounted for, both for raising exceptions and for making reservations with shared check-in/check-out dates. (Wave 2 complete.)

--- spec/manager_spec.rb
+++ spec/manager_spec.rb
@@ -99,63 +99,90 @@ describe "Manager Spec" do
       check_in2 = Date.new(2019, 06, 05)
       check_out2 = Date.new(2019, 06, 10)
       room8 = 8
-      res2 = this_manager.make_res_for_room(check_in2, check_out2, room8)
+      this_manager.make_res_for_room(check_in2, check_out2, room8)
     }
 
     let (:res3) {
       check_in3 = Date.new(2019, 06, 11)
       check_out3 = Date.new(2019, 06, 20)
       room1 = 1
-      res3 = this_manager.make_res_for_room(check_in3, check_out3, room1)
+      this_manager.make_res_for_room(check_in3, check_out3, room1)
     }
 
     let (:res4) {
       check_in4 = Date.new(2019, 6, 8)
       check_out4 = Date.new(2019, 6, 9)
       room1 = 1
-      res4 = this_manager.make_res_for_room(check_in4, check_out4, room1)
+      this_manager.make_res_for_room(check_in4, check_out4, room1)
     }
 
     let (:res5_overlap) {
       check_in_test = Date.new(2019, 6, 8)
       check_out_test = Date.new(2019, 6, 9)
       room1 = 1
-      res_test_same_date = this_manager.make_res_for_room(check_in_test, check_out_test, room1)
+      this_manager.make_res_for_room(check_in_test, check_out_test, room1)
     }
 
     let (:res6_overlap_start) {
       check_in6 = Date.new(2019, 6, 10)
       check_out6 = Date.new(2019, 6, 12)
       room1 = 1
-      res_test_same_date = this_manager.make_res_for_room(check_in6, check_out6, room1)
+      this_manager.make_res_for_room(check_in6, check_out6, room1)
     }
 
     let (:res7_overlap_end) {
       check_in7 = Date.new(2019, 6, 19)
       check_out7 = Date.new(2019, 6, 22)
       room1 = 1
-      res_test_same_date = this_manager.make_res_for_room(check_in7, check_out7, room1)
+      this_manager.make_res_for_room(check_in7, check_out7, room1)
     }
 
     let (:res8_overlap_all) {
       check_in7 = Date.new(2019, 6, 10)
       check_out7 = Date.new(2019, 6, 21)
       room1 = 1
-      res_test_same_date = this_manager.make_res_for_room(check_in7, check_out7, room1)
+      this_manager.make_res_for_room(check_in7, check_out7, room1)
     }
 
     let (:res9_overlap_inside) {
       check_in7 = Date.new(2019, 6, 15)
       check_out7 = Date.new(2019, 6, 16)
       room1 = 1
-      res_test_same_date = this_manager.make_res_for_room(check_in7, check_out7, room1)
+      this_manager.make_res_for_room(check_in7, check_out7, room1)
     }
 
     let (:res10_overlap_two_res) {
       check_in7 = Date.new(2019, 6, 2)
       check_out7 = Date.new(2019, 6, 10)
       room1 = 1
-      res_test_same_date = this_manager.make_res_for_room(check_in7, check_out7, room1)
+      this_manager.make_res_for_room(check_in7, check_out7, room1)
+    }
+
+    let (:res11_span_month) {
+      check_in = Date.new(2019, 5, 29)
+      check_out = Date.new(2019, 6, 2)
+      room = 9
+      this_manager.make_res_for_room(check_in, check_out, room)
+    }
+
+    let (:res12_start_date_align) {
+      check_in = Date.new(2019, 6, 2)
+      check_out = Date.new(2019, 6, 12)
+      room = 9
+      this_manager.make_res_for_room(check_in, check_out, room)
+    }
+
+    let (:res13_end_date_align) {
+      check_in = Date.new(2019, 6, 12)
+      check_out = Date.new(2019, 6, 15)
+      room = 9
+      this_manager.make_res_for_room(check_in, check_out, room)
+    }
+    let (:res14_append_to_beginning_with_overlap) {
+      check_in = Date.new(2019, 5, 12)
+      check_out = Date.new(2019, 5, 29)
+      room = 9
+      this_manager.make_res_for_room(check_in, check_out, room)
     }
 
     it "Display a list of rooms not reserved on a given date range" do
@@ -201,13 +228,25 @@ describe "Manager Spec" do
       expect { res10_overlap_two_res }.must_raise ArgumentError # new res overlaps contained dates of existing
     end
 
-    # it "Can create a reservation at edges of months" do
-    #         check_in = Date.new(2019, 06, 03)
-    #         check_out = Date.new(2019, 06, 07)
-    #         room = 1
-    #         this_manager.make_res_for_room(check_in, check_out, room)
-    #         expect(this_manager.rooms_reservations_hash[1][0]).must_be_instance_of Hotel::Reservation
+    it "Can create a reservation at edges of months" do
+      res11_span_month
 
-    # end
+      expect(this_manager.rooms_reservations_hash[9][0].ckin_date.to_s).must_equal "2019-05-29"
+    end
+
+    it "Reservation is allowed if check in or check out is same as another reservation's check in or check out date" do
+      res11_span_month
+      res12_start_date_align
+      res13_end_date_align
+      res14_append_to_beginning_with_overlap
+      #   puts "==================="
+      #   this_manager.rooms_reservations_hash[9].each_with_index do |res, i|
+      #     puts "Index #{i}: #{res.ckin_date} - #{res.ckout_date}"
+      #   end
+      expect(this_manager.rooms_reservations_hash[9][0].ckin_date.to_s).must_equal "2019-05-12"
+      expect(this_manager.rooms_reservations_hash[9][1].ckin_date.to_s).must_equal "2019-05-29"
+      expect(this_manager.rooms_reservations_hash[9][2].ckin_date.to_s).must_equal "2019-06-02"
+      expect(this_manager.rooms_reservations_hash[9][3].ckin_date.to_s).must_equal "2019-06-12"
+    end
   end
 end
