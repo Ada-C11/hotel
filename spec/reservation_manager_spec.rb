f7262139bcd0f28a2a5d7e390bdd0a0e75c4ce54
Grace Shea <grace.m.shea@gmail.com>
2019-03-17 20:12:15 -0700

    instantiation tests for reservation_manager_spec added

--- spec/reservation_manager_spec.rb
+++ spec/reservation_manager_spec.rb
@@ -1,2 +1,22 @@
 require "simplecov"
 SimpleCov.start
+
+require_relative "spec_helper"
+
+describe "Reservation_Manager" do
+  before do
+    @Reservation_Manager = Hotel::Reservation_Manager.new
+  end
+
+  it "has an array of all rooms in hotel" do
+    expect(@hotel.rooms).must_be_kind_of Array
+  end
+
+  it "has 20 rooms in the hotel" do
+    expect(@hotel.rooms.length).must_equal 20
+  end
+
+  it "each index of the array is an instance of class" do
+    expect(@hotel.rooms[0]).must_be_kind_of Hotel::Room
+  end
+end
