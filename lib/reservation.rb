eb6ec97cabb9e70f707b15960df9ad4d641e3c37
Grace Shea <grace.m.shea@gmail.com>
2019-03-17 21:02:14 -0700

    corrected syntax errors. current rake: 25 tests, 0 assertions, 0 failures, 3 errors

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -3,8 +3,8 @@ module Hotel
     attr_reader :name, :room_id, :check_in_date, :check_out_date, :nights_stay
 
     def initialize(input)
-      check_in_date = input[:check_in_date]
-      check_out_date = input[:check_out_date]
+      @check_in_date = input[:check_in_date]
+      @check_out_date = input[:check_out_date]
       @name = input[:name]
       @room_id = input[:room_id]
       @check_in_date = input[:check_in_date]
--- spec/reservation_spec.rb
+++ spec/reservation_spec.rb
@@ -6,22 +6,18 @@ require_relative "spec_helper"
 describe "Reservation class" do
   describe "initialize" do
     before do
-     
     end
 
     it "creates an instance of a Reservation" do
-      
     end
 
     it "has has a room number " do
-
     end
 
     it "has a check-in date" do
-    
     end
 
     it "has a check-out date" do
-   
     end
   end
+end
