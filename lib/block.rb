3662ed83102a424665ff48949af313c11ac6cc8e
MyriamWD <myriam.waldend@gmail.com>
2019-03-17 19:16:05 -0700

    changet attr_reader for attr_accesor for rooms in Block class

--- lib/block.rb
+++ lib/block.rb
@@ -1,7 +1,8 @@
 
 module Hotel
   class Block
-    attr_reader :id, :start_date, :end_date, :rooms, :rate_discount
+    attr_reader :id, :start_date, :end_date, :rate_discount
+    attr_accessor :rooms
 
     def initialize(id, start_date, end_date, rooms, rate_discount)
       @start_date = start_date
