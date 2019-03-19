685de81ad651534deebe6881fdb0d63e8d6a888a
Grace Shea <grace.m.shea@gmail.com>
2019-03-19 08:28:24 -0700

    reservation and room specs

--- spec/reservation_spec.rb
+++ spec/reservation_spec.rb
@@ -6,18 +6,28 @@ require_relative "spec_helper"
 describe "Reservation class" do
   describe "initialize" do
     before do
+      @nights = [Date.new(2021, 10, 8), Date.new(2021, 10, 9), Date.new(2021, 10, 10), Date.new(2021, 10, 11)]
+      input = { name: "Butter",
+               room_id: 5,
+               check_in_date: Date.new(2021, 10, 8),
+               check_out_date: Date.new(2021, 10, 12) }
+      @reservation = Hotel::Reservation.new(input)
     end
 
     it "creates an instance of a Reservation" do
-    end
-
-    it "has has a room number " do
+      expect(@reservation).must_be_kind_of Hotel::Reservation
     end
 
     it "has a check-in date" do
+      expect(@reservation.check_in_date).must_be_kind_of Date
     end
 
     it "has a check-out date" do
+      expect(@reservation.check_out_date).must_be_kind_of Date
+    end
+
+    it "has has a room id" do
+      expect(@reservation.room_id).must_equal 5
     end
   end
 end
--- spec/room_spec.rb
+++ spec/room_spec.rb
@@ -5,16 +5,20 @@ require_relative "spec_helper"
 
 describe "Room class" do
   describe "Room initialize" do
+    @room = Hotel::Room.new(1)
     before do
     end
 
     it "is an instance of Room" do
+      expect(@room).must_be_kind_of Hotel::Room
     end
 
-    it "can return a room number" do
+    it "can return a room id" do
+      expect(@room.room_id).must_equal 1
     end
 
     it "returns an array of reservations" do
+      expect(@room.reservations).must_be_kind_of Array
     end
   end
 end
