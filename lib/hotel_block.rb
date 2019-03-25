aec5bd5834daceb00134871284dec7ef7487e45f
Stephanie Marchante <smarcha04@gmail.com>
2019-03-24 20:05:25 -0700

    add rooms available and block date check methods and associated tests

--- lib/hotel_block.rb
+++ lib/hotel_block.rb
@@ -6,7 +6,8 @@ module Hotel
       if rooms.length > 5
         raise ArgumentError, "Blocks cannot be more than 5 rooms."
       end
-      
+
+
       @rooms = rooms
       @date_range = date_range
       @cost = cost
@@ -17,40 +18,25 @@ module Hotel
       if rooms.length > 5
         raise ArgumentError, "You cannot book more than 5 rooms."
       end
-   
-      reservation = Hotel::Reservation.new(room, check_in, check_out)
+
+      unless rooms_available.include?(room)
+        raise ArgumentError, "Sorry, this room is not available for block reservations" 
+      end
+  
+      reservation = Hotel::Reservation.new(room, date_range)
       @reservations << reservation
       return reservation
     end
 
-    # # check to see if check in and check out match instances of check in and check out
-    # def dates_match(check_in, check_out)
-    #   return check_in == @check_in && check_out == @check_out
-    # end
+    def rooms_available
+      reserved_rooms = @reservations.map{|reservation| reservation.room}
+      available_rooms = @rooms - reserved_rooms
+      return available_rooms
+    end
 
-    # # map over reservations array to find available rooms by room number
-    # def rooms_available
-    #   reserved_rooms = @reservations.map{|reservation| reservation.room_number}
-    #   available_rooms = @room - reserved_rooms
-    #   return available_rooms
-    # end
-
-    # def make_block(room, check_in, check_out, rate = 100)
-    #   hotel_block = Hotel::HotelBlock.new(room, check_in, check_out, rate)
-    #   @reservations << hotel_block
-    #   return hotel_block
-    # end
-
-    # def search_block_date(check_in, check_out)
-    #   return @reservations.find do
-    #     |block| block.dates_match(check_in, check_out)
-    #   end
-    # end
-
-    # def book_block(room, check_in, check_out)
-    #   reservation = Hotel::Reservation.new(room, check_in, check_out, cost: 200)
-    #   return reservation
-    # end
+    def block_date_check(check_in, check_out)
+      return check_in == @check_in && check_out == @check_out
+    end
 
   end
 end
--- spec/hotel_block_spec.rb
+++ spec/hotel_block_spec.rb
@@ -10,87 +10,47 @@ describe "Hotel Block" do
       expect(Hotel::HotelBlock.new([*1..5], @test_range)).must_be_instance_of(Hotel::HotelBlock)
     end 
  
-  #   it "raises an argument error for booking more than 5 rooms" do
-  #     expect{Hotel::HotelBlock.new([*1..6], @check_in, @check_out, 100)}.must_raise(ArgumentError)
-  #   end
+    it "raises an argument error for booking more than 5 rooms" do
+      expect{Hotel::HotelBlock.new([*1..6], @test_range)}.must_raise(ArgumentError)
+    end
   end 
 
-  # describe "add reservation method" do
-  #   before do
-  #     @hotelblock = Hotel::HotelBlock.new([*1..5], @check_in, @check_out )
-  #   end
+  describe "book block reservation method" do
+    before do
+      @hotel_block = Hotel::HotelBlock.new([*1..5], @test_range, 100)
+    end
 
-  #   it "returns an instance of reservation with a room #" do
-  #     reservation = @hotelblock.add_reservation(1, @check_in, @check_out, 100)
-  #     expect(reservation).must_be_instance_of(Hotel::Reservation)
-  #     expect(reservation.room_number).must_equal(1)
-  #   end
+    it "returns a reservation with the right room number and dates" do
+      reservation = @hotel_block.book_block_reservation(3, @test_range)
+      expect(reservation.room).must_equal(3)
+      expect(reservation).must_be_instance_of(Hotel::Reservation)
+    end
 
-  #   it "raises an argument error if date range is invalid" do
-  #     expect{@hotelblock.add_reservation(5, Date.new(2019,3,15), Date.new(2019,3,17))}.must_raise(ArgumentError)
-  #     expect{@hotelblock.add_reservation(5, Date.new(2019,3,15), Date.new(2019,3,18))}.must_raise(ArgumentError)
-  #   end
+    it "raises an argument error for room numbers that are incorrect" do
+      expect{@hotel_block.book_block_reservation(7, @test_range)}.must_raise(ArgumentError)
+    end
+  end
 
-  # end
+  describe "block rooms available method" do
+    before do
+      @hotel_block = Hotel::HotelBlock.new([*1..5], @test_range, 100)
+    end
+    
+    it "returns all currently available room blocks" do
+      expect(@hotel_block.rooms_available).must_equal([1,2,3,4,5])
+    end
 
-  # describe "room available method" do
-  #   before do
-  #     @hotelblock = Hotel::HotelBlock.new([*1..5], Date.new(2019,3,15), Date.new(2019,3,18))
-  #   end
+    it "returns an array of all of the rooms that are available" do
+      @hotel_block.book_block_reservation(3, @test_range)
+      @hotel_block.book_block_reservation(4, @test_range)
+      expect(@hotel_block.rooms_available).must_equal([1,2,5])
+    end
 
-  #   it "all block rooms return if they are all available" do
-  #     expect(@hotelblock.rooms_available).must_equal([1, 2, 3, 4, 5])
-  #   end
-
-  #   it "returns an empty array if there are no available block rooms" do
-  #     [*1..5].each do |r|
-  #       @hotelblock.add_reservation(r, @check_in, @check_out, 100)
-  #     end
-
-  #     expect(@hotelblock.rooms_available).must_equal([])
-  #   end
-
-  #   it "returns an array of block rooms that are available" do
-  #     @hotelblock.add_reservation(3, @check_in, @check_out, 100)
-  #     expect(@hotelblock.rooms_available).must_equal([1, 2, 4, 5])
-  #     @hotelblock.add_reservation(2, @check_in, @check_out, 100)
-  #     expect(@hotelblock.rooms_available).must_equal([1, 4, 5])
-  #   end
-
-  # end 
-
-  # describe "make block reservation method" do
-  #   before do
-  #     @check_in = Date.new(2019,3,15)
-  #     @check_out = Date.new(2019,3,18)
-  #     @frontdesk = Hotel::HotelBlock.new([*1..5], @check_in, @check_out)
-
-  #     @block_res = @frontdesk.make_block([*1..5], @check_in, @check_out)
-  #   end
-
-  #   it "returns an instance of a block" do
-  #     expect(@block_res).must_be_instance_of(Hotel::HotelBlock)
-  #   end
-
-  #   it "adds the booked block to the block reservation array" do
-  #     starter_block_res = @frontdesk.reservations.length
-  #     @frontdesk.make_block([*1..5], @check_in, @check_out, 100)
-  #     expect(@frontdesk.reservations.length - starter_block_res).must_equal(1)
-  #   end
-
-  #   it "will raise an error if reservation you are booking is in a block" do
-  #       @frontdesk.make_block([2, 3], @check_in, @check_out)
-  #       expect{@frontdesk.add_reservation(2, @check_in, @check_out)}.must_raise ArgumentError
-  #   end
-
-  # end
-
-  # describe "book block method" do
-  #   it "returns a reservation" do
-  #     @frontdesk = Hotel::HotelBlock.new([*1..5], @check_in, @check_out)
-  #     block_res = @frontdesk.book_block(3, @check_in, @check_out)
-  #     expect(block_res).must_be_instance_of(Hotel::Reservation)
-  #   end
-  # end
-
+    it "returns an empty array if all block rooms are booked" do
+      [*1..5].each do |room|
+        @hotel_block.book_block_reservation(room, @test_range)
+      end
+      expect(@hotel_block.rooms_available).must_equal([])
+    end
+  end
 end
