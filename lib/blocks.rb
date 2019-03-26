1117e063f33f20dadeb979d8c18b76faff50434e
Tatiana Quintana <tatsqui@gmail.com>
2019-03-25 20:14:39 -0700

    "making modifications to RoomBooker"

--- lib/blocks.rb
+++ lib/blocks.rb
@@ -4,13 +4,12 @@ class BlockParty
   attr_reader :blocked_rooms, :total_cost
   ROOM_BLOCK = 5
 
-  def initialize(check_in:, check_out:, blocked_rooms:, discount:)
+  def initialize(dates:, blocked_rooms:, discount:)
     # unless id.instance_of?(Integer) && id > 0 && id <= 20
     #   raise ArgumentError, "ID must be a positive number, given #{id}..."
     # end
 
-    @check_in = Date.parse(check_in)
-    @check_out = Date.parse(check_out)
+    @dates = dates
     @blocked_rooms = blocked_rooms
     @discount = discount
   end
