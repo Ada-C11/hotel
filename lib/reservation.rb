b5ea618bcb45c807449f2925cd69079f370ed2f7
Faiza Husain <faiza.ahsan1222@gmail.com>
2019-03-26 21:53:05 -0700

    Cleaned up initialize method and created 3 additional methods to calculate room cost, check block size, and populate the room_blocks array

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -9,39 +9,49 @@ module Hotel
     RATE = 200.00
     DISCOUNTED_RATE = 160.00
 
-    def initialize(booking_ref:, number_of_rooms:, first_room_number:, check_in:, check_out:, total_cost:, room_blocks:)
+    def initialize(booking_ref:, number_of_rooms:, first_room_number:, check_in:, check_out:)
       @booking_ref = booking_ref
-
-      if (number_of_rooms <= 5)
-        @number_of_rooms = number_of_rooms
-      else
-        raise ArgumentError, "A block can only have 5 rooms or less."
-      end # if
-
+      @number_of_rooms = check_block_size(number_of_rooms)
       @first_room_number = first_room_number
-
+      @check_in = check_in
+      @check_out = check_out
+      @room_blocks = populate_room_blocks(first_room_number, number_of_rooms)
+      @total_cost = total_cost_calc(number_of_rooms, check_out, check_in)
       if (first_room_number + (number_of_rooms - 1) > 20)
         raise ArgumentError, "Rooms exceed 20, change Room #{first_room_number}."
       end # if
 
-      @check_in = check_in
-      @check_out = check_out
 
       if (@check_in >= @check_out) || (@check_in < Date.today)
         raise ArgumentError, "Check-out date cannot be before check-in date. Check_out date cannot be earlier than today's date."
       end # if
+    end
 
-      if (number_of_rooms == 1)
-        @total_cost = ((check_out - check_in) * number_of_rooms) * RATE
+    def check_block_size(number_of_rooms)
+      if (number_of_rooms <= 5)
+        block_size = number_of_rooms
       else
-        @total_cost = ((check_out - check_in) * number_of_rooms) * DISCOUNTED_RATE
+        raise ArgumentError, "A block can only have 5 rooms or less."
+      end 
+      return block_size
+    end
+
+
+    def total_cost_calc(number_of_rooms, check_out, check_in)
+      if (number_of_rooms == 1)
+        total_cost = ((check_out - check_in) * number_of_rooms) * RATE
+      else
+        total_cost = ((check_out - check_in) * number_of_rooms) * DISCOUNTED_RATE
       end # if
+      return total_cost
+    end
 
-      @room_blocks = Array.new
-
+    def populate_room_blocks(first_room_number, number_of_rooms)
+      room_blocks = []
       for i in (0...number_of_rooms)
-        @room_blocks << (first_room_number + i)
+        room_blocks << (first_room_number + i)
       end
+      return room_blocks
     end
   end
 end
