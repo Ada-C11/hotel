fdfb65d0a77675ecea051d8c77ee89fa7fe24531
Sopheary Chiv <sophearychiv@gmail.com>
2019-03-28 14:50:55 -0700

    moved total_cost method to Reservation

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -3,20 +3,26 @@ require "date"
 
 module Hotel
   class Reservation
-    attr_reader :reservation_id, :room, :room_id, :check_in_date, :check_out_date
+    attr_reader :reservation_id, :room_id, :check_in_date, :check_out_date
 
     def initialize(reservation_id:,
                    room_id:,
                    check_in_date:,
-                   check_out_date:)
+                   check_out_date:,
+                   rate: 200)
       @reservation_id = reservation_id
       @room_id = room_id
       @check_in_date = Date.parse(check_in_date)
       @check_out_date = Date.parse(check_out_date)
+      @rate = rate
     end
 
-    def self.load_all
-      return []
+    def total_cost
+      return (@check_out_date - @check_in_date) * @rate
     end
+
+    # def self.load_all
+    #   return []
+    # end
   end
 end
