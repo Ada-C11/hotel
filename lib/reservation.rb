be4a675a70270e55cb6511964539488a14a5f37a
amyesh <amyesh08@gmail.com>
2019-03-28 16:25:46 -1000

    Added constants to reservation#cost method

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -5,6 +5,9 @@ module Hotel
     attr_reader :checkin_date, :num_of_nights
     attr_accessor :name, :room_num, :checkout_date, :reserved_nights, :block_reference, :block_availability
 
+    REG_PRICE = 200
+    BLOCK_PRICE = 150
+
     def initialize(name, checkin_date, num_of_nights, block_reference: "N/A", block_availability: :UNAVAILABLE)
       @name = name
       @checkin_date = Date.parse(checkin_date)
@@ -34,7 +37,7 @@ module Hotel
     end
 
     def cost
-      return @block_reference == "N/A" ? @num_of_nights * 200 : @num_of_nights * 150
+      return @block_reference == "N/A" ? @num_of_nights * REG_PRICE : @num_of_nights * BLOCK_PRICE
     end
   end
 end
