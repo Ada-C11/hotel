e6635a8e4111139d9db8fccc78515ddebdff7fdd
MyriamWD <myriam.waldend@gmail.com>
2019-03-31 00:35:23 -0700

    added dicounted rate for blocks

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -1,15 +1,19 @@
 
 module Hotel
   class Reservation
-    attr_reader :id, :start_date, :end_date, :room
-
-    def initialize(id, start_date, end_date, room)
-      @id = id
+    attr_reader :start_date, :end_date, :room, :block_id, :discount
+    attr_accessor :reservation_id, :status
+    # add discount to initialize as an optional
+    def initialize(start_date, end_date, room, status: nil, reservation_id: nil, block_id: nil, discount: nil)
       @start_date = start_date
       @end_date = end_date
       @room = room
       @total_nights = total_nights
       @total_cost = total_cost
+      @status = status
+      @reservation_id = reservation_id
+      @block_id = block_id
+      @discount = discount
     end
 
     def total_nights
@@ -20,6 +24,11 @@ module Hotel
     def total_cost
       cost_per_night = 200.0
       total_cost = 0
+      if discount.class == Float
+        discounted_cost_per_night = cost_per_night * (1 - discount)
+        total_cost = discounted_cost_per_night * total_nights
+        return total_cost
+      end
       total_cost = total_nights * cost_per_night
       return total_cost
     end
