4935d54def73abc4c140abe51ab89a4f5ffd6dd3
MyriamWD <myriam.waldend@gmail.com>
2019-03-31 00:25:30 -0700

    eliminated require relatives for block and block manager

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,6 +12,4 @@ Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 # Require_relative your lib files here!
 require_relative "../lib/reservation"
 require_relative "../lib/reservation_manager"
-require_relative "../lib/block"
-require_relative "../lib/block_manager"
 require_relative "../lib/reservations_blocks_template"
